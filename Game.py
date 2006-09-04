71129290abbbcdb3392a94f5fed8990febe94f0b
Thomas Dybdahl Ahle <thomas@ahle.dk>
2006-09-04 18:49:43 +0000

    Did chessclock decisecond precision and gain.

--- Game.py
+++ Game.py
@@ -2,7 +2,8 @@ from Utils.History import History
 def game (board, player1, player2, chessclock = None, seconds = 0, plus = 0):
     board.history = History()
     if chessclock:
-        chessclock.setTime(seconds)
+        chessclock.setTime(seconds*10)
+        chessclock.setGain(plus*10)
     while True:
         for player in player1, player2:
             answer = player.makeMove(board.history)
