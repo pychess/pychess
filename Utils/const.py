1db0092a537e3848bbf25674d9434f0719252d0c
Thijs Vermeir <thijsvermeir@gmail.com>
2006-11-03 08:23:12 +0000

    Adding Utils/const.py (Thomas update)

---
+++ Utils/const.py
@@ -0,0 +1,18 @@
+WHITE, BLACK = range(2)
+
+reprColor = ["White", "Black"]
+
+RUNNING, DRAW, WHITEWON, BLACKWON = range(4)
+
+reprResult = ["*", "1/2-1/2", "1-0", "0-1"]
+
+DRAW_REPITITION, DRAW_50MOVES, DRAW_STALEMATE, DRAW_AGREE, \
+    WON_RESIGN, WON_CALLFLAG, WON_MATE = range(7)
+
+KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN = range(6)
+
+reprSign = ["K", "Q", "R", "B", "N", "P"]
+chr2Sign = {"k":KING, "q": QUEEN, "r": ROOK, "b": BISHOP, "n": KNIGHT, "p":PAWN}
+
+WHITE_OO, WHITE_OOO, BLACK_OO, \
+    BLACK_OOO, WHITE_CASTLED, BLACK_CASTLED = map(lambda x: 2**x, range(6))
