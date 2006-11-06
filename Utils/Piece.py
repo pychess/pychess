e24e1ca32de9be0e7d0233329be4ad6087712d76
Thomas Dybdahl Ahle <thomas@ahle.dk>
2006-11-06 17:32:55 +0000

    Animation :D

--- Utils/Piece.py
+++ Utils/Piece.py
@@ -7,11 +7,15 @@ class Piece:
     def __init__ (self, color, sign):
         self.color = color
         self.sign = sign
-    
-    def __repr__ (self):
-        return "<%s %s>" % (reprColor[self.color], reprPiece[self.sign])
-
+    	
+    	self.opacity = 1.0
+    	self.x = None
+    	self.y = None
+    	
     def __eq__ (self, other):
         return  isinstance(other, Piece) and \
                 self.color == other.color and \
                 self.sign == other.sign
+    
+    def __repr__ (self):
+        return "<%s %s>" % (reprColor[self.color], reprPiece[self.sign])
