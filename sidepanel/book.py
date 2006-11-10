a496bcdad9e61d903453cc08eace022ae743f541
Thomas Dybdahl Ahle <thomas@ahle.dk>
2006-11-10 09:30:55 +0000

    Better tabbing (and reordering)

--- sidepanel/book.py
+++ sidepanel/book.py
@@ -1,82 +1,84 @@
 import gtk, gobject, cairo
 import gamewidget
+from Utils.book import getOpenings
+from Utils.Move import parseSAN, movePool
 
 __title__ = _("Opening Book")
 
-widgets = gtk.glade.XML("sidepanel/book.glade")
-tv = widgets.get_widget("treeview")
-sw = widgets.get_widget("scrolledwindow")
-sw.unparent()
-__widget__ = gtk.Alignment(0,0,1,1)
-__widget__.add(sw)
-
-store = gtk.ListStore(str, str, gobject.TYPE_PYOBJECT)
-def ready (window, page_num):
-    tv.set_model(store)
-    tv.append_column(gtk.TreeViewColumn("Move", gtk.CellRendererText(), text=0))
-    r = gtk.CellRendererText()
-    r.set_property("xalign", 1)
-    tv.append_column(gtk.TreeViewColumn("Games", r, text=1))
-    tv.append_column(gtk.TreeViewColumn("Win/Draw/Loss", window.BookCellRenderer(), data=2))
+class Sidepanel:
     
-    global board, boardcontrol
-    boardcontrol = gamewidget.getWidgets(page_num)[0]
-    board = boardcontrol.view
-    board.connect("shown_changed", shown_changed)
-    tv.connect("cursor_changed", selection_changed)
-    tv.connect("select_cursor_row", selection_changed)
-    tv.connect("row-activated", row_activated)
-
-from Utils.book import getOpenings
-
-def shown_changed (board, shown):
-    global openings
-    openings = getOpenings(board.history[-1])
-    openings.sort(lambda a, b: sum(b[1:])-sum(a[1:]))
-    
-    board.bluearrow = None
-    
-    def helper():
-        store.clear()
+    def load (self, window, page_num):
+        widgets = gtk.glade.XML("sidepanel/book.glade")
+        self.tv = widgets.get_widget("treeview")
+        self.sw = widgets.get_widget("scrolledwindow")
+        self.sw.unparent()
         
-        if not openings and __widget__.get_child() == sw:
-            __widget__.remove(sw)
-            label = gtk.Label(_("In this position,\nthere is no book move."))
-            label.set_property("yalign",0.1)
-            __widget__.add(label)
-            __widget__.show_all()
+        self.store = gtk.ListStore(str, str, gobject.TYPE_PYOBJECT)
+        self.tv.set_model(self.store)
+        
+        self.tv.append_column(gtk.TreeViewColumn(
+                "Move", gtk.CellRendererText(), text=0))
+        r = gtk.CellRendererText()
+        r.set_property("xalign", 1)
+        self.tv.append_column(gtk.TreeViewColumn("Games", r, text=1))
+        self.tv.append_column(gtk.TreeViewColumn(
+                "Win/Draw/Loss", window.BookCellRenderer(), data=2))
+        
+        self.boardcontrol = gamewidget.getWidgets(page_num)[0]
+        self.board = self.boardcontrol.view
+        self.board.connect("shown_changed", self.shown_changed)
+        self.tv.connect("cursor_changed", self.selection_changed)
+        self.tv.connect("select_cursor_row", self.selection_changed)
+        self.tv.connect("row-activated", self.row_activated)
+        
+        return self.sw
+    
+    def shown_changed (self, board, shown):
+        self.openings = getOpenings(self.board.history[-1])
+        self.openings.sort(lambda a, b: sum(b[1:])-sum(a[1:]))
+        
+        self.board.bluearrow = None
+        
+        def helper():
+            store.clear()
+            
+            if not self.openings and __widget__.get_child() == self.sw:
+                __widget__.remove(self.sw)
+                label = gtk.Label(_("In this position,\nthere is no book move."))
+                label.set_property("yalign",0.1)
+                __widget__.add(label)
+                __widget__.show_all()
+                return
+            if self.openings and __widget__.get_child() != self.sw:
+                __widget__.remove(__widget__.get_child())
+                __widget__.add(self.sw)
+            
+            i = 0
+            for move, wins, draws, loses in self.openings:
+                games = wins+draws+loses
+                if not games: continue
+                wins,draws,loses = map(lambda x: x/float(games), (wins,draws,loses))
+                store.append ([move, str(games), (wins,draws,loses)])
+        gobject.idle_add(helper)
+
+    def selection_changed (self, widget):
+        if len(self.board.history) != self.board.shown+1:
+            # History/moveparsing model, sucks, sucks, sucks
+            self.board.bluearrow = None
             return
-        if openings and __widget__.get_child() != sw:
-            __widget__.remove(__widget__.get_child())
-            __widget__.add(sw)
         
-        i = 0
-        for move, wins, draws, loses in openings:
-            games = wins+draws+loses
-            if not games: continue
-            wins,draws,loses = map(lambda x: x/float(games), (wins,draws,loses))
-            store.append ([move, str(games), (wins,draws,loses)])
-    gobject.idle_add(helper)
-
-from Utils.Move import parseSAN, movePool
-
-def selection_changed (widget):
-    if len(board.history) != board.shown+1:
-        # History/moveparsing model, sucks, sucks, sucks
-        board.bluearrow = None
-        return
+        iter = self.tv.get_selection().get_selected()[1]
+        if iter == None:
+            self.board.bluearrow = None
+            return
+        else: sel = self.tv.get_model().get_path(iter)[0]
+        
+        move = parseSAN(self.board.history[-1], self.openings[sel][0])
+        self.board.bluearrow = move.cords
+        movePool.add(move)
     
-    iter = tv.get_selection().get_selected()[1]
-    if iter == None:
-        board.bluearrow = None
-        return
-    else: sel = tv.get_model().get_path(iter)[0]
+    def row_activated (self, widget, *args):
+    	arrow = self.board.bluearrow
+    	if arrow:
+    		self.boardcontrol.emit_move_signal(*arrow)
     
-    move = parseSAN(board.history[-1], openings[sel][0])
-    board.bluearrow = move.cords
-    movePool.add(move)
-
-def row_activated (widget, *args):
-	arrow = board.bluearrow
-	if arrow:
-		boardcontrol.emit_move_signal(*arrow)
--- sidepanel/history.py
+++ sidepanel/history.py
@@ -1,18 +1,13 @@
 import gtk, gobject
 from gtk import gdk
 import gamewidget
+from Utils.Move import toSAN
+
+from gtk.gdk import keyval_from_name
+leftkeys = map(keyval_from_name,("Left", "KP_Left"))
+rightkeys = map(keyval_from_name,("Right", "KP_Right"))
 
 __title__ = _("Move History")
-
-widgets = gtk.glade.XML("sidepanel/history.glade")
-__widget__ = widgets.get_widget("panel")
-__widget__.unparent()
-
-__active__ = True
-
-numbers = widgets.get_widget("treeview1")
-left = widgets.get_widget("treeview2")
-right = widgets.get_widget("treeview3")
 
 def fixList (list, xalign=0):
     list.set_model(gtk.ListStore(str))
@@ -22,49 +17,6 @@ def fixList (list, xalign=0):
     #renderer.set_property("alignment", "left")
     list.append_column(gtk.TreeViewColumn(None, renderer, text=0))
 
-def ready (window, page_num):
-    fixList(numbers, 1)
-    map(fixList, (left, right))
-    numbers.modify_fg(gtk.STATE_INSENSITIVE, gtk.gdk.Color(0,0,0))
-    
-    widgets.signal_autoconnect ({
-        "treeview1_selection_changed": lambda w: select_cursor_row(w,1), 
-        "treeview2_selection_changed": lambda w: select_cursor_row(w,2), 
-        "treeview3_selection_changed": lambda w: select_cursor_row(w,3),
-        "on_treeview2_key_press_event": lambda w,e: key_press_event(1,e),
-        "on_treeview3_key_press_event": lambda w,e: key_press_event(2,e)
-    })
-    
-    global board
-    board = gamewidget.getWidgets(page_num)[0].view
-    
-    board.history.connect("cleared", new_history_object)
-    board.history.connect("changed", history_changed)
-    board.connect("shown_changed", shown_changed)
-    
-def select_cursor_row (tree, col):
-    iter = tree.get_selection().get_selected()[1]
-    if iter == None: return
-    else: sel = tree.get_model().get_path(iter)[0]
-    board.shown = sel*2+col-1
-
-from gtk.gdk import keyval_from_name
-leftkeys = map(keyval_from_name,("Left", "KP_Left"))
-rightkeys = map(keyval_from_name,("Right", "KP_Right"))
-def key_press_event (col, event):
-    if event.keyval in leftkeys and col == 2:
-        shown = board.shown - 1
-        w = left
-    elif event.keyval in rightkeys and col == 1:
-        shown = board.shown + 1
-        w = right
-    else: return
-    row = int((shown-1) / 2)
-    def todo():
-        w.set_cursor((row,))
-        w.grab_focus()
-    idle_add(todo)
-
 def idle_add(proc, *args):
     """Makes sure function is only called once"""
     def proc_star():
@@ -72,56 +24,108 @@ def idle_add(proc, *args):
         return False
     gobject.idle_add(proc_star)
 
-def new_history_object (history):
-    def helper():
-        left.get_model().clear()
-        right.get_model().clear()
-        numbers.get_model().clear()
-    gobject.idle_add(helper)
+class Sidepanel:
 
-from Utils.Move import toSAN
+    def load (self, window, page_num):
+        
+        widgets = gtk.glade.XML("sidepanel/history.glade")
+        __widget__ = widgets.get_widget("panel")
+        __widget__.unparent()
+        
+        __active__ = True
+        
+        numbers = widgets.get_widget("treeview1")
+        left = widgets.get_widget("treeview2")
+        right = widgets.get_widget("treeview3")
 
-def history_changed (history):
+        fixList(numbers, 1)
+        map(fixList, (left, right))
+        numbers.modify_fg(gtk.STATE_INSENSITIVE, gtk.gdk.Color(0,0,0))
+        
+        widgets.signal_autoconnect ({
+            "treeview1_selection_changed": lambda w: self.select_cursor_row(w,1), 
+            "treeview2_selection_changed": lambda w: self.select_cursor_row(w,2), 
+            "treeview3_selection_changed": lambda w: self.select_cursor_row(w,3),
+            "on_treeview2_key_press_event": lambda w,e: self.key_press_event(1,e),
+            "on_treeview3_key_press_event": lambda w,e: self.key_press_event(2,e)
+        })
+        
+        self.board = gamewidget.getWidgets(page_num)[0].view
+        
+        self.board.history.connect("cleared", self.new_history_object)
+        self.board.history.connect("changed", self.history_changed)
+        self.board.connect("shown_changed", self.shown_changed)
+        
+        return __widget__
     
-    if not history.moves: return
+    def select_cursor_row (self, tree, col):
+        iter = tree.get_selection().get_selected()[1]
+        if iter == None: return
+        else: sel = tree.get_model().get_path(iter)[0]
+        self.board.shown = sel*2+col-1
 
-    if len(history) % 2 == 0:
-        num = str(int(len(history)/2))+"."
-        idle_add(numbers.get_model().append, [num])
-
-    view = len(history) & 1 and right or left
-    notat = toSAN(history[-2], history[-1], history.moves[-1])
-
-    def todo():
-        view.get_model().append([notat])
-        if board.shown < len(history):
-            return
-        shown = len(history)-1
+    def key_press_event (self, col, event):
+        if event.keyval in leftkeys and col == 2:
+            shown = self.board.shown - 1
+            w = left
+        elif event.keyval in rightkeys and col == 1:
+            shown = self.board.shown + 1
+            w = right
+        else: return
         row = int((shown-1) / 2)
-        view.get_selection().select_iter(view.get_model().get_iter(row))
-        other = shown & 1 and right or left
-        other.get_selection().unselect_all()
-
-    idle_add(todo)
-    
-    if board.shown < len(history):
-        return
-    idle_add(widgets.get_widget("panel").get_vscrollbar().set_value,
-            numbers.get_allocation().height)
-
-def shown_changed (board, shown):
-    if shown <= 0:
         def todo():
-            left.get_selection().unselect_all()
-            right.get_selection().unselect_all()
+            w.set_cursor((row,))
+            w.grab_focus()
         idle_add(todo)
-        return
+
+    def new_history_object (self, history):
+        def helper():
+            left.get_model().clear()
+            right.get_model().clear()
+            numbers.get_model().clear()
+        gobject.idle_add(helper)
+
+    def history_changed (self, history):
+        
+        if not history.moves: return
     
-    col = shown & 1 and left or right
-    other = shown & 1 and right or left
-    row = int((shown-1) / 2)
-    def todo():
-        col.get_selection().select_iter(col.get_model().get_iter(row))
-        numbers.scroll_to_cell((row,))
-        other.get_selection().unselect_all()
-    idle_add(todo)
+        if len(history) % 2 == 0:
+            num = str(int(len(history)/2))+"."
+            idle_add(numbers.get_model().append, [num])
+    
+        view = len(history) & 1 and right or left
+        notat = toSAN(history[-2], history[-1], history.moves[-1])
+    
+        def todo():
+            view.get_model().append([notat])
+            if self.board.shown < len(history):
+                return
+            shown = len(history)-1
+            row = int((shown-1) / 2)
+            view.get_selection().select_iter(view.get_model().get_iter(row))
+            other = shown & 1 and right or left
+            other.get_selection().unselect_all()
+    
+        idle_add(todo)
+        
+        if self.board.shown < len(history):
+            return
+        idle_add(widgets.get_widget("panel").get_vscrollbar().set_value,
+                numbers.get_allocation().height)
+
+    def shown_changed (self, board, shown):
+        if shown <= 0:
+            def todo():
+                left.get_selection().unselect_all()
+                right.get_selection().unselect_all()
+            idle_add(todo)
+            return
+        
+        col = shown & 1 and left or right
+        other = shown & 1 and right or left
+        row = int((shown-1) / 2)
+        def todo():
+            col.get_selection().select_iter(col.get_model().get_iter(row))
+            numbers.scroll_to_cell((row,))
+            other.get_selection().unselect_all()
+        idle_add(todo)
