e736accfdac139518f573449f195a6e027653efc
Thomas Dybdahl Ahle <thomas@ahle.dk>
2006-11-15 20:00:32 +0000

    We should also be able to load http and ftp and scp uris

---
+++ Utils/protoopen.py
@@ -0,0 +1,20 @@
+import urllib
+
+def protoopen (uri):
+    """ Function for opening many things """
+    
+    uri = urllib.url2pathname(uri) # escape special chars
+    uri = uri.strip('\r\n\x00') # remove \r\n and NULL
+    
+    s = uri.find("://")
+    protocol = uri[:s]
+    path = uri[s+3:]
+    
+    print uri
+    
+    if protocol == "file":
+        return file(path)
+    if protocol == "http":
+        return urllib.urlopen(uri)
+    if protocol == "ftp":
+        pass # We can use the gnome keymanager for finding ftp keys
