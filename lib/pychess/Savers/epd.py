4f0afd5654f7b654a6899d4c8114f6462953b407
Thomas Dybdahl Ahle <thomas@ahle.dk>
2006-12-21 14:10:19 +0000

    # FIXED: Issue 108 # ADDED: Suport for fifty moves rule, and count of fullmoves in epd

--- lib/pychess/Savers/epd.py
+++ lib/pychess/Savers/epd.py
@@ -41,7 +41,8 @@ def save (file, game):
     file.write(history.curCol() == WHITE and "w" or "b")
     file.write(" ")
     
-    if history[-1].castling == 0:
+    if not sum([history[-1].castling & c for c in \
+            (WHITE_OO, WHITE_OOO, BLACK_OO, BLACK_OOO)]):
         file.write("-")
     else:
         if history[-1].castling & WHITE_OO:
@@ -58,7 +59,14 @@ def save (file, game):
     	file.write(repr(history[-1].enpassant))
     else:
 	    file.write("-")
-
+    file.write(" ")
+    
+    file.write(str(history[-1].fifty))
+    file.write(" ")
+    
+    fullmove = len(history)/2+1
+    file.write(str(fullmove))
+    
     #Closing the file prevents us from using StringIO
     #file.close()
     
@@ -77,9 +85,11 @@ class EpdFile (ChessFile):
         if not history: history = History(mvlist=False)
         else: history.reset(mvlist=False)
         
-        data = self.games[gameno].split(" ")
-        if len(data) < 4:
-            return
+        data = self.games[gameno].split()
+        if len(data) < 5:
+            # It is actually in six parts, the sixth being the fullmove number
+            # We don't use that one though, so no need to fail if it is missing
+            raise ValueError, "Bad epd string "+ repr(self.games[gameno])
         
         rows = []
         for row in data[0].split("/"):
@@ -129,6 +139,8 @@ class EpdFile (ChessFile):
         for char in data[2]:
             if char in dic:
                 history[-1].castling |= dic[char]
+        
+        history[-1].fifty = int(data[4])
         
         if len(history) > 1:
             history.emit("changed")
