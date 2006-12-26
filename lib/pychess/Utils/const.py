7452db419c788333b3784b978aa05fd5e0c3c59f
Thomas Dybdahl Ahle <thomas@ahle.dk>
2006-12-26 21:40:55 +0000

    # FIXED: Might fix issue 121. Isn't tested enough. Going on vacation. # CHANGED: A little clean up in const.py

--- lib/pychess/Utils/const.py
+++ lib/pychess/Utils/const.py
@@ -26,33 +26,48 @@ def prefix (subpath):
 
 ###
 
+# Hint modes
 HINT, SPY = range(2)
 reprMode = ["hint", "spy"]
 
+# Player types
 LOCAL, ARTIFICIAL, REMOTE = range(3)
 
+# Engine strengths
+EASY, INTERMEDIATE, EXPERT = range(3)
+
+# Engine modes
+NORMAL, ANALYZING, INVERSE_ANALYZING = range(3)
+
+# Algebraic notation types: Short, Long, Figure and Simpe
 SAN, LAN, FAN, AN = range(4)
 
+# Player colors
 WHITE, BLACK = range(2)
-
 reprColor = ["White", "Black"]
 
+# AlphaBeta search value types
 hashfALPHA, hashfBETA, hashfEXACT = range(3)
 
+# Game states
 RUNNING, DRAW, WHITEWON, BLACKWON = range(4)
-
-RESIGNATION, FLAG_CALL, DRAW_OFFER = range(3)
-
 reprResult = ["*", "1/2-1/2", "1-0", "0-1"]
 
+# Castling states (logarithmic)
+WHITE_OO, WHITE_OOO, BLACK_OO, \
+    BLACK_OOO, WHITE_CASTLED, BLACK_CASTLED = map(lambda x: 2**x, range(6))
+
+# Extended game states
 DRAW_REPITITION, DRAW_50MOVES, DRAW_STALEMATE, DRAW_AGREE, DRAW_INSUFFICIENT, \
     WON_RESIGN, WON_CALLFLAG, WON_MATE = range(8)
 
+# Player actions
+RESIGNATION, FLAG_CALL, DRAW_OFFER = range(3)
+
+# Piece types
 KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN = range(6)
-
-reprSign = ["K", "Q", "R", "B", "N", "P"]
 reprPiece = ["King", "Queen", "Rook", "Bishop", "Knight", "Pawn"]
-chr2Sign = {"k":KING, "q": QUEEN, "r": ROOK, "b": BISHOP, "n": KNIGHT, "p":PAWN}
 
-WHITE_OO, WHITE_OOO, BLACK_OO, \
-    BLACK_OOO, WHITE_CASTLED, BLACK_CASTLED = map(lambda x: 2**x, range(6))
+# Piece signs
+reprSign = ["K", "Q", "R", "B", "N", "P"]
+chr2Sign = {"k":KING, "q": QUEEN, "r": ROOK, "b": BISHOP, "n": KNIGHT, "p":PAWN}
