d015341aa9ce7d3878005f5b700112e4d5939e1c
Bajusz Tamás <gbtami@users.noreply.github.com>
2007-01-16 00:05:04 +0000

    anhanced move generation TestCase

--- testing/validator.py
+++ testing/validator.py
@@ -4,42 +4,43 @@ import __builtin__
 __builtin__.__dict__['_'] = lambda s: s
 
 from pychess.Savers.epd import load
-from pychess.Utils.validator import findMoves2 
+from pychess.Utils.validator import findMoves2, validate
 
 
 class FindMovesTestCase(unittest.TestCase):
     """Move generator test using perftsuite.epd from
        http://www.albert.nu/programs/sharper/perft.htm"""
-
+    
+    testDepth = 2
+    
     def perft(self, board, depth):
         if depth == 0:
             self.count += 1
             return
         for move in findMoves2(board):
+            # use this only if test failes
+            #print move
+            #if not validate(move, board):
+                #print board, move
+                #return
             self.perft(board.move(move), depth-1)
 
     def setUp(self):
         f = open('gamefiles/perftsuite.epd')
-        epd = load(f)
-        self.board = epd.loadToHistory(1, 0)[0]
+        data = f.readlines()
+        f.seek(0)
+        self.epd = load(f)
+        self.positions = [pos.split(";") for pos in data]
  
-    def testPerft_1(self):
-        """Testing move generator with depth 1"""
-        self.count = 0
-        self.perft(self.board, 1)
-        self.assertEqual(self.count, 48)
-
-    def testPerft_2(self):
-        """Testing move generator with depth 2"""
-        self.count = 0
-        self.perft(self.board, 2)
-        self.assertEqual(self.count, 2039)
-
-    def testPerft_3(self):
-        """Testing move generator with depth 3 (this takes some minutes!)"""
-        self.count = 0
-        self.perft(self.board, 3)
-        self.assertEqual(self.count, 97862)
+    def testPerft(self):
+        """Testing move generator with several positions"""
+        for i in range(len(self.positions)):
+            print i+1, int(self.positions[i][self.testDepth][3:])
+            self.board = self.epd.loadToHistory(i, 0)[0]
+            #print self.board
+            self.count = 0
+            self.perft(self.board, self.testDepth)
+            self.assertEqual(self.count, int(self.positions[i][self.testDepth][3:]))
 
 if __name__ == '__main__':
     unittest.main()
