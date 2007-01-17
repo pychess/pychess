c57daa99a5d0df01be4c7e3031fe2c321c00c7b2
Thomas Dybdahl Ahle <thomas@ahle.dk>
2007-01-17 20:39:37 +0000

    # ADDED: New bitboard, gnuchess inspired, Board and move generation modules. This kinda breaks the branch, sorry, as a lot of code depends on the old Board implementation. The code that does work, can be tried by running the test.py script in /. (A script which should soon be turned into a unittest ofcource) The idea is though, that the Old board (with the 2 dimensional Piece lists) should be a subclass of the new LBoard class, providing the needed API.

--- testing/run_tests.py
+++ testing/run_tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 def suite():
-    modules_to_test = ('eval', 'validator', 'hash') 
+    modules_to_test = ("hash", "movegen") 
     tests = unittest.TestSuite()
     for module in map(__import__, modules_to_test):
         tests.addTest(unittest.findTestCases(module))
