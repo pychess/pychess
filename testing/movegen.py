c32cb5e3c6d5d030986589264e3aea4805cc8cb1
Thomas Dybdahl Ahle <thomas@ahle.dk>
2007-01-18 18:05:32 +0000

    #FIXED: Made the unittest for movegeneration succeed - for depth 1 and 2 #ADDED: __repr__ function for LBoard

--- testing/movegen.py
+++ testing/movegen.py
@@ -3,7 +3,7 @@ import unittest
 import __builtin__
 __builtin__.__dict__['_'] = lambda s: s
 
-from pychess.Utils.lmovegen import genAllMoves
+from pychess.Utils.lmovegen import genAllMoves, isCheck
 from pychess.Utils.LBoard import LBoard
 
 from pychess.Utils.bitboard import toString
@@ -19,14 +19,18 @@ class FindMovesTestCase(unittest.TestCase):
         if depth == 0:
             self.count += 1
             return
+        #print "\n".join([" "*3*depth+l for l in repr(board).split("\n")])
         for move in genAllMoves(board):
             # use this only if test failes
             #print move
             #if not validate(move, board):
                 #print board, move
                 #return
-            #print move >> 12, reprCord[(move >> 6) & 63], reprCord[move & 63]
             board.applyMove(move)
+            if isCheck(board, 1-board.color):
+                board.popMove()
+                continue
+            #print " "*3*depth, move >> 12, reprCord[(move >> 6) & 63], reprCord[move & 63]
             self.perft(board, depth-1)
             board.popMove()
     
@@ -41,20 +45,24 @@ class FindMovesTestCase(unittest.TestCase):
         """Testing move generator with several positions"""
         board = LBoard ()
         for i, (pos, depths) in enumerate(self.positions):
-            print i, "/", len(self.positions)
+            print i+1, "/", len(self.positions)
             
             board.applyFen(pos)
-            print toString(board.boards[WHITE][PAWN])
             hash = board.hash
+            print board
+            self.assertFalse(isCheck(board, board.color))
             
             for depth, suposedMoveCount in enumerate(depths):
                 if depth+1 > MAXDEPTH: break
                 self.count = 0
                 print "searching depth %d for %d moves" % (depth+1, suposedMoveCount)
                 self.perft (board, depth+1)
+                self.assertEqual(board.hash, hash)
+                #print self.count
                 self.assertEqual(self.count, suposedMoveCount)
-                
-            self.assertEqual(board.hash, depths)
-    
+            
+            
+            break
+            
 if __name__ == '__main__':
     unittest.main()
