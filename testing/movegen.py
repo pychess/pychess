bb0cc4f282ed6b2805e266dba051962574e33ead
Thomas Dybdahl Ahle <thomas@ahle.dk>
2007-01-18 15:02:57 +0000

    #FIXED: Made the unittest for movegeneration run, even though it fails

--- testing/movegen.py
+++ testing/movegen.py
@@ -4,12 +4,16 @@ import __builtin__
 __builtin__.__dict__['_'] = lambda s: s
 
 from pychess.Utils.lmovegen import genAllMoves
+from pychess.Utils.LBoard import LBoard
+
+from pychess.Utils.bitboard import toString
+from pychess.Utils.const import WHITE, PAWN, reprCord
+
+MAXDEPTH = 3
 
 class FindMovesTestCase(unittest.TestCase):
     """Move generator test using perftsuite.epd from
        http://www.albert.nu/programs/sharper/perft.htm"""
-    
-    testDepth = 2
     
     def perft(self, board, depth):
         if depth == 0:
@@ -21,24 +25,36 @@ class FindMovesTestCase(unittest.TestCase):
             #if not validate(move, board):
                 #print board, move
                 #return
-            self.perft(board.move(move), depth-1)
-
+            #print move >> 12, reprCord[(move >> 6) & 63], reprCord[move & 63]
+            board.applyMove(move)
+            self.perft(board, depth-1)
+            board.popMove()
+    
     def setUp(self):
-        f = open('gamefiles/perftsuite.epd')
-        data = f.readlines()
-        f.seek(0)
-        self.epd = load(f)
-        self.positions = [pos.split(";") for pos in data]
- 
-    def testPerft(self):
+        self.positions = []
+        for line in open('gamefiles/perftsuite.epd'):
+            parts = line.split(";")
+            depths = [int(s[3:].rstrip()) for s in parts[1:]]
+            self.positions.append( (parts[0], depths) )
+    
+    def testMovegen(self):
         """Testing move generator with several positions"""
-        for i, pos in enumerate(self.positions):
-            print i+1, int(pos[self.testDepth][3:])
-            self.board = self.epd.loadToHistory(i, 0)[0]
-            #print self.board
-            self.count = 0
-            self.perft(self.board, self.testDepth)
-            self.assertEqual(self.count, int(pos[self.testDepth][3:]))
-
+        board = LBoard ()
+        for i, (pos, depths) in enumerate(self.positions):
+            print i, "/", len(self.positions)
+            
+            board.applyFen(pos)
+            print toString(board.boards[WHITE][PAWN])
+            hash = board.hash
+            
+            for depth, suposedMoveCount in enumerate(depths):
+                if depth+1 > MAXDEPTH: break
+                self.count = 0
+                print "searching depth %d for %d moves" % (depth+1, suposedMoveCount)
+                self.perft (board, depth+1)
+                self.assertEqual(self.count, suposedMoveCount)
+                
+            self.assertEqual(board.hash, depths)
+    
 if __name__ == '__main__':
     unittest.main()
