510baa66f74241694b013da74a2d98fc2969d251
Bajusz Tamás <gbtami@users.noreply.github.com>
2007-01-29 10:20:54 +0000

    on castling validation pawn pointing move validation needs pawn color instead of board color MAXDEPTH=3 unittest passed

--- testing/validator.py
+++ testing/validator.py
@@ -3,15 +3,15 @@ import unittest
 import __builtin__
 __builtin__.__dict__['_'] = lambda s: s
 
+from pychess.Utils.const import *
 from pychess.Savers.epd import load
 from pychess.Utils.validator import findMoves2, validate
 
+MAXDEPTH = 3
 
 class FindMovesTestCase(unittest.TestCase):
     """Move generator test using perftsuite.epd from
        http://www.albert.nu/programs/sharper/perft.htm"""
-    
-    testDepth = 2
     
     def perft(self, board, depth):
         if depth == 0:
@@ -23,7 +23,12 @@ class FindMovesTestCase(unittest.TestCase):
             #if not validate(move, board):
                 #print board, move
                 #return
-            self.perft(board.move(move), depth-1)
+            if board[move.cord0].sign == PAWN and move.cord1.y in (0,7):
+                for sign in (QUEEN, ROOK, BISHOP, KNIGHT):
+                    move.promotion = sign
+                    self.perft(board.move(move), depth-1)
+            else:
+                self.perft(board.move(move), depth-1)
 
     def setUp(self):
         f = open('gamefiles/perftsuite.epd')
@@ -35,12 +40,12 @@ class FindMovesTestCase(unittest.TestCase):
     def testPerft(self):
         """Testing move generator with several positions"""
         for i in range(len(self.positions)):
-            print i+1, int(self.positions[i][self.testDepth][3:])
+            print i+1, int(self.positions[i][MAXDEPTH][3:])
             self.board = self.epd.loadToHistory(i, 0)[0]
             #print self.board
             self.count = 0
-            self.perft(self.board, self.testDepth)
-            self.assertEqual(self.count, int(self.positions[i][self.testDepth][3:]))
+            self.perft(self.board, MAXDEPTH)
+            self.assertEqual(self.count, int(self.positions[i][MAXDEPTH][3:]))
 
 if __name__ == '__main__':
     unittest.main()
