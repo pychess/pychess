4f5e85ac1eee5dc1074f7ff6f70d357526c05eff
Thomas Dybdahl Ahle <thomas@ahle.dk>
2007-03-01 17:04:52 +0000

    # Added validateMove test (only tests true moves) # Updated eval test to 0.7

--- testing/run_tests.py
+++ testing/run_tests.py
@@ -1,7 +1,7 @@
 import unittest
 
 def suite():
-    modules_to_test = ("validator","fen") 
+    modules_to_test = ("movegen","fen","eval") 
     tests = unittest.TestSuite()
     for module in map(__import__, modules_to_test):
         tests.addTest(unittest.findTestCases(module))
