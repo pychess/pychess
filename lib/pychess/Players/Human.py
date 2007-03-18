d40dd5a39860e0342e2a05ff980d77caee16aad8
Thomas Dybdahl Ahle <thomas@ahle.dk>
2007-03-18 22:19:35 +0000

    # ADDED: You can now accept challenges and seeks on fics. # CHANGED: Chessclock now only starts after both players have made a move, to make the model fics compitable # ADDED: Better support for offers on fics like takeback

--- lib/pychess/Players/Human.py
+++ lib/pychess/Players/Human.py
@@ -2,18 +2,19 @@ import gtk
 
 from threading import Condition
 from gobject import GObject
+from Queue import Queue
 
 from Player import Player, PlayerIsDead
 from pychess.Utils.const import LOCAL, DRAW_OFFER, RESIGNATION, FLAG_CALL
 
 class Human (Player):
     __type__ = LOCAL
+    
     def __init__ (self, board, color):
         GObject.__init__(self)
         
-        self.move = None
         self.gamemodel = board.view.model
-        self.cond = Condition()
+        self.queue = Queue()
         self.color = color
         self.board = board
         self.conid = [board.connect("piece_moved", self.piece_moved)]
@@ -28,55 +29,71 @@ class Human (Player):
     def piece_moved (self, board, move):
         if self.gamemodel.boards[-1].color != self.color:
             return
-        self.cond.acquire()
-        self.move = move
-        self.cond.notify()
-        self.cond.release()
+        self.queue.put(move)
     
     def emit_action (self, action):
         if self.gamemodel.boards[-1].color != self.color:
             return
-        self.emit("action", action)
+        self.emit("action", action, 0)
     
     def makeMove (self, gamemodel):
+    
         self.board.locked = False
-        self.cond.acquire()
-        while not self.move:
-            self.cond.wait()
+        item = self.queue.get(block=True)
+        
         self.board.locked = True
-        if self.move == "del":
-            self.cond.release()
+        if item == "del":
             raise PlayerIsDead
-        move = self.move
-        self.move = None
-        self.cond.release()
-        return move
-
-    def offerDraw (self):
-        d = gtk.MessageDialog(type=gtk.MESSAGE_QUESTION, buttons=gtk.BUTTONS_YES_NO)
-        d.set_markup(_("<big><b>You've got a draw offer. Accept?</b></big>"))
-        d.format_secondary_text(_("Your opponent has offered you a draw. If you accept it the game will end with score 1/2 - 1/2."))
+        
+        return item
+    
+    def _offer (self, action, param, title, desciprtion):
+        d = gtk.MessageDialog (
+                type = gtk.MESSAGE_QUESTION, buttons = gtk.BUTTONS_YES_NO)
+        d.set_markup (title)
+        d.format_secondary_text (description)
         result = d.run()
         d.hide()
         if result == gtk.RESPONSE_YES:
-            self.emit("action", DRAW_OFFER)
-
+            self.emit("action", action, param)
+    
+    def offerDraw (self):
+        self._offer ( DRAW_OFFER, 0,
+                _("<big><b>You've got a draw offer. Accept?</b></big>"),
+                _("Your opponent has offered you a draw. If you accept it the game will end with score 1/2 - 1/2.")
+        )
+    
+    def offerAbort (self):
+        self._offer ( ABORT_OFFER, 0,
+                _("<big><b>You've got an abort offer. Accept?</b></big>"),
+                _("Your opponent has offered you to abort the game. If you accept, the game will end with no rating change.")
+        )
+    
+    def offerAdjourn (self):
+        self._offer ( ADJOURN_OFFER, 0,
+                _("<big><b>You've got an adjourn offer. Accept?</b></big>"),
+                _("Your opponent has offered you to adjourn the game. If you accept, the game will adjourned, and you can later resume it (If your opponent is online and willing).")
+        )
+    
+    def offerTakeback (self, toPly):
+        self._offer ( TAKEBACK_OFFER, toPly,
+                _("<big><b>Your opponent wants to undo. Accept?</b></big>"),
+                _("Your opponent wants to undo back to halfmove %s.. If you accept, the game will continue from the earlier position.")
+        )
+    
     def setName (self, name):
         self.name = name
-
+    
     def __repr__ (self):
         #TODO: Get name from preferences or accountname
         #(probably preferences, as accountname would give problems in pvp games)
         return self.name
-
+    
     def kill (self):
-        self.cond.acquire()
         for id in self.conid:
             if self.board.handler_is_connected(id):
                 self.board.disconnect(id)
-        self.move = "del"
-        self.cond.notify()
-        self.cond.release()
+        self.queue.put("del")
         
     def setBoard (self, fen):
         # BoardControl, from which we are reciving moves, will be set by others
