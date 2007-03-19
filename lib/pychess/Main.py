1714eea56d3b48845797c5f08bd0717e8792105b
Thomas Dybdahl Ahle <thomas@ahle.dk>
2007-03-19 16:03:34 +0000

    # FIXED: A bug that caused pychess to crash when resigning. # FIXED: Resigning or calling flag wasn't reported back to the fics server.

--- lib/pychess/Main.py
+++ lib/pychess/Main.py
@@ -161,13 +161,25 @@ class GladeHandlers:
                 DRAW_CALLFLAG: _("as both players ran out of time"),
                 DRAW_STALEMATE: _("because of stalemate"),
                 DRAW_AGREE: _("as the players agreed to"),
+                DRAW_ADJUDICATION: _("as decided by an admin"),
+                DRAW_LENGTH: _("as game exceed the max length"),
+                
                 WON_RESIGN: _("as opponent resigned"),
                 WON_CALLFLAG: _("as opponent ran out of time"),
                 WON_MATE: _("on a mate"),
                 WON_DISCONNECTION: _("as opponent disconnected"),
+                WON_ADJUDICATION:  _("as decided by an admin"),
+                
                 ADJOURNED_LOST_CONNECTION: _("as a player lost connection"),
                 ADJOURNED_AGREEMENT: _("as the players agreed to"),
-                REASON_ABORTED: "",
+                ADJOURNED_SERVER_SHUTDOWN: _("as the server was shut down"),
+                
+                ABORTED_ADJUDICATION: _("as decided by an admin"),
+                ABORTED_AGREEMENT: _("as the players agreed to"),
+                ABORTED_COURTESY: _("courtesy by a player"),
+                ABORTED_EARLY: _("in the early phase of the game"),
+                ABORTED_SERVER_SHUTDOWN: _("as the server was shut down"),
+                
                 UNKNOWN_REASON: _("by no known reason")
             }[reason]
             gmwidg.status("%s %s" % (m1,m2), idle_add=True)
