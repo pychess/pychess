bc69908a0aae3fe6737abb31907455dd48517883
Thomas Dybdahl Ahle <thomas@ahle.dk>
2007-06-16 16:53:29 +0000

    FIXED: 132 CHANGED: gstreamer should not make a new object each time a sound is played

--- lib/pychess/System/gstreamer.py
+++ lib/pychess/System/gstreamer.py
@@ -1,53 +1,30 @@
-# GstPlayer is borrowed from istanbul
 
 import pygst
 pygst.require('0.10')
 import gst
+from gobject import GObject, SIGNAL_RUN_FIRST, TYPE_NONE
 
-class GstPlayer:
-    def __init__(self, uri):
-        self.playing = False
-        self.player = gst.element_factory_make("playbin", "player")
-        self.on_eos = False
-        self.player.set_property('uri', uri)
-        
-        self.bus = self.player.get_bus()
-        self.bus.enable_sync_message_emission()
-        self.bus.add_signal_watch()
-        self.bus.connect('sync-message::element', self.on_sync_message)
-        self.bus.connect('message', self.on_message)
+class Player (GObject):
+    __gsignals__ = {'end': (SIGNAL_RUN_FIRST, TYPE_NONE, ())}
     
-    def on_sync_message(self, bus, message):
-        if message.structure is None: return
-        if message.structure.get_name() == 'prepare-xwindow-id':
-            message.src.set_property('force-aspect-ratio', True)
+    def __init__(self):
+        GObject.__init__(self)
+        self.player = gst.element_factory_make("playbin")
+        self.player.get_bus().add_watch(self.on_message)
     
     def on_message(self, bus, message):
         if message.type == gst.MESSAGE_ERROR:
-            err, debug = message.parse_error()
-            print "Error: %s" % err, debug
-            gerror, debug = message.parse_error()
-            m = gtk.MessageDialog(None, gtk.DIALOG_MODAL, gtk.MESSAGE_ERROR,
-                gtk.BUTTONS_CLOSE, _("Error playing sound"))
-            m.format_secondary_text(
-                _('There was an error playing: %s\n\nDebug Information:\n%s') % (
-                    gerror, debug))
-            self.playing = False
-        
+            gsterror, message = message.parse_error()
+            print message
         elif message.type == gst.MESSAGE_EOS:
-            self.playing = False
-            
-    def play(self):
-        gst.info("playing player")
+            self.emit("end")
+        return True
+    
+    def play(self, uri):
+        self.player.set_state(gst.STATE_NULL)
+        self.player.set_property("uri", uri)
         self.player.set_state(gst.STATE_PLAYING)
-        self.playing = True
 
-from threading import Lock
-lock = Lock()
+player = Player()
 def playSound (uri):
-    # If you run this to many time, it'll crash the program with some strange
-    # GThread-ERROR **: file gthread-posix.c: line 356 (): error
-    lock.acquire()
-    instance = GstPlayer(uri)
-    instance.play()
-    lock.release()
+    player.play(uri)
