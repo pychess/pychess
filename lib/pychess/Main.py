c9f4d3e5ad619907f4579d9749657df9bf117f47
Thomas Dybdahl Ahle <thomas@ahle.dk>
2007-09-13 18:28:13 +0000

    Updated version name to Philidor 0.8beta1

--- lib/pychess/Main.py
+++ lib/pychess/Main.py
@@ -44,7 +44,7 @@ def makeLogDialogReady ():
 
 def makeAboutDialogReady ():
     clb = window["aboutdialog1"].get_child().get_children()[1].get_children()[2]
-    window["aboutdialog1"].set_version(VERSION)
+    window["aboutdialog1"].set_version(VERSION_NAME+" "+VERSION)
     def callback(button, *args):
         window["aboutdialog1"].hide()
         return True
