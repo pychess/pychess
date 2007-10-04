5fbbad4013e6de64f1c0963182cc3fde5752b6bb
Thomas Dybdahl Ahle <thomas@ahle.dk>
2007-10-04 17:18:55 +0000

    Fixed: Errors should be in the logdialog

--- lib/pychess/ic/DisconnectManager.py
+++
@@ -1,8 +0,0 @@
-
-class DisconnectManager:
-    def __init__ (self, connection):
-        self.connection = connection
-    
-    def disconnect (self):
-        print >> self.connection.client, "quit"
-        self.connection.disconnect()
