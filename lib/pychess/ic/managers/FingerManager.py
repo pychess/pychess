31310c7944c507f4167719f782f9bef44e2d3952
Thomas Dybdahl Ahle <thomas@ahle.dk>
2007-11-17 00:07:54 +0000

    The ratingtable should update after game ends

--- lib/pychess/ic/managers/FingerManager.py
+++ lib/pychess/ic/managers/FingerManager.py
@@ -2,8 +2,6 @@
 from threading import RLock
 from gobject import *
 
-types = "(Blitz|Lightning|Standard)"
-
 types = "(blitz|lightning|standard)"
 rated = "(rated|unrated)"
 colors = "(?:\[(white|black)\]\s?)?"
@@ -15,14 +13,24 @@ class FingerManager (GObject):
     
     __gsignals__ = {
         'fingeringFinished' : (SIGNAL_RUN_FIRST, TYPE_NONE, (object, str, object)),
+        'ratingAdjusted' : (SIGNAL_RUN_FIRST, TYPE_NONE, (str, str)),
     }
     
     def __init__ (self, connection):
         GObject.__init__(self)
         self.connection = connection
+        
         self.connection.expect_fromto (self.onFinger,
                 "Finger of %s:" % names,
                 "Timeseal +: +(On|Off)")
+        
+        # We don't use this. Rather we use BoardManagers "gameEnded", after
+        # which we do a refinger. This is to ensure not only rating, but also
+        # wins/looses/draws are updated
+        #self.connection.expect(self.onRatingAdjust,
+        #        "%s rating adjustment: (\d+) --> (\d+)" % types
+        # Notice if you uncomment this: The expression has to be compiled with
+        # re.IGNORECASE, or the first letter of 'type' must be capital
     
     def onFinger (self, matchlist):
         name, title = matchlist[0].groups()
@@ -48,6 +56,11 @@ class FingerManager (GObject):
             # elif parts[:4] == ["%", "of", "life", "online"]:
         
         self.emit ("fingeringFinished", ratings, email, timeonline)
+    
+    def onRatingAdjust (self, match):
+        # Notice: This is only recived for us, not for other persons we finger
+        type, old, new = match.groups()
+        self.emit("ratingAdjusted", type, new)
     
     def finger (self, user):
         print >> self.connection.client, "finger %s /sbl r" % user
