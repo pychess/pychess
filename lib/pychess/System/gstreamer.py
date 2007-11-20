3a1ac9820aaae62e89315fae0ed6647fb6f74ad8
Thomas Dybdahl Ahle <thomas@ahle.dk>
2007-11-20 16:08:16 +0000

    Setting "Use Sound" checkbutton insensitive when gstreamer see errors Adding assert in Board.py to help indentify nullpiece-move bugs Updating versionname to beta2

--- lib/pychess/System/gstreamer.py
+++ lib/pychess/System/gstreamer.py
@@ -1,47 +1,43 @@
 
+from threading import Lock
+from Queue import Queue
+
 from gobject import GObject, SIGNAL_RUN_FIRST, TYPE_NONE
+import pygst
+pygst.require('0.10')
+import gst
+
+from Log import log
 
 class Player (GObject):
-    __gsignals__ = {'end': (SIGNAL_RUN_FIRST, TYPE_NONE, ())}
+    __gsignals__ = {
+        'end': (SIGNAL_RUN_FIRST, TYPE_NONE, ()),
+        'error': (SIGNAL_RUN_FIRST, TYPE_NONE, (object,))
+    }
     
     def __init__(self):
         GObject.__init__(self)
         self.player = gst.element_factory_make("playbin")
-        self.player.get_bus().add_watch(self.on_message)
+        self.player.get_bus().add_watch(self.onMessage)
+        self.playLock = Lock()
     
-    def on_message(self, bus, message):
+    def onMessage(self, bus, message):
         if message.type == gst.MESSAGE_ERROR:
-            gsterror, message = message.parse_error()
-            print message
+            self.emit("error", message)
+            simpleMessage, advMessage = message.parse_error()
+            log.warn("Gstreamer error '%s': %s" % (simpleMessage, advMessage))
         elif message.type == gst.MESSAGE_EOS:
             self.emit("end")
         return True
     
-    def play(self, uri):
-        self.player.set_state(gst.STATE_NULL)
-        self.player.set_property("uri", uri)
+    def checkSound(self):
         self.player.set_state(gst.STATE_PLAYING)
-
-def playSound (uri):
-    ensureReady ()
-    lock.acquire()
-    try:
-        player.play(uri)
-    finally:
-        lock.release()
-
-ready = False
-def ensureReady ():
-    global ready
-    if ready:
-        return
-    else: ready = True
     
-    import pygst
-    pygst.require('0.10')
-    global player, lock, gst
-    import gst
-    player = Player()
-    from threading import Lock
-    lock = Lock()
-    
+    def play(self, uri):
+        self.playLock.acquire()
+        try:
+            self.player.set_state(gst.STATE_NULL)
+            self.player.set_property("uri", uri)
+            self.player.set_state(gst.STATE_PLAYING)
+        finally:
+            self.playLock.release()
