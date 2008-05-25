91d0c6f7a217796eced6cd95f717c5e483a1a2a7
Leonardo Gregianin <leogregianin@gmail.com>
2008-05-25 04:06:26 +0000

    translators list save in file

--- translators.py
+++ translators.py
@@ -3,15 +3,20 @@
 
 from urllib import urlopen
 from BeautifulSoup import BeautifulSoup
+import codecs
 
 # Languages sorted by name.
-# Unicode problem az, pl, ro
-langs = ('ar', 'pt_BR', 'bg', 'ca', 'cs', 'da', 'nl',
-         'en_GB', 'et', 'fi', 'fr', 'gl', 'de', 'el',
-         'he', 'hi', 'hu', 'ga', 'it', 'ja', 'nb', 'oc',
-         'fa', 'pt', 'ru', 'sk', 'es', 'sv', 'tr', 'wa', )
+langs = ('az', 'ar', 'pt_BR', 'bg', 'ca', 'cs', 'da', 'nl',
+         'en_GB', 'et', 'fi', 'fr', 'gl', 'de', 'el', 'he',
+         'hi', 'hu', 'ga', 'it', 'ja', 'nb', 'oc', 'fa', 'pl',
+         'pt', 'ro', 'ru', 'sk', 'es', 'sv', 'tr', 'wa', )
 
-print "PyChess is translated into %s languages" % len(langs)
+print "Getting data from Rosetta Launchpad."
+filename = 'TRANSLATORS'
+pychess = ("PyChess is translated into %s languages.\n" % len(langs))
+f = codecs.open(filename, 'a', 'utf-8')
+f.write(u'%s' % pychess)
+f.close()
 
 for lang in langs:
     site = "https://translations.launchpad.net/pychess/trunk/+pots/pychess/%s/+translate" % lang
@@ -20,6 +25,13 @@ for lang in langs:
     names = soup.find("div", {"class":"portletContent"})
     l = soup('title')[0].string
     language = l.split("into ")[1]
-    print u"\n%s translators:" % language
+    ltranslators = u"\n%s translators:\n" % language
+    print ("processing '%s'...") % lang
+    f = codecs.open(filename, 'a', 'utf-8')
+    f.write(u'%s' % ltranslators)
+    f.close()
+    # TODO: getting email address of translators.
     for link in names.findChildren("a"):
-        print link.string
+        f = codecs.open(filename, 'a', 'utf-8')
+        f.write(u'%s\n' % link.string)
+        f.close()
