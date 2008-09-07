cd22e58f42e59884cc50f29d1da4536448a8c91e
Thomas Dybdahl Ahle <thomas@ahle.dk>
2008-09-07 22:23:41 +0000

    Started work on making log dialog more readable

--- lib/pychess/System/SubProcess.py
+++ lib/pychess/System/SubProcess.py
@@ -151,6 +151,7 @@ class SubProcess:
         
         self.defname = os.path.split(path)[1]
         self.defname = self.defname[:1].upper() + self.defname[1:].lower()
+        self.defname = (self.defname, time.strftime("%H:%m:%S"))
         log.debug(path+"\n", self.defname)
         
         self.priority = 15
