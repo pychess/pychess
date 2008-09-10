12ff645c2e033a33c46903619bd32cb8cb731f4f
Thomas Dybdahl Ahle <thomas@ahle.dk>
2008-09-10 15:52:16 +0000

    Reenabled tip of the day Made ICLounge better at remembering its position and size

--- lib/pychess/widgets/tipOfTheDay.py
+++ lib/pychess/widgets/tipOfTheDay.py
@@ -1,37 +1,52 @@
 import gtk.glade, os
 from pychess.System import conf
-from pychess.System.uistuff import GladeWidgets
+from pychess.System import uistuff
 from pychess.System.prefix import addDataPrefix
 from random import randrange
 
-widgets = GladeWidgets("tipoftheday.glade")
+uistuff.cacheGladefile("tipoftheday.glade")
 
-widgets["checkbutton1"].set_active(conf.get("show_tip_at_startup", True))
-widgets["checkbutton1"].connect("toggled",
-    lambda w: conf.set("show_tip_at_startup", w.get_active()))
-
-widgets["close_button"].connect("clicked",
-    lambda w: widgets["window1"].hide())
-widgets["window1"].connect("delete_event",
-    lambda w, a: widgets["window1"].hide() or True)
-
-widgets["back_button"].connect("clicked",
-    lambda w: set_currentIndex(currentIndex-1))
-widgets["forward_button"].connect("clicked",
-    lambda w: set_currentIndex(currentIndex+1))
-
-def show ():
-    set_currentIndex(randrange(len(tips)))
-    widgets["window1"].show()
-
-currentIndex = 0
-def set_currentIndex (value):
-    if len(tips) == 0: return
-    if value < 0: value = len(tips)-1
-    elif value >= len(tips): value = 0
-    global currentIndex
-    currentIndex = value
-    widgets["tipfield"].set_markup(tips[value])
+class TipOfTheDay:
+    
+    @classmethod
+    def _init (cls):
+        cls.widgets = uistuff.GladeWidgets("tipoftheday.glade")
+        
+        uistuff.keepWindowSize("tipoftheday", cls.widgets["window1"],
+                               (320,240), uistuff.POSITION_CENTER)
+        
+        cls.widgets["checkbutton1"].set_active(conf.get("show_tip_at_startup", False))
+        cls.widgets["checkbutton1"].connect("toggled",
+            lambda w: conf.set("show_tip_at_startup", w.get_active()))
+        
+        cls.widgets["close_button"].connect("clicked",
+            lambda w: cls.widgets["window1"].emit("delete-event", None))
+        cls.widgets["window1"].connect("delete_event",
+            lambda w, a: cls.widgets["window1"].hide())
+        
+        cls.widgets["back_button"].connect("clicked",
+            lambda w: cls.set_currentIndex(cls.currentIndex-1))
+        cls.widgets["forward_button"].connect("clicked",
+            lambda w: cls.set_currentIndex(cls.currentIndex+1))
+        
+        cls.currentIndex = 0
+    
+    
+    @classmethod
+    def show (cls):
+        if not hasattr(cls, "widgets"):
+            cls._init()
+        cls.set_currentIndex(randrange(len(tips)))
+        cls.widgets["window1"].show()
+    
+    
+    @classmethod
+    def set_currentIndex (cls, value):
+        if len(tips) == 0: return
+        if value < 0: value = len(tips)-1
+        elif value >= len(tips): value = 0
+        cls.currentIndex = value
+        cls.widgets["tipfield"].set_markup(tips[value])
 
 tips = (
     _("You can start a new game by pressing <b>Game -> New Game</b>"),
