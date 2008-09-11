141fc28defefd77f5d8e5f7d7062c8a8d4ad2844
Thomas Dybdahl Ahle <thomas@ahle.dk>
2008-09-11 17:45:18 +0000

    Made findbar actually work

--- lib/pychess/widgets/LogDialog.py
+++ lib/pychess/widgets/LogDialog.py
@@ -34,6 +34,7 @@ class InformationWindow:
         cls.treeview = gtk.TreeView(gtk.TreeStore(str))
         cls.treeview.append_column(gtk.TreeViewColumn("", gtk.CellRendererText(), text=0))
         cls.treeview.set_headers_visible(False)
+        cls.treeview.get_selection().set_mode(gtk.SELECTION_BROWSE)
         sw.add(cls.treeview)
         cls.pages = gtk.Notebook()
         cls.pages.set_show_tabs(False)
@@ -44,8 +45,9 @@ class InformationWindow:
         
         def selectionChanged (selection):
             treestore, iter = selection.get_selected()
-            child = cls.pathToPage[treestore.get_path(iter)]["child"]
-            cls.pages.set_current_page(cls.pages.page_num(child))
+            if iter:
+                child = cls.pathToPage[treestore.get_path(iter)]["child"]
+                cls.pages.set_current_page(cls.pages.page_num(child))
         cls.treeview.get_selection().connect("changed", selectionChanged)
     
     @classmethod
@@ -68,7 +70,8 @@ class InformationWindow:
                 str(LOG))
             cls.tagToTime[tag] = timestamp
         
-        message = unicode(message, "utf-8", 'replace')
+        if type(message) == str:
+            message = unicode(message, "utf-8", 'replace')
         textview.get_buffer().insert_with_tags_by_name(
             textview.get_buffer().get_end_iter(), message, str(importance))
     
@@ -94,13 +97,26 @@ class InformationWindow:
         
         findbar = widgets["findbar"]
         findbar.hide()
-        # Make searchEntry share height with the buttons
+        # Make searchEntry and "out of label" share height with the buttons
         widgets["prevButton"].connect("size-allocate", lambda w, alloc:
-                widgets["searchEntry"].set_size_request(-1, alloc.height))
+                widgets["searchEntry"].set_size_request(-1, alloc.height) or
+                widgets["outofLabel"].set_size_request(-1, alloc.height-2))
+        
+        # Make "out of label" more visually distinct
+        uistuff.makeYellow(widgets["outofLabel"])
+        widgets["outofLabel"].hide()
+        
+        widgets["closeButton"].connect("clicked", lambda w:
+                                       widgets["findbar"].hide())
         
         # Connect showing/hiding of the findbar
         cls.window.connect("key-press-event", cls.onTextviewKeypress, widgets)
         widgets["findbar"].connect("key-press-event", cls.onFindbarKeypress)
+        
+        widgets["searchEntry"].connect("changed", cls.onSearchChanged, widgets)
+        
+        widgets["prevButton"].connect("clicked", lambda w: cls.searchJump(-1, widgets))
+        widgets["nextButton"].connect("clicked", lambda w: cls.searchJump(1, widgets))
         
         
         
@@ -130,6 +146,42 @@ class InformationWindow:
         
         cls._createPage(None, tag[:1])
         return cls._getPageFromTag(tag)
+    
+    @classmethod
+    def onSearchChanged (cls, searchEntry, widgets):
+        pattern = searchEntry.get_text()
+        widgets["outofLabel"].props.visible = bool(pattern)
+        if not pattern:
+            return
+        
+        text = widgets["textview"].get_buffer().props.text.lower()
+        
+        widgets["outofLabel"].hits = []
+        widgets["outofLabel"].searchCurrent = -1
+        i = -len(pattern)
+        while True:
+            i = text.find(pattern, i+len(pattern))
+            if i != -1:
+                widgets["outofLabel"].hits.append(i)
+            else: break
+        
+        cls.searchJump(1, widgets)
+        
+    @classmethod
+    def searchJump (cls, count, widgets):
+        amount = len(widgets["outofLabel"].hits)
+        if not amount:
+            widgets["outofLabel"].set_text("0 %s 0" % _("of"))
+        else:
+            widgets["outofLabel"].searchCurrent += count
+            current = widgets["outofLabel"].searchCurrent % amount
+            widgets["outofLabel"].set_text("%d %s %d" % (current+1, _("of"), amount))
+            goto = widgets["outofLabel"].hits[current]
+            iter0 = widgets["textview"].get_buffer().get_iter_at_offset(goto)
+            length = len(widgets["searchEntry"].get_text())
+            iter1 = widgets["textview"].get_buffer().get_iter_at_offset(goto+length)
+            widgets["textview"].get_buffer().select_range(iter0, iter1)
+            widgets["textview"].scroll_to_iter(iter0, 0.2)
     
     @classmethod
     def onTextviewKeypress (cls, textview, event, widgets):
