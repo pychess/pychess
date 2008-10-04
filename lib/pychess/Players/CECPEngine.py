fbf802cb833816c5821d2b0521d084b2636bc3b8
Bajusz Tamás <gbtami@users.noreply.github.com>
2008-10-04 18:51:55 +0000

    use variant command only for non standard rule variants

--- lib/pychess/Players/CECPEngine.py
+++ lib/pychess/Players/CECPEngine.py
@@ -326,7 +326,7 @@ class CECPEngine (ProtocolEngine):
             self.changeLock.release()
     
     def setOptionVariant (self, variant):
-        if variant.cecp_name != "normal" and variant in variants.values():
+        if variant in variants.values() and not variant.standard_rules:
             assert variant.cecp_name in self.features["variants"], \
                     "%s dosn't support %s variant" % (self, variant.cecp_name)
             self.optionQueue.append("variant %s" % variant.cecp_name)
