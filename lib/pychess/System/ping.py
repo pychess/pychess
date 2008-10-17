34f803a289a01b9da920bcf8608f17dc35d015a1
Thomas Dybdahl Ahle <thomas@ahle.dk>
2008-10-17 11:55:57 +0000

    Moved pinger to new SubProcess system

--- lib/pychess/System/ping.py
+++ lib/pychess/System/ping.py
@@ -7,11 +7,12 @@ import select, signal, re, os, atexit
 from gobject import GObject, SIGNAL_RUN_FIRST
 
 from pychess.System import glock
+from pychess.System.Log import log
 from pychess.System.SubProcess import SubProcess, SubProcessError, searchPath
 from pychess.System.ThreadPool import PooledThread
 from pychess.Utils.const import SUBPROCESS_PTY
 
-class Pinger (GObject, PooledThread):
+class Pinger (GObject):
     """ The recieved signal contains the time it took to get response from the
         server in millisecconds. -1 means that some error occurred """
     
@@ -30,38 +31,47 @@ class Pinger (GObject, PooledThread):
             re.compile("(Destination Host Unreachable)"),
         )
         
-        atexit.register(self.stop)
+        self.restartsOnDead = 3
+        self.deadCount = 0
     
     def start (self):
-        self.subproc = SubProcess(
-                searchPath("ping"), [self.host], env={"LANG":"en"}, type=SUBPROCESS_PTY)
-        PooledThread.start(self)
+        assert not self.subproc
+        self.subproc = SubProcess(searchPath("ping"), [self.host], env={"LANG":"en"})
+        self.conid1 = self.subproc.connect("line", self.__handleLine)
+        self.conid2 = self.subproc.connect("died", self.__handleDead)
     
-    def run (self):
-        while True:
-            try:
-                line = self.subproc.readline()
-            except SubProcessError, e:
-                self.emit("error", ", ".join(e[1]))
-                return
-            
-            match = self.expression.search(line)
-            if match:
-                time, unit = match.groups()
-                time = float(time)
-                if unit == "s":
-                    time *= 1000
-                self.emit("recieved", time)
-            
-            else:
-                for expr in self.errorExprs:
-                    match = expr.search(line)
-                    if match:
-                        self.emit("error", match.groups()[0])
+    def __handleLine (self, subprocess, line):
+        match = self.expression.search(line)
+        if match:
+            time, unit = match.groups()
+            time = float(time)
+            if unit == "s":
+                time *= 1000
+            self.emit("recieved", time)
+        else:
+            for expr in self.errorExprs:
+                match = expr.search(line)
+                if match:
+                    self.emit("error", match.groups()[0])
+    
+    def __handleDead (self, subprocess):
+        if self.deadCount < self.restartsOnDead:
+            log.warn("Pinger died and restarted (%d/%d)\n" %
+                     (self.deadCount+1, self.restartsOnDead),
+                     self.subproc.defname)
+            self.stop()
+            self.start()
+            self.deadCount += 1
+        else:
+            self.emit("error", _("Died"))
+            self.stop()
     
     def stop (self):
-        if not self.subproc: return
-        self.subproc.gentleKill()
+        assert self.subproc
+        exitCode = self.subproc.gentleKill()
+        self.subproc.disconnect(self.conid1)
+        self.subproc.disconnect(self.conid2)
+        self.subproc = None
 
 if __name__ == "__main__":
     pinger = Pinger("google.com")
