cc9f2f8df68a796af75ea23af26d5e012c395abc
Thomas Dybdahl Ahle <thomas@ahle.dk>
2008-10-23 22:05:08 +0000

    A new and better try to fix that index error problem. This should also fix a problem on ubuntu, where the human player is named ",,,"

--- lib/pychess/System/conf_configParser.py
+++ lib/pychess/System/conf_configParser.py
@@ -13,7 +13,6 @@ if not configParser.has_section(section+"_Types"):
     configParser.add_section(section+"_Types")
 atexit.register(lambda: configParser.write(open(path,"w")))
 
-notifiers = {}
 idkeyfuncs = {}
 conid = 0
 
@@ -32,17 +31,13 @@ typeDecode = {
     repr(bool): configParser.getboolean,
 }
 
-def notify_add (key, func):
+def notify_add (key, func, args):
     global conid
-    if key in notifiers:
-        notifiers[key].append(conid)
-    else: notifiers[key] = [conid]
-    idkeyfuncs[conid] = (key, func)
+    idkeyfuncs[conid] = (key, func, args)
     conid += 1
+    return conid
 
 def notify_remove (conid):
-    key = idkeyfuncs[conid][0]
-    del notifiers[key][conid]
     del idkeyfuncs[conid]
 
 def get (key):
@@ -56,9 +51,9 @@ def set (key, value):
     except Exception, e:
         log.error("Unable to save configuration '%s'='%s' because of error: %s %s"%
                 (repr(key), repr(value), e.__class__.__name__, ", ".join(str(a) for a in e.args)))
-    if key in notifiers:
-        for key, func in [idkeyfuncs[id] for id in notifiers[key]]:
-            func (None)
+    for key_, func, args in idkeyfuncs.values():
+        if key_ == key:
+            func (None, *args)
 
 def hasKey (key):
     return configParser.has_option(section, key) and \
