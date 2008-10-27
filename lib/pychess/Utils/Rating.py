93293c354322e0b3b2210c416e985d07e0a6b638
Thomas Dybdahl Ahle <thomas@ahle.dk>
2008-10-27 16:10:44 +0000

    Added mattgatto's patch against losses/draws mismatch

--- lib/pychess/Utils/Rating.py
+++ lib/pychess/Utils/Rating.py
@@ -1,6 +1,6 @@
 class Rating:
-    def __init__(self, type, elo, deviation = 0, wins = 0, draws = 0,
-                                  losses = 0, bestElo = 0, bestTime = 0):
+    def __init__(self, type, elo, deviation = 0, wins = 0, losses = 0,
+                                  draws = 0, bestElo = 0, bestTime = 0):
         self.type = type
         self.elo = elo
         self.deviation = deviation
