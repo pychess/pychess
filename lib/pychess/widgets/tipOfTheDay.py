31324ce1dbc70ed848dc0dd88a13860cf48e326a
Leonardo Gregianin <leogregianin@gmail.com>
2008-11-18 11:13:39 +0000

    more tips

--- lib/pychess/widgets/tipOfTheDay.py
+++ lib/pychess/widgets/tipOfTheDay.py
@@ -49,11 +49,17 @@ class TipOfTheDay:
         cls.widgets["tipfield"].set_markup(tips[value])
 
 tips = (
-    _("You can start a new game by pressing <b>Game -> New Game</b>"),
+    _("You can start a new game by <b>Game</b> > <b>New Game</b>, in New Game window do you can choose <b>Players</b>, <b>Time Control</b> and <b>Chess Variants</b>."),
+    _("You can choose from 8 different difficulties to play against the computer."),
+    _("Chess Variants are like the pieces of the last line will be placed on the board."),
+    _("To save a game <b>Game</b> > <b>Save Game As</b>, give the filename and choose where you want to be saved. At the bottom choose extension type of the file, and <b>Save</b>."),
+    _("Do you can call flag when the clock is with you, <b>Actions</b> > <b>Call Flag</b>."),
+    _("Pressing <b>Ctrl+Z</b> to offer opponent the possible rollback moves."),
+    _("To play on <b>Fullscreen mode</b>, just type <b>F11</b>. Coming back, F11 again."),
+    _("<b>Hint mode</b> analyzing your game, enable this type <b>Ctrl+H</b>."),
+    _("<b>Spy mode</b> analyzing the oponnent game, enable this type <b>Ctrl+Y</b>."),
+    _("You can play chess listening to the sounds of the game, for that, <b>Settings</b> > <b>Preferences</b> > <b>Sound tab</b>, enable <b>Use sounds in PyChess</b> and choose your preferred sounds."),
+    _("Do you can help translate Pychess in your language, <b>Help</b> > <b>Translate Pychess</b>."),
     _("Do you know that it is possible to finish a chess game in just 2 turns?"),
-    _("The number of Shannon as rough estimates of the number of possible bids in the game of chess, which is 10<sup>123</sup> and, as comparison, the number of atoms in the universe is estimated in 4x10<sup>78</sup> and 6x10<sup>79</sup>."),
-    _("A tip a day keeps the doctor away"),
-    _("Do you can help translate Pychess in your language, pressing <b>Help -> Translate Pychess</b>"),
-    _("Do you can call flag when the clock is with you, pressing <b>Actions -> Call Flag</b>"),
-    _("If you want to win all your chessmatches..."),
+    _("Do you know that the number of possible chess games exceeds the number of atoms in the Universe?"),
 )
