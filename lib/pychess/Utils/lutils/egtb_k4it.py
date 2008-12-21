cb9af7d4f75bd951609dd03495edc15694566313
Thomas Dybdahl Ahle <thomas@ahle.dk>
2008-12-21 13:04:15 +0000

    Fixed a bug that made the egtb only work for white. Also added a cache to it, so we don't bang too much on the server.

--- lib/pychess/Utils/lutils/egtb_k4it.py
+++ lib/pychess/Utils/lutils/egtb_k4it.py
@@ -4,8 +4,9 @@ import re
 from pychess.Utils.lutils.lmove import newMove, FILE, RANK
 from pychess.Utils.const import *
 from pychess.Utils.lutils.bitboard import bitLength
+from pychess.System.Log import log
 
-URL = "http://www.k4it.de/egtb/fetch.php?"
+URL = "http://www.k4it.de/egtb/fetch.php?action=egtb&fen="
 expression = re.compile("(\d+)-(\d+)-?(\d+)?: (Win in \d+|Draw|Lose in \d+)")
 PROMOTION_FLAGS = {
     8: QUEEN_PROMOTION,
@@ -14,7 +15,13 @@ PROMOTION_FLAGS = {
     11: KNIGHT_PROMOTION
 }
 
+table = {}
+
 def probeEndGameTable (board):
+    
+    fen = board.asFen().split()[0] + " w - - 0 1"
+    if (fen,board.color) in table:
+        return table[(fen,board.color)]
     
     # k4it has all 6-men tables except 5 vs. 1
     whites = bitLength(board.friends[WHITE])
@@ -23,52 +30,50 @@ def probeEndGameTable (board):
         return []
     
     # Request the page
-    data = {
-        "action": "egtb",
-        "fen": board.asFen()
-    }
-    f = urllib.urlopen(URL + urllib.urlencode(data))
+    f = urllib.urlopen(URL + fen)
     data = f.read()
     
-    # The response has not only the moves of our requested color, but the
-    # the opponents moves as well. We don't use these.
-    color_data, opcolor_data = data.split("NEXTCOLOR")
-    
     # Parse
-    moves = []
-    for fcord, tcord, promotion, result in expression.findall(color_data):
-        fcord = int(fcord)
-        tcord = int(tcord)
+    for color, move_data in enumerate(data.split("\nNEXTCOLOR\n")):
+        moves = []
+        for fcord, tcord, promotion, result in expression.findall(move_data):
+            fcord = int(fcord)
+            tcord = int(tcord)
+            
+            if promotion:
+                flag = PROMOTION_FLAGS[int(promotion)]
+            elif RANK(fcord) != RANK(tcord) and FILE(fcord) != FILE(tcord) and \
+                    board.arBoard[fcord] == PAWN and board.arBoard[tcord] == EMPTY:
+                flag = ENPASSANT
+            else: flag = NORMAL_MOVE
+            
+            move = newMove(fcord, tcord, flag)
+            
+            if result == "Draw":
+                state = DRAW
+                steps = 0
+            else:
+                s, steps = result.split(" in ")
+                steps = int(steps)
+            
+            if result.startswith("Win"):
+                if board.color == WHITE:
+                    state = (WHITEWON, int(steps))
+                else: state = (BLACKWON, int(steps))
+            elif result.startswith("Lose"):
+                if board.color == WHITE:
+                    state = (BLACKWON, int(steps))
+                else: state = (WHITEWON, int(steps))
+            
+            moves.append( (move,state,steps) )
         
-        if promotion:
-            flag = PROMOTION_FLAGS[int(promotion)]
-        elif RANK(fcord) != RANK(tcord) and FILE(fcord) != FILE(tcord) and \
-                board.arBoard[fcord] == PAWN and board.arBoard[tcord] == EMPTY:
-            flag = ENPASSANT
-        else: flag = NORMAL_MOVE
-        
-        move = newMove(fcord, tcord, flag)
-        
-        if result == "Draw":
-            state = DRAW
-            steps = 0
+        if moves:
+            table[(fen,board.color)] = moves
         else:
-            s, steps = result.split(" in ")
-            steps = int(steps)
-        
-        if result.startswith("Win"):
-            if board.color == WHITE:
-                state = (WHITEWON, int(steps))
-            else: state = (BLACKWON, int(steps))
-        elif result.startswith("Lose"):
-            if board.color == WHITE:
-                state = (BLACKWON, int(steps))
-            else: state = (WHITEWON, int(steps))
-        
-        moves.append( (move,state,steps) )
+            log.warn("Unable to get egtb data for position: %s.\nData was: %s" %
+                     (board.asFen(), repr(data)))
     
-    assert moves, repr(data)
-    return moves
+    return table[(fen,board.color)]
 
 if __name__ == "__main__":
     from pychess.Utils.lutils.LBoard import LBoard
