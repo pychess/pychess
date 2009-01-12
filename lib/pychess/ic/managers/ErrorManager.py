5a1f305e479f49de9bcc2b049a0c1932f95ea29e
Thomas Dybdahl Ahle <thomas@ahle.dk>
2009-01-12 22:14:07 +0000

    Catch fics illegal moves

--- lib/pychess/ic/managers/ErrorManager.py
+++ lib/pychess/ic/managers/ErrorManager.py
@@ -7,13 +7,15 @@ class ErrorManager (GObject):
     
     __gsignals__ = {
         'onCommandNotFound' : (SIGNAL_RUN_FIRST, TYPE_NONE, (str,)),
-        'onAmbiguousMove' : (SIGNAL_RUN_FIRST, TYPE_NONE, (str,))
+        'onAmbiguousMove' : (SIGNAL_RUN_FIRST, TYPE_NONE, (str,)),
+        'onIllegalMove' : (SIGNAL_RUN_FIRST, TYPE_NONE, (str,)),
     }
     
     def __init__ (self, connection):
         GObject.__init__(self)
         connection.expect_line (self.onError, "(.*?): Command not found\.")
         connection.expect_line (self.onAmbiguousMove, "Ambiguous move \((%s)\)\." % sanmove)
+        connection.expect_line (self.onIllegalMove, "Illegal move \((%s)\)\." % sanmove)
     
     def onError (self, match):
         command = match.groups()[0]
@@ -22,3 +24,7 @@ class ErrorManager (GObject):
     def onAmbiguousMove (self, match):
         move = match.groups()[0]
         self.emit("onAmbiguousMove", move)
+    
+    def onIllegalMove (self, match):
+        move = match.groups()[0]
+        self.emit("onIllegalMove", move)
