e5122d17ccb0db658266fc66fdc732a1180fcef4
Matthew Gatto <mattgatto@gmail.com>
2009-04-03 21:53:33 +0000

    Change some variants to standard_rules=True and remove OfferManager.offerRematch() which is no longer used

--- lib/pychess/Variants/asymmetricrandom.py
+++ lib/pychess/Variants/asymmetricrandom.py
@@ -93,7 +93,7 @@ class AsymmetricRandomChess:
     cecp_name = "unknown"
     board = AsymmetricRandomBoard
     need_initial_board = True
-    standard_rules = False
+    standard_rules = True
     variant_group = VARIANTS_SHUFFLE
 
 
