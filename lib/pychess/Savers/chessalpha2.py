5e6d8890cb6ce7104c31c0e4865235585cb9de20
Thomas Dybdahl Ahle <thomas@ahle.dk>
2009-06-08 15:26:47 +0000

    Fixed chessalpha save for position opened games

--- lib/pychess/Savers/chessalpha2.py
+++ lib/pychess/Savers/chessalpha2.py
@@ -29,23 +29,20 @@ lisPieces = ((FAN_PIECES[BLACK][KNIGHT],'K'),
  (FAN_PIECES[WHITE][KING],'n'),
  ('†', '+'),
  ('‡', '+'))
-#{\rtf1\ansi{\fonttbl\f0\fswiss Helvetica;}\f0\pard
-#This is some {\b bold} text.\par
-#}
 
+style = """
+table.pychess {display:inline-block; vertical-align:top; margin-right:2em;}
+table.pychess td {margin:0; padding:0; font-size:10pt; font-family:"Chess Alpha 2"; padding-left:.5em}
+table.pychess td.numa {width:0; padding:0; text-align:right}
+table.pychess td.numa {width:0; padding-left:1em; text-align:right}
+table.pychess pre {margin:0; padding:0; font-family:"Chess Alpha 2"; font-size:16pt; text-align:center}"""
 
 def save (file, model):
     """Saves the position as a diagram using chess fonts"""
     
     print >> file, "<meta http-equiv='Content-Type' content='text/html;charset=UTF-8'>"
-    print >> file, """<style type='text/css'>
-                      table{display:inline-block; vertical-align:top; margin-right:2em;}
-                      td.mov{margin:0;padding:0;font-size:10pt;font-family:"Chess Alpha 2"; padding-left:.5em}
-                      td.numa, td.numb {width:0; padding:0; text-align:right}
-                      td.numb {padding-left:1em}
-                      pre{margin:0; padding:0}</style>"""
-    print >> file, """<table cellspacing="0" cellpadding="0"><tr><td colspan='6'>
-                      <pre style='font-family:"Chess Alpha 2"; font-size:16pt; text-align:center'>"""
+    print >> file, "<style type='text/css'>%s</style>"%style
+    print >> file, "<table cellspacing='0' cellpadding='0' class='pychess'><tr><td colspan='6'><pre>"
     writeDiagram(file, model)
     print >> file, "</pre></td></tr>"
     
@@ -55,39 +52,21 @@ def save (file, model):
             fan = fan.replace(f,r)
         return fan
     sanmvs = map(conv, sanmvs)
+    if model.lowply & 1: sanmvs = [">"]+sanmvs
     sanmvs.extend(['']*((4-(len(sanmvs)%4))%4))
     sanmvs = [(sanmvs[i],sanmvs[i+1]) for i in xrange(0,len(sanmvs),2)]
     for i in xrange((len(sanmvs)+1)/2):
-        writeMoves(file, str(i+1), sanmvs[i],
-                         str(i+1+len(sanmvs)/2), sanmvs[i+len(sanmvs)/2])
+        writeMoves(file, str(i+1+model.lowply/2), sanmvs[i],
+                         str(i+1+len(sanmvs)/2+model.lowply/2), sanmvs[i+len(sanmvs)/2])
     print >> file, "</table>"
     
-    #<tr><td valign='top'><pre style='font-family:\"Chess Alpha 2\"'>"
-    #print >> file, "1. >\tMb6+"
-    #print >> file, "2. nc8\tMc6+"
-    #print >> file, "3.\tnd8\tNg7"
-    #print >> file, "4.\te5?\tMb6+"
-    #print >> file, "</pre></td><td valign='top'><pre style='font-family:\"Chess Alpha 2\"'>"
-    #print >> file, "5.\tne8\tMb8+"
-    #print >> file, "6.\td8=m\tMb5+"
-    #print >> file, "<div style='text-aling:center'>½-½</div>"
-    #print >> file, "</pre></td></tr></table>"
-    
     file.close()
-
-def writeMoves2(file, m1, movepair1, m2, movepair2):
-    preit = lambda s: "<td style='font-family:\"Chess Alpha 2\"; font-size:10pt'>%s</td>"%s
-    preitr = lambda s: "<td style='font-family:\"Chess Alpha 2\"; font-size:10pt; text-align:right; width:0'>%s</td>"%s
-    m1 += '.'; m2 += '.' 
-    print >> file, "<tr>%s%s%s" %  (preitr(m1), preit(movepair1[0]), preit(movepair1[1]))
-    if not movepair2[0]: m2 = ''
-    print >> file, "%s%s%s</tr>" %  (preitr(m2), preit(movepair2[0]), preit(movepair2[1]))
 
 def writeMoves(file, m1, movepair1, m2, movepair2):
     m1 += '.'; m2 += '.' 
     if not movepair2[0]: m2 = ''
-    print >> file, """<tr><td class='mov numa'>%s</td><td class='mov'>%s</td><td class='mov'>%s</td>
-                          <td class='mov numb'>%s</td><td class='mov'>%s</td><td class='mov'>%s</td></tr>""" % \
+    print >> file, """<tr><td class='numa'>%s</td><td>%s</td><td>%s</td>
+                          <td class='numb'>%s</td><td>%s</td><td>%s</td></tr>""" % \
                           (m1, movepair1[0], movepair1[1], m2, movepair2[0], movepair2[1])
 
 
