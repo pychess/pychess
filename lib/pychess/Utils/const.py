6e5538f6d821c0e113dc130ef14487d21340974e
Bajusz Tamás <gbtami@users.noreply.github.com>
2009-10-19 14:06:30 +0000

    bump version to beta2

--- lib/pychess/Utils/const.py
+++ lib/pychess/Utils/const.py
@@ -5,7 +5,7 @@
 ################################################################################
 
 NAME = "PyChess"
-VERSION = "0.10beta1"
+VERSION = "0.10beta2"
 VERSION_NAME = "Staunton"
 ENGINES_XML_API_VERSION = "0.10"
 
