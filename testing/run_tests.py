8d5a051aaab0077cc75b9e1bb16afbfe3c9af373
Bajusz Tamás <gbtami@users.noreply.github.com>
2009-11-22 19:32:11 +0000

    preliminary pgn test

--- testing/run_tests.py
+++ testing/run_tests.py
@@ -1,7 +1,16 @@
 import unittest
 
+modules_to_test = (
+    "bitboard",
+    "eval",
+    "fen",
+    "frc_castling",
+    "movegen",
+    "pgn",
+    "zobrist",
+    ) 
+
 def suite():
-    modules_to_test = ("movegen","fen","eval", "zobrist", "frc_castling","bitboard") 
     tests = unittest.TestSuite()
     for module in map(__import__, modules_to_test):
         tests.addTest(unittest.findTestCases(module))
