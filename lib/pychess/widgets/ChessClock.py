c98ed400cd945bb922f3420e8a75167eb61605ff
Matthew Gatto <mattgatto@gmail.com>
2010-11-11 20:32:47 -0800

    Added argparse support and a --thread-debug command line option; moved ChessClock.redraw_canvas stuff from glock.acquire() to gobject.idle_add() to avoid some of the deadlocks which have plaguing every non-trivial addition I make lately

--- lib/pychess/widgets/ChessClock.py
+++ lib/pychess/widgets/ChessClock.py
@@ -1,12 +1,9 @@
 # -*- coding: UTF-8 -*-
 
 from math import ceil, pi, cos, sin
-
+import cairo, gtk, pango, gobject
 from gtk import gdk
-import gtk, time, pango
-import cairo
 
-from pychess.System import glock
 from pychess.System.repeat import repeat_sleep
 from pychess.Utils.const import WHITE, BLACK
 
@@ -136,16 +133,14 @@ class ChessClock (gtk.DrawingArea):
         context.show_layout(layout1)
     
     def redraw_canvas(self):
+        def redraw (self):
+            if self.window:
+                a = self.get_allocation()
+                rect = gdk.Rectangle(0, 0, a.width, a.height)
+                self.window.invalidate_rect(rect, True)
+                self.window.process_updates(True)
         if self.window:
-            glock.acquire()
-            try:
-                if self.window:
-                    a = self.get_allocation()
-                    rect = gdk.Rectangle(0, 0, a.width, a.height)
-                    self.window.invalidate_rect(rect, True)
-                    self.window.process_updates(True)
-            finally:
-                glock.release()
+            gobject.idle_add(redraw, self, priority=gobject.PRIORITY_HIGH)
     
     def setModel (self, model):
         self.model = model
