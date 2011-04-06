4850a9d7c329f513df89693148e930afe2223f64
Matthew Gatto <mattgatto@gmail.com>
2011-04-06 07:34:50 -0700

    Fixed markup appearing in FICS Lounge for "Sanctions: ...", a regression from revision 1dea0eebc9ff

---
+++ lib/pychess/Variants/losers.py
@@ -0,0 +1,26 @@
+# Losers Chess
+
+from pychess.Utils.const import *
+from pychess.Utils.lutils.bitboard import bitLength
+from pychess.Utils.Board import Board
+
+class LosersBoard(Board):
+    variant = LOSERSCHESS
+
+
+class LosersChess:
+    __desc__ = _("FICS losers: http://www.freechess.org/Help/HelpFiles/losers_chess.html")
+    name = _("Losers")
+    cecp_name = "losers"
+    board = LosersBoard
+    need_initial_board = False
+    standard_rules = False
+    variant_group = VARIANTS_OTHER
+
+
+def testKingOnly(board):
+    boards = board.boards[board.color]
+    
+    return bitLength(boards[PAWN]) + bitLength(boards[KNIGHT]) + \
+           bitLength(boards[BISHOP]) + bitLength(boards[ROOK]) + \
+           bitLength(boards[QUEEN]) == 0
---
+++ lib/pychess/Variants/shuffle.py
@@ -0,0 +1,42 @@
+# Shuffle Chess
+
+import random
+
+from pychess.Utils.const import *
+from pychess.Utils.Board import Board
+
+class ShuffleBoard(Board):
+    variant = SHUFFLECHESS
+
+    def __init__ (self, setup=False):
+        if setup is True:
+            Board.__init__(self, setup=self.shuffle_start())
+        else:
+            Board.__init__(self, setup=setup)
+
+    def shuffle_start(self):
+        tmp = ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
+        random.shuffle(tmp)
+        tmp = ''.join(tmp)
+        tmp = tmp + '/pppppppp/8/8/8/8/PPPPPPPP/' + tmp.upper() + ' w - - 0 1'
+        
+        return tmp
+
+class ShuffleChess:
+    __desc__ = _("xboard nocastle: http://tim-mann.org/xboard/engine-intf.html#8\n" +
+                 "FICS wild/2: http://www.freechess.org/Help/HelpFiles/wild.html\n" +
+                 "* Random arrangement of the pieces behind the pawns\n" +
+                 "* No castling\n" +
+                 "* Black's arrangement mirrors white's")
+    name = _("Shuffle")
+    cecp_name = "nocastle"
+    board = ShuffleBoard
+    need_initial_board = True
+    standard_rules = True
+    variant_group = VARIANTS_SHUFFLE
+
+
+if __name__ == '__main__':
+    Board = ShuffleBoard(True)
+    for i in range(10):
+        print Board.shuffle_start()
