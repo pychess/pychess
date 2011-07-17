4bf8e94cf74d5603f9d7cefe8a7b412c41793243
Matthew Gatto <mattgatto@gmail.com>
2011-07-17 08:03:33 -0700

    Made "Show Chat" button of FICS Lounge insensitive until Lounge is finished loading, a work-around for issue 677

--- lib/pychess/ic/ICLounge.py
+++ lib/pychess/ic/ICLounge.py
@@ -70,6 +70,9 @@ class ICLounge (GObject):
         self.connection.connect("disconnected", lambda connection: self.close())
         self.connection.connect("error", self.on_connection_error)
         
+        # workaround for https://code.google.com/p/pychess/issues/detail?id=677
+        self.connection.nm.connect("readNews", self.on_news_item)
+        
         if self.connection.isRegistred():
             numtimes = conf.get("numberOfTimesLoggedInAsRegisteredUser", 0) + 1
             conf.set("numberOfTimesLoggedInAsRegisteredUser", numtimes)
@@ -100,6 +103,10 @@ class ICLounge (GObject):
             # observed
             CreatedBoards(w,c)
         )
+
+    @glock.glocked
+    def on_news_item (self, nm, news):            
+        self.widgets["show_chat_button"].set_sensitive(True)    
 
     def show (self):
         self.widgets["fics_lounge"].show()
