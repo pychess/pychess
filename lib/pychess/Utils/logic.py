01f555c89f13e809aaf02d51aa3ca33d43bf4724
Justin Blanchard <UncombedCoconut@users.noreply.github.com>
2012-07-15 10:26:21 -0500

    getStatus() should not need to clone the board

--- lib/pychess/Utils/logic.py
+++ lib/pychess/Utils/logic.py
@@ -49,14 +49,13 @@ def getStatus (board):
         if ldraw.testMaterial (lboard):
             return DRAW, DRAW_INSUFFICIENT
     
-    board_clone = lboard.clone()
     hasMove = False
-    for move in lmovegen.genAllMoves (board_clone):
-        board_clone.applyMove(move)
-        if board_clone.opIsChecked():
-            board_clone.popMove()
+    for move in lmovegen.genAllMoves (lboard):
+        lboard.applyMove(move)
+        if lboard.opIsChecked():
+            lboard.popMove()
             continue
-        board_clone.popMove()
+        lboard.popMove()
         hasMove = True
         break
 
