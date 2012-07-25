55c99a60518de5f894ab0a85383c37cf4c124796
Bajusz Tamás <gbtami@users.noreply.github.com>
2012-07-25 22:46:58 +0200

    Move database walk back to database.py

--- lib/pychess/Savers/database.py
+++ lib/pychess/Savers/database.py
@@ -8,7 +8,7 @@ from pgn import PGNFile
 from pychess.Utils.const import reprResult, WHITE, BLACK
 from pychess.Utils.const import *
 from pychess.Utils.lutils.LBoard import LBoard
-from pychess.Database.util import *
+from pychess.Utils.lutils.lmove import newMove
 from pychess.Database import model as dbmodel
 from pychess.Database.model import metadata, event, site, player, pl1, pl2, game, annotator
 from pychess.Variants.fischerandom import FischerRandomChess
@@ -16,6 +16,54 @@ from pychess.Variants.fischerandom import FischerRandomChess
 __label__ = _("PyChess database")
 __endings__ = "pdb",
 __append__ = True
+
+
+MAXMOVE = newMove(63, 63, NULL_MOVE)
+COMMENT, VARI_START, VARI_END, NAG = [MAXMOVE+i+1 for i in range(4)]
+
+def walk(node, arr, txt):
+    """Recurively walks the node tree to collect moves and comments.
+       
+       Arguments:
+       node - list (a tree of lboards created by the pgn parser)
+       arr - array("H") (2 byte unsigned ints representing lmove objects
+                        or COMMENT, VARI_START, VARI_END, NAG+nag)
+       txt - list (comment strings)"""
+        
+    while True: 
+        if node is None:
+            break
+        
+        # Initial game or variation comment
+        if node.prev is None:
+            for child in node.children:
+                if isinstance(child, basestring):
+                    arr.append(COMMENT)
+                    txt.append(child)
+            node = node.next
+            continue
+
+        arr.append(node.history[-1][0])
+
+        for nag in node.nags:
+            if nag:
+                arr.append(NAG + int(nag[1:]))
+
+        for child in node.children:
+            if isinstance(child, basestring):
+                # comment
+                arr.append(COMMENT)
+                txt.append(child)
+            else:
+                # variations
+                arr.append(VARI_START)
+                walk(child[0], arr, txt)
+                arr.append(VARI_END)
+
+        if node.next:
+            node = node.next
+        else:
+            break
 
 
 def save (file, model):
