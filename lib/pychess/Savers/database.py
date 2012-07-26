1f516bdee84b480ab33a089f13594ecd14b8ab5b
Bajusz Tamás <gbtami@users.noreply.github.com>
2012-07-26 22:20:06 +0200

    Merge from database clone

--- lib/pychess/Savers/database.py
+++ lib/pychess/Savers/database.py
@@ -8,63 +8,14 @@ from pgn import PGNFile
 from pychess.Utils.const import reprResult, WHITE, BLACK
 from pychess.Utils.const import *
 from pychess.Utils.lutils.LBoard import LBoard
-from pychess.Utils.lutils.lmove import newMove
 from pychess.Database import model as dbmodel
+from pychess.Database.dbwalk import walk, COMMENT, VARI_START, VARI_END, NAG
 from pychess.Database.model import metadata, event, site, player, pl1, pl2, game, annotator
 from pychess.Variants.fischerandom import FischerRandomChess
 
 __label__ = _("PyChess database")
 __endings__ = "pdb",
 __append__ = True
-
-
-MAXMOVE = newMove(63, 63, NULL_MOVE)
-COMMENT, VARI_START, VARI_END, NAG = [MAXMOVE+i+1 for i in range(4)]
-
-def walk(node, arr, txt):
-    """Prepares a game data for databse.
-       Recursively walks the node tree to collect moves and comments.
-       
-       Arguments:
-       node - list (a tree of lboards created by the pgn parser)
-       arr - array("H") (2 byte unsigned ints representing lmove objects
-                        or COMMENT, VARI_START, VARI_END, NAG+nag)
-       txt - list (comment strings)"""
-        
-    while True: 
-        if node is None:
-            break
-        
-        # Initial game or variation comment
-        if node.prev is None:
-            for child in node.children:
-                if isinstance(child, basestring):
-                    arr.append(COMMENT)
-                    txt.append(child)
-            node = node.next
-            continue
-
-        arr.append(node.history[-1][0])
-
-        for nag in node.nags:
-            if nag:
-                arr.append(NAG + int(nag[1:]))
-
-        for child in node.children:
-            if isinstance(child, basestring):
-                # comment
-                arr.append(COMMENT)
-                txt.append(child)
-            else:
-                # variations
-                arr.append(VARI_START)
-                walk(child[0], arr, txt)
-                arr.append(VARI_END)
-
-        if node.next:
-            node = node.next
-        else:
-            break
 
 
 def save (file, model):
@@ -246,7 +197,6 @@ class Database(PGNFile):
                 if parenthesis == 0:
                     v_last_board.children.append(self.parse_string(v_array[:-1], last_board.prev, position, variation=True))
                     v_array = array("H")
-                    prev_elem = VARI_END
                     continue
 
             elif elem == VARI_START:
