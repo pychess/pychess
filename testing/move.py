55ae11e0a0c4b1b26bf77fee20bffada87f99e63
Bajusz Tamás <gbtami@users.noreply.github.com>
2012-08-18 14:28:56 +0200

    More piece theme

---
+++ testing/move.py
@@ -0,0 +1,59 @@
+import sys
+import unittest
+
+from pychess.Utils.Move import Move
+from pychess.Utils.lutils.LBoard import LBoard
+from pychess.Utils.lutils.lmove import parseSAN, parseFAN, toFAN
+from pychess.Utils.lutils.lmovegen import genAllMoves
+
+
+class MoveTestCase(unittest.TestCase):
+    
+    def test_paresSAN(self):
+        """Testing parseSAN with unambiguous notations variants"""
+        
+        board = LBoard()
+        board.applyFen("4k2B/8/8/8/8/8/8/B3K3 w - - 0 1")        
+
+        self.assertEqual(repr(Move(parseSAN(board, 'Ba1b2'))), 'a1b2')
+        self.assertEqual(repr(Move(parseSAN(board, 'Bh8b2'))), 'h8b2')
+
+        self.assertEqual(repr(Move(parseSAN(board, 'Bab2'))), 'a1b2')
+        self.assertEqual(repr(Move(parseSAN(board, 'Bhb2'))), 'h8b2')
+
+        self.assertEqual(repr(Move(parseSAN(board, 'B1b2'))), 'a1b2')
+        self.assertEqual(repr(Move(parseSAN(board, 'B8b2'))), 'h8b2')
+
+
+        board = LBoard()
+        board.applyFen("4k2B/8/8/8/8/8/1b6/B3K3 w - - 0 1")        
+
+        self.assertEqual(repr(Move(parseSAN(board, 'Ba1xb2'))), 'a1b2')
+        self.assertEqual(repr(Move(parseSAN(board, 'Bh8xb2'))), 'h8b2')
+
+        self.assertEqual(repr(Move(parseSAN(board, 'Baxb2'))), 'a1b2')
+        self.assertEqual(repr(Move(parseSAN(board, 'Bhxb2'))), 'h8b2')
+
+        self.assertEqual(repr(Move(parseSAN(board, 'B1xb2'))), 'a1b2')
+        self.assertEqual(repr(Move(parseSAN(board, 'B8xb2'))), 'h8b2')
+
+    def test_parseFAN(self):
+        """Testing parseFAN"""
+
+        board = LBoard()
+        board.applyFen("rnbqkbnr/8/8/8/8/8/8/RNBQKBNR w KQkq - 0 1")        
+
+        for lmove in genAllMoves(board):
+            board.applyMove(lmove)
+            if board.opIsChecked():
+                board.popMove()
+                continue
+
+            board.popMove()
+
+            fan = toFAN(board, lmove)
+            self.assertEqual(parseFAN(board, fan), lmove)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ translators.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+import collections
+
+#############################
+# Configuration starts here #
+#############################
+
+FILENAME = 'TRANSLATORS'
+POOLSIZE = 7
+
+###########################
+# Configuration ends here #
+###########################
+
+from urllib import urlopen
+from multiprocessing import Pool
+import re
+
+print "Getting data from Rosetta Launchpad..."
+data = urlopen('http://translations.launchpad.net/pychess/trunk/+translations').read()
+langs = re.findall('/pychess/trunk/\+pots/pychess/(.*?)/\+translate', data)
+langs.sort()
+
+def findContributors(lang):
+    site = "https://translations.launchpad.net/pychess/trunk/+pots/pychess/%s/+translate" % lang
+    data = urlopen(site).read()
+    language = re.findall("<h1>Browsing (.*?) translation</h1>", data)[0]
+    start = data.find('Contributors to this translation')
+    pers = re.findall('class="sprite person">(.*?)</a>', data[start:])
+    print "Did", language
+    return [language, pers]
+
+with open(FILENAME,'w') as file:
+    pool = Pool(POOLSIZE)
+    contributors = pool.map(findContributors, langs)
+    for lang, (language, pers) in zip(langs, contributors):
+        print >> file, "[%s] %s" % (lang, language)
+        for per in pers:
+            print >> file, "     " + per
+        print >> file
+
