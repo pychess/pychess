1c1300d099fc0d133e2f52b658fea7b5b179d93a
Bajusz Tamás <gbtami@users.noreply.github.com>
2013-04-29 21:31:35 +0200

    Ratings can be shorter than 4 digit

---
+++ lib/pychess/Utils/lutils/egtb_gaviota.py
@@ -0,0 +1,224 @@
+import os
+import re
+import sys
+import platform
+from ctypes import *
+
+from bitboard import firstBit, clearBit
+from lmovegen import genAllMoves, genCheckEvasions
+from pychess.Utils.const import *
+from pychess.System import conf
+from pychess.System.prefix import addDataPrefix, getDataPrefix
+from pychess.System.Log import log
+
+class TB_STATS(Structure):
+    _fields_ = [
+                 ( 'wdl_easy_hits'    , c_ulong*2 ),
+                 ( 'wdl_hard_prob'    , c_ulong*2 ),
+                 ( 'wdl_soft_prob'    , c_ulong*2 ),
+                 ( 'wdl_cachesize'    , c_size_t  ),
+                 ( 'wdl_occupancy'    , c_double  ),
+                 
+                 ( 'dtm_easy_hits'    , c_ulong*2 ),
+                 ( 'dtm_hard_prob'    , c_ulong*2 ),
+                 ( 'dtm_soft_prob'    , c_ulong*2 ),
+                 ( 'dtm_cachesize'    , c_size_t  ),
+                 ( 'dtm_occupancy'    , c_double  ),
+                 
+                 ( 'total_hits'       , c_ulong*2 ),
+                 ( 'memory_hits'      , c_ulong*2 ),
+                 ( 'drive_hits'       , c_ulong*2 ),
+                 ( 'drive_miss'       , c_ulong*2 ),
+                 ( 'bytes_read'       , c_ulong*2 ),
+                 ( 'files_opened'     , c_ulong   ),
+                 ( 'memory_efficiency', c_double  ),
+               ]
+
+
+class egtb_gaviota:
+    
+    def __init__ (self):
+        self.libgtb = None
+        self.initialized = False
+
+        # Get a list of files in the tablebase folder.
+        configuredTbPath = conf.get("egtb_path", "")
+        tbPath = configuredTbPath or getDataPrefix()
+        try:
+            tbPathContents = os.listdir(tbPath)
+        except OSError, e:
+            if configuredTbPath:
+                log.warn("Unable to open Gaviota TB folder: %s" % repr(e))
+            return
+        
+        # Find files named *.gtb.cp# and pick the most common "#".
+        # (This is the compression scheme; the library currently only uses one at a time.)
+        schemeCount = [0] * 10
+        for filename in tbPathContents:
+            match = re.search("\.gtb\.cp(\d)$", filename)
+            if match:
+                schemeCount[int(match.group(1))] += 1
+        compressionScheme = max(zip(schemeCount, range(10)))
+        if compressionScheme[0] == 0:
+            if configuredTbPath:
+                log.warn("Could not find any Gaviota TB files in %s" % configuredTbPath)
+            return
+        compressionScheme = compressionScheme[1]
+        
+        # Locate and load the library.
+        if not self._loadLibrary():
+            return
+        self._setupFunctionPrototypes()
+        
+        self.pathList = self.tbpaths_init()
+        self.pathList = self.tbpaths_add(self.pathList, tbPath)
+        initInfo = self.tb_init(True, compressionScheme, self.pathList)
+        self.initialized = ( self.tb_is_initialized() != 0 )
+        if not self.initialized:
+            log.warn(initInfo or "Failed to initialize Gaviota EGTB library")
+            self.pathList = self.tbpaths_done(self.pathList)
+            return
+        elif initInfo:
+            log.info(initInfo)
+        
+        # TODO: Set up a WDL cache area once the engine can use it.
+        self.initialized &= self.tbcache_init(4*1024*1024, 0)
+        if not self.initialized:
+            log.warn("Failed to initialize Gaviota EGTB cache")
+            self.tb_done()
+            self.pathList = self.tbpaths_done(self.pathList)
+            return
+        
+        self.availability = self.tb_availability()
+    
+    def __del__ (self):
+        if self.initialized:
+            self.tb_done()
+            self.pathList = self.tbpaths_done(self.pathList)
+    
+    def supports (self, size):
+        return self.initialized and ( \
+                   sum(size) <= 2 or  \
+                   (self.availability & (3 << (2*sum(size)-6))) != 0 )
+    
+    def scoreAllMoves (self, board):
+        result, depth = self.scoreGame(board, False, False)
+        if result is None:
+            return []
+        
+        scores = []
+        gen = board.isChecked() and genCheckEvasions or genAllMoves
+        for move in gen(board):
+            board.applyMove(move)
+            if not board.opIsChecked():
+                result, depth = self.scoreGame(board, False, False)
+                if result is None:
+                    log.warn("An EGTB file does not have all its dependencies")
+                    board.popMove()
+                    return []
+                scores.append( (move, result, depth) )
+            board.popMove()
+        
+        def mateScore(mrd):
+            if mrd[1] == DRAW:
+                return 0
+            absScore = 32767 - mrd[2]
+            if (board.color == WHITE) ^ (mrd[1] == WHITEWON):
+                return absScore
+            return -absScore
+        
+        scores.sort(key=mateScore)
+        return scores
+    
+    def scoreGame(self, board, omitDepth, probeSoft):
+        stm = board.color
+        epsq = board.enpassant or 64 # 64 is tb_NOSQUARE
+        castles = (board.castling >> 2 & 3) | (board.castling << 2 & 12)
+        tbinfo = c_uint()
+        depth = c_uint()
+        
+        SqArray= c_uint * 65
+        PcArray= c_byte * 65
+        pc, sq = [], []
+        for color in (WHITE, BLACK):
+            sq.append(SqArray())
+            pc.append(PcArray())
+            i = 0
+            bb = board.friends[color]
+            while bb:
+                b = firstBit(bb)
+                bb = clearBit(bb, b)
+                sq[-1][i] = b
+                pc[-1][i] = board.arBoard[b]
+                i += 1
+            sq[-1][i] = 64 # tb_NOSQUARE, terminates the list
+            pc[-1][i] =  0 # tb_NOPIECE,  terminates the list
+        
+        if       omitDepth and     probeSoft:
+            ok = self.tb_probe_WDL_soft(stm, epsq, castles, sq[WHITE], sq[BLACK], pc[WHITE], pc[BLACK], byref(tbinfo))
+        elif     omitDepth and not probeSoft:
+            ok = self.tb_probe_WDL_hard(stm, epsq, castles, sq[WHITE], sq[BLACK], pc[WHITE], pc[BLACK], byref(tbinfo))
+        elif not omitDepth and     probeSoft:
+            ok = self.tb_probe_soft    (stm, epsq, castles, sq[WHITE], sq[BLACK], pc[WHITE], pc[BLACK], byref(tbinfo), byref(depth))
+        elif not omitDepth and not probeSoft:
+            ok = self.tb_probe_hard    (stm, epsq, castles, sq[WHITE], sq[BLACK], pc[WHITE], pc[BLACK], byref(tbinfo), byref(depth))
+        
+        resultMap = [ DRAW, WHITEWON, BLACKWON ]
+        if not ok or not 0 <= tbinfo.value <= 2:
+            return None, None
+        result = resultMap[tbinfo.value]
+        if omitDepth or result == DRAW:
+            depth = None
+        else:
+            depth = depth.value
+        return result, depth
+    
+    def _loadLibrary (self):
+        if platform.system() == "Windows":
+            libName = "gtb.dll"
+        else:
+            libName = "libgtb.so"
+        arch = "x86-64" if sys.maxsize > 2**32 else "x86-32"
+        for path in [ libName, addDataPrefix("gaviota/%s/%s" % (arch, libName)) ]:
+            try:
+                self.libgtb = CDLL(path)
+                break
+            except OSError:
+                pass
+        return self.libgtb
+    
+    # Prototypes from gtb-probe.h follow.
+
+    def _setupFunctionPrototypes (self):
+        def proto(name, returnType, *args):
+            argTypes = map(lambda x: x[0], args)
+            argNames = map(lambda x: x[1], args)
+            funcType = CFUNCTYPE(returnType, *argTypes)
+            paramFlags = tuple(zip([1] * len(args), argNames))
+            setattr(self, name, funcType((name, self.libgtb), paramFlags))
+
+        paths_t = POINTER(c_char_p)
+        uip = POINTER(c_uint)
+        ucp = POINTER(c_byte)
+
+        proto("tb_init"   , c_char_p, (c_int, "verbosity"), (c_int, "compression_scheme"), (paths_t, "paths"))
+        proto("tb_restart", c_char_p, (c_int, "verbosity"), (c_int, "compression_scheme"), (paths_t, "paths"))
+        proto("tb_done", None)
+        proto("tb_probe_hard", c_int, (c_uint, "stm"), (c_uint, "epsq"), (c_uint, "castles"), (uip, "wSQ"), (uip, "bSQ"), (ucp, "wPC"), (ucp, "bPC"), (uip, "tbinfo"), (uip, "plies"))
+        proto("tb_probe_soft", c_int, (c_uint, "stm"), (c_uint, "epsq"), (c_uint, "castles"), (uip, "wSQ"), (uip, "bSQ"), (ucp, "wPC"), (ucp, "bPC"), (uip, "tbinfo"), (uip, "plies"))
+        proto("tb_probe_WDL_hard", c_int, (c_uint, "stm"), (c_uint, "epsq"), (c_uint, "castles"), (uip, "wSQ"), (uip, "bSQ"), (ucp, "wPC"), (ucp, "bPC"), (uip, "tbinfo"))
+        proto("tb_probe_WDL_soft", c_int, (c_uint, "stm"), (c_uint, "epsq"), (c_uint, "castles"), (uip, "wSQ"), (uip, "bSQ"), (ucp, "wPC"), (ucp, "bPC"), (uip, "tbinfo"))
+        proto("tb_is_initialized", c_int)
+        proto("tb_availability", c_uint)
+        proto("tb_indexmemory", c_size_t)
+        proto("tbcache_init"   , c_int, (c_size_t, "cache_mem"), (c_int, "wdl_fraction"))
+        proto("tbcache_restart", c_int, (c_size_t, "cache_mem"), (c_int, "wdl_fraction"))
+        proto("tbcache_done", None)
+        proto("tbcache_is_on", c_int)
+        proto("tbcache_flush", None)
+        proto("tbstats_reset", None)
+        proto("tbstats_get", None, (POINTER(TB_STATS), "stats"))
+        proto("tbpaths_init", paths_t)
+        proto("tbpaths_add", paths_t, (paths_t, "ps"), (c_char_p, "newpath"))
+        proto("tbpaths_done", paths_t, (paths_t, "ps"))
+        proto("tbpaths_getmain", c_char_p)
---
+++ setup.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from imp import load_module, find_module
+pychess = load_module("pychess", *find_module("pychess",["lib"]))
+
+from distutils.core import setup
+from glob import glob
+from os import listdir
+from os.path import isdir, isfile
+import os
+import sys
+
+# To run "setup.py register" change name to "NAME+VERSION_NAME"
+# because pychess from another author already exist in pypi.
+VERSION = pychess.VERSION
+
+DESC = "Gnome chess game"
+
+LONG_DESC = """PyChess is a Gtk chess client, originally developed for Gnome, but running well under all other Linux desktops.
+(Which we know of, at least). PyChess is 100% python code, from the top of the UI to the bottom of the chess engine, and all code
+is licensed under the GNU General Public License.
+
+The goal of PyChess is to provide an advanced chess client for Linux following the Gnome Human Interface Guidelines. The client should be usable
+to those new to chess, who just want to play a short game and get back to their work, as well as those who wants to use the computer to further
+enhance their play."""
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: X11 Applications :: Gnome',
+    'Intended Audience :: End Users/Desktop',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: POSIX',
+    'Programming Language :: Python',
+    'Topic :: Desktop Environment :: Gnome',
+    'Topic :: Games/Entertainment :: Board Games',
+    'Natural Language :: Arabic',
+    'Natural Language :: Bulgarian',
+    'Natural Language :: Catalan',
+    'Natural Language :: Czech',
+    'Natural Language :: Danish',
+    'Natural Language :: Dutch',
+    'Natural Language :: English',
+    'Natural Language :: Finnish',
+    'Natural Language :: French',
+    'Natural Language :: German',
+    'Natural Language :: Greek',
+    'Natural Language :: Hebrew',
+    'Natural Language :: Hindi', 
+    'Natural Language :: Hungarian',
+    'Natural Language :: Italian',
+    'Natural Language :: Japanese',
+    'Natural Language :: Norwegian',
+    'Natural Language :: Persian',
+    'Natural Language :: Polish',
+    'Natural Language :: Portuguese',
+    'Natural Language :: Portuguese (Brazilian)',
+    'Natural Language :: Romanian',
+    'Natural Language :: Russian',
+    'Natural Language :: Slovak',
+    'Natural Language :: Spanish',
+    'Natural Language :: Swedish',
+    'Natural Language :: Turkish',
+]
+
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+if not isfile("eco.db"):
+    execfile("pgn2ecodb.py")
+
+if not isfile(os.path.abspath("pieces/Pychess.png")):
+    execfile("create_theme_preview.py")
+
+DATA_FILES = [("share/pychess",
+    ["README", "AUTHORS", "ARTISTS", "DOCUMENTERS", "LICENSE", "TRANSLATORS", "pychess_book.bin", "eco.db"])]
+
+# UI
+DATA_FILES += [("share/pychess/glade", glob('glade/*.glade'))]
+DATA_FILES += [("share/pychess/glade", glob('glade/*.png'))]
+DATA_FILES += [("share/pychess/glade", glob('glade/*.svg'))]
+DATA_FILES += [("share/pychess/flags", glob('flags/*.png'))]
+
+# Sidepanel (not a package)
+DATA_FILES += [("share/pychess/sidepanel", glob('sidepanel/*.glade'))]
+DATA_FILES += [("share/pychess/sidepanel", glob('sidepanel/*.py'))]
+
+# Data
+DATA_FILES += [('share/applications', ['pychess.desktop'])]
+DATA_FILES += [('share/icons/hicolor/scalable/apps', ['pychess.svg'])]
+DATA_FILES += [('share/pixmaps', ['pychess.svg'])]
+DATA_FILES += [("share/pychess/sounds", glob('sounds/*.ogg'))]
+DATA_FILES += [('share/icons/hicolor/24x24/apps', ['pychess.png'])]
+DATA_FILES += [('share/gtksourceview-1.0/language-specs', ['gtksourceview-1.0/language-specs/pgn.lang'])]
+
+# Piece sets
+DATA_FILES += [("share/pychess/pieces", glob('pieces/*.png'))]
+DATA_FILES += [("share/pychess/pieces/ttf", glob('pieces/ttf/*.ttf'))]
+
+for dir in [d for d in listdir('pieces') if isdir(os.path.join('pieces', d)) and d != 'ttf']:
+    DATA_FILES += [("share/pychess/pieces/"+dir, glob('pieces/'+dir+'/*.svg'))]
+
+# Manpages
+DATA_FILES += [('share/man/man1', ['manpages/pychess.1.gz'])]
+
+# Language
+pofile = "LC_MESSAGES/pychess"
+if sys.platform == "win32":
+    argv0_path = os.path.dirname(os.path.abspath(sys.executable))
+    sys.path.append(argv0_path + "\\tools\\i18n")
+    import msgfmt
+
+for dir in [d for d in listdir("lang") if d.find(".svn") < 0 and isdir("lang/"+d) and d != "en"]:
+    if sys.platform == "win32":
+        file = "lang/%s/%s" % (dir,pofile)
+        msgfmt.make(file+".po", file+".mo")
+    else:
+        os.popen("msgfmt lang/%s/%s.po -o lang/%s/%s.mo" % (dir,pofile,dir,pofile))
+    DATA_FILES += [("share/locale/"+dir+"/LC_MESSAGES", ["lang/"+dir+"/"+pofile+".mo"])]
+
+# Packages
+
+PACKAGES = ["pychess", "pychess.gfx", "pychess.ic", "pychess.ic.managers",
+            "pychess.Players", "pychess.Savers", "pychess.System",
+            "pychess.Utils", "pychess.Utils.lutils", "pychess.Variants",
+            "pychess.widgets", "pychess.widgets.pydock" ]
+# Setup
+
+setup (
+    name             = 'pychess',
+    version          = VERSION,
+    author           = 'Pychess team',
+    author_email     = 'pychess-people@googlegroups.com',
+    maintainer       = 'Thomas Dybdahl Ahle',
+    classifiers      = CLASSIFIERS,
+    keywords         = 'python gtk chess xboard gnuchess game pgn epd board linux',
+    description      = DESC,
+    long_description = LONG_DESC,
+    license          = 'GPL3',
+    url              = 'http://pychess.org',
+    download_url     = 'http://code.google.com/p/pychess/downloads/list',
+    package_dir      = {'': 'lib'},
+    packages         = PACKAGES,
+    data_files       = DATA_FILES,
+    scripts          = ['pychess']
+)
