b3765dfd20da3842995f52ffb70be4d8365ed0e2
Christopher King <G.nius.ck@gmail.com>
2013-06-25 16:06:51 -0400

    Show player names in "Game Information" dialog. (Issue 331)

---
+++ lib/pychess/Savers/png.py
@@ -0,0 +1,114 @@
+# -*- coding: UTF-8 -*-
+
+import os
+
+import gtk
+import cairo
+import rsvg
+
+from pychess.Utils.const import *
+from pychess.gfx import Pieces
+from pychess.widgets.BoardView import BoardView
+from pychess.widgets import gamewidget
+from pychess.System.prefix import addDataPrefix
+
+
+PADDING = 3
+SQUARE = 40
+
+
+def export(widget, game):
+    """Export the current position into a .png file"""
+
+    d = Diagram(game)
+
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, SQUARE*8, SQUARE*8)
+    context = cairo.Context(surface)
+
+    d.shown = widget.board.view.shown
+    d.draw_position(context)
+
+    dialog = gtk.FileChooserDialog(_("Export position"), None, gtk.FILE_CHOOSER_ACTION_SAVE,
+        (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_SAVE, gtk.RESPONSE_ACCEPT))
+    dialog.set_current_folder(os.environ["HOME"])
+
+    dialog.set_current_name("%s %s %s.png" %
+                            (game.players[0], _("vs."), game.players[1]))
+
+    filter = gtk.FileFilter()
+    filter.set_name(_("All files"))
+    filter.add_pattern("*")
+    dialog.add_filter(filter)
+
+    filter = gtk.FileFilter()
+    filter.set_name(_("Images"))
+    filter.add_mime_type("image/png")
+    filter.add_pattern("*.png")
+    dialog.add_filter(filter)
+
+    # Keep running the dialog until the user has canceled it or made an error
+    # free operation
+    while True:
+    
+        response = dialog.run()
+    
+        if response == gtk.RESPONSE_CANCEL:
+            break
+
+        uri = dialog.get_filename()
+        
+        if os.path.isfile(uri):
+            d = gtk.MessageDialog(type=gtk.MESSAGE_QUESTION)
+            d.add_buttons(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, _("_Replace"),
+                        gtk.RESPONSE_ACCEPT)
+
+            d.set_title(_("File exists"))
+            folder, file = os.path.split(uri)
+            d.set_markup(_("<big><b>A file named '%s' already exists. Would you like to replace it?</b></big>") % file)
+            d.format_secondary_text(_("The file already exists in '%s'. If you replace it, its content will be overwritten.") % folder)
+            replaceRes = d.run()
+            d.hide()
+
+            if replaceRes == gtk.RESPONSE_CANCEL:
+                continue
+
+        try:
+            surface.write_to_png(uri)
+        except IOError, e:
+            d = gtk.MessageDialog(type=gtk.MESSAGE_ERROR)
+            d.add_buttons(gtk.STOCK_OK, gtk.RESPONSE_OK)
+            d.set_title(_("Could not save the file"))
+            d.set_markup(_("<big><b>PyChess was not able to export the position</b></big>"))
+            d.format_secondary_text(_("The error was: %s") % ", ".join(str(a) for a in e.args))
+            os.remove(uri)
+            d.run()
+            d.hide()
+            continue
+
+        break
+        
+    dialog.destroy()
+
+
+class Diagram(BoardView):
+    def draw_position(self, context):
+        context.set_source_rgb(0.5, 0.5, 0.5)
+        self.__drawBoard (context)
+
+        pieces = self.model.getBoardAtPly(self.shown)
+        context.set_source_rgb(0, 0, 0)
+        for y, row in enumerate(pieces.data):
+            for x, piece in row.items():
+                if piece is not None:
+                    Pieces.drawPiece(piece, context, x*SQUARE, (7-y)*SQUARE, SQUARE)
+
+    def __drawBoard(self, context):
+        for x in xrange(8):
+            for y in xrange(8):
+                if (x+y) % 2 == 1:
+                    context.rectangle(x*SQUARE, y*SQUARE, SQUARE, SQUARE)
+        context.fill()
+
+        if not self.showCords:
+            context.rectangle(0, 0, 8*SQUARE, 8*SQUARE)
+            context.stroke()
---
+++ lib/pychess/Utils/repr.py
@@ -0,0 +1,61 @@
+from const import *
+
+import __builtin__
+if '_' not in __builtin__.__dict__:
+    __builtin__.__dict__['_'] = lambda s: s
+
+
+reprColor = [_("White"), _("Black")]
+
+reprPiece = ["Empty", _("Pawn"), _("Knight"), _("Bishop"), _("Rook"), _("Queen"), _("King"), "BPawn"]
+
+localReprSign = ["", _("P"), _("N"), _("B"), _("R"), _("Q"), _("K")]
+
+reprResult_long = {
+    DRAW: _("The game ended in a draw"),
+    WHITEWON: _("%(white)s won the game"),
+    BLACKWON: _("%(black)s won the game"),
+    KILLED: _("The game has been killed"),
+    ADJOURNED: _("The game has been adjourned"),
+    ABORTED: _("The game has been aborted"),
+}
+
+reprReason_long = {
+    DRAW_INSUFFICIENT: _("Because neither player has got sufficient material to mate"),
+    DRAW_REPITITION: _("Because the same position was repeated three times in a row"),
+    DRAW_50MOVES: _("Because the last 50 moves brought nothing new"),
+    DRAW_CALLFLAG: _("Because both players ran out of time"),
+    DRAW_STALEMATE: _("Because %(mover)s stalemated"),
+    DRAW_AGREE: _("Because the players agreed to"),
+    DRAW_ADJUDICATION: _("Because of adjudication by an admin"),
+    DRAW_LENGTH: _("Because the game exceed the max length"),
+    DRAW_BLACKINSUFFICIENTANDWHITETIME: _("Because %(white)s ran out of time and %(black)s hasn't got insufficient material to mate"),
+    DRAW_WHITEINSUFFICIENTANDBLACKTIME: _("Because %(black)s ran out of time and %(white)s hasn't got insufficient material to mate"),
+
+    WON_RESIGN: _("Because %(loser)s resigned"),
+    WON_CALLFLAG: _("Because %(loser)s ran out of time"),
+    WON_MATE: _("Because %(loser)s has been checkmated"),
+    WON_DISCONNECTION: _("Because %(loser)s disconnected"),
+    WON_ADJUDICATION:  _("Because of adjudication by an admin"),
+    WON_NOMATERIAL: _("Because %(loser)s lost all pieces but the king"),
+
+    ADJOURNED_LOST_CONNECTION: _("Because a player lost connection"),
+    ADJOURNED_AGREEMENT: _("Because both players agreed to adjourn the game"),
+    ADJOURNED_SERVER_SHUTDOWN: _("Because the server was shut down"),
+    ADJOURNED_COURTESY: _("Because a player lost connection and the other player requested adjournment"),
+    ADJOURNED_COURTESY_WHITE: _("Because %(black)s lost connection to the server and %(white)s requested adjournment"),
+    ADJOURNED_COURTESY_BLACK: _("Because %(white)s lost connection to the server and %(black)s requested adjournment"),
+    ADJOURNED_LOST_CONNECTION_WHITE: _("Because %(white)s lost connection to the server"),
+    ADJOURNED_LOST_CONNECTION_BLACK: _("Because %(black)s lost connection to the server"),
+
+    ABORTED_ADJUDICATION: _("Because of adjudication by an admin"),
+    ABORTED_AGREEMENT: _("Because both players agreed to"),
+    ABORTED_COURTESY: _("Because of courtesy by a player"),
+    ABORTED_EARLY: _("Because a player quit. No winner was found due to the early phase of the game"),
+    ABORTED_SERVER_SHUTDOWN: _("Because the server was shut down"),
+
+    WHITE_ENGINE_DIED: _("Because the %(white)s engine died"),
+    BLACK_ENGINE_DIED: _("Because the %(black)s engine died"),
+    DISCONNECTED: _("Because the connection to the server was lost"),
+    UNKNOWN_REASON: _("The reason is unknown")
+}
