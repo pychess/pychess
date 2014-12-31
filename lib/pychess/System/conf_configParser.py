a5ab13873f2772bebe9c3f70425a56f74257478a
Bajusz Tamás <gbtami@users.noreply.github.com>
2014-12-31 10:39:34 +0100

    Get rid os types from pychess config file to be 2 and 3 compatible simultaneously

--- lib/pychess/System/conf_configParser.py
+++ lib/pychess/System/conf_configParser.py
@@ -1,6 +1,7 @@
 import os, atexit
+from ast import literal_eval
 
-from pychess.compat import SafeConfigParser, PY2
+from pychess.compat import SafeConfigParser
 from pychess.System.Log import log
 
 configParser = SafeConfigParser()
@@ -12,30 +13,11 @@ if os.path.isfile(path):
     configParser.readfp(open(path))
 if not configParser.has_section(section):
     configParser.add_section(section)
-if not configParser.has_section(section+"_Types"):
-    configParser.add_section(section+"_Types")
 atexit.register(lambda: configParser.write(open(path,"w")))
 
 idkeyfuncs = {}
 conid = 0
 
-typeEncode = {
-    str: repr(str),
-    int: repr(int),
-    float: repr(float),
-    bool: repr(bool)
-}
-if PY2:
-    typeEncode[unicode] = repr(unicode)
-
-typeDecode = {
-    repr(str): configParser.get,
-    repr(int): configParser.getint,
-    repr(float): configParser.getfloat,
-    repr(bool): configParser.getboolean,
-}
-if PY2:
-    typeDecode[repr(unicode)] = configParser.get
 
 def notify_add (key, func, args):
     global conid
@@ -47,13 +29,21 @@ def notify_remove (conid):
     del idkeyfuncs[conid]
 
 def get (key):
-    decoder = typeDecode[configParser.get(section+"_Types", key)]
-    return decoder(section, key)
+    try:
+        return configParser.getint(section, key)
+    except ValueError:
+        pass
+
+    try:
+        return configParser.getboolean(section, key)
+    except ValueError:
+        pass
+
+    return configParser.get(section, key)
 
 def set (key, value):
     try:
         configParser.set (section, key, str(value))
-        configParser.set (section+"_Types", key, typeEncode[type(value)])
     except Exception as e:
         log.error("Unable to save configuration '%s'='%s' because of error: %s %s"%
                 (repr(key), repr(value), e.__class__.__name__, ", ".join(str(a) for a in e.args)))
@@ -62,5 +52,4 @@ def set (key, value):
             func (None, *args)
 
 def hasKey (key):
-    return configParser.has_option(section, key) and \
-           configParser.has_option(section+"_Types", key)
+    return configParser.has_option(section, key)
