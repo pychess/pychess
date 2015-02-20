13cb2d73d1c32db9e41c21b4bd141383d8afe279
gbtami <gbtami@gmail.com>
2015-02-20 12:50:28 +0100

    Standalone pychess engine creation

---
+++ lib/pychess/System/LogEmitter.py
@@ -0,0 +1,37 @@
+import time
+import logging
+
+from gi.repository import GObject
+from .GtkWorker import EmitPublisher, Publisher
+
+
+class LogEmitter(GObject.GObject):
+    __gsignals__ = {
+        "logged": (GObject.SignalFlags.RUN_FIRST, None, (object,))
+    }                                              # list of (str, float, str, int)
+    def __init__ (self):
+        GObject.GObject.__init__(self)
+
+        # We store everything in this list, so that the LogDialog, which is
+        # imported a little later, will have all data ever given to Log.
+        # When Dialog inits, it will set this list to None, and we will stop
+        # appending data to it. Ugly? Somewhat I guess.
+        self.messages = []
+
+        self.publisher = EmitPublisher (self, "logged",
+            'LogEmitter.publisher', Publisher.SEND_LIST)
+        self.publisher.start()
+
+class GLogHandler(logging.Handler):
+    def __init__ (self, emitter):
+        logging.Handler.__init__(self)
+        self.emitter = emitter
+        
+    def emit(self, record):
+        message = self.format(record)
+        if self.emitter.messages != None:
+            self.emitter.messages.append((record.task, time.time(), message, record.levelno))
+        
+        self.emitter.publisher.put((record.task, time.time(), message, record.levelno))
+
+logemitter = LogEmitter()
