534b129a7649d1612bc68b3622a3688a480bfc4a
gbtami <gbtami@gmail.com>
2015-05-15 15:15:02 +0200

    Fixed analyzing unit tests

--- testing/analysis.py
+++ testing/analysis.py
@@ -58,7 +58,7 @@ class CECPTests(EmittingTestCase):
         engine.putline(analine)
         results = self.getSignalResults(analyzer)
         self.assertNotEqual(results, None, "signal wasn't sent")
-        self.assertEqual(results, ([(listToMoves(board,moves), score, depth)],))
+        self.assertEqual(results, ([(moves, score, depth)],))
     
     def setUp (self):
         self.engineA, self.analyzerA = self._setupengine(ANALYZING)
