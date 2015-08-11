955473842fcec098017c1400e2d307aeba57777d
gbtami <gbtami@gmail.com>
2015-08-11 21:14:34 +0200

    Updated stdeb config file

--- create_deb.sh
+++ create_deb.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python setup.py --command-packages=stdeb.command sdist_dsc bdist_deb
+python3 setup.py --command-packages=stdeb.command sdist_dsc --with-python2=True --with-python3=True bdist_deb
