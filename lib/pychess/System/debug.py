9b06190203e13330c844e50a137d8c282ee2f1db
gbtami <gbtami@gmail.com>
2015-08-23 13:24:40 +0200

    Add some more version info on startup to .log

--- lib/pychess/System/debug.py
+++ lib/pychess/System/debug.py
@@ -7,21 +7,21 @@ from threading import Thread
 from pychess.System import fident
 from pychess.System.Log import log
 
+def dump_threads ():
+    id2thread = {}
+    for thread in threading.enumerate():
+        id2thread[thread.ident] = thread
+    
+    stacks = []
+    for thread_id, frame in sys._current_frames().items():
+        stack = traceback.format_list(traceback.extract_stack(frame))
+        stacks.append("Thread: %s (%d)" % (id2thread[thread_id].name, thread_id))
+        stacks.append("".join(stack))
+    
+    log.debug("\n" + "\n".join(stacks))
+
 def start_thread_dump ():
     def thread_dumper ():
-        def dump_threads ():
-            id2thread = {}
-            for thread in threading.enumerate():
-                id2thread[thread.ident] = thread
-            
-            stacks = []
-            for thread_id, frame in sys._current_frames().items():
-                stack = traceback.format_list(traceback.extract_stack(frame))
-                stacks.append("Thread: %s (%d)" % (id2thread[thread_id].name, thread_id))
-                stacks.append("".join(stack))
-            
-            log.debug("\n" + "\n".join(stacks))
-        
         while True:
             dump_threads()
             time.sleep(10)
