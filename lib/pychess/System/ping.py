e5f9a3bc12be0e2b5c45db95366e90ce41467d92
Bajusz Tamás <gbtami@gmail.com>
2015-09-09 13:01:37 +0200

    Fix #1013 FICS pinger doesn't work on Windows

--- lib/pychess/System/ping.py
+++ lib/pychess/System/ping.py
@@ -1,11 +1,13 @@
-from __future__ import print_function
 # -*- coding: UTF-8 -*-
+from __future__ import print_function
+
+import re
+import sys
 
 from gi.repository import GObject
 
 from pychess.System.Log import log
 from pychess.System.SubProcess import SubProcess, searchPath
-import re
 
 class Pinger (GObject.GObject):
     """ The recieved signal contains the time it took to get response from the
@@ -21,7 +23,7 @@ class Pinger (GObject.GObject):
         self.host = host
         self.subproc = None
         
-        self.expression = re.compile("time=([\d\.]+) (m?s)")
+        self.expression = re.compile("=([\d\.]+) (m?s)")
 
         # We need untranslated error messages in regexp search
         # below, so have to use deferred translation here
@@ -37,16 +39,15 @@ class Pinger (GObject.GObject):
     
     def start (self):
         assert not self.subproc
-        self.subproc = SubProcess(searchPath("ping"),
-                                  ["-i10", self.host], env={"LANG":"en"})
+        if sys.platform == "win32":
+            args = ["-t", self.host]
+        else:
+            args = ["-i10", self.host]
+        self.subproc = SubProcess(searchPath("ping"), args, env={"LANG":"en"})
         self.conid1 = self.subproc.connect("line", self.__handleLines)
         self.conid2 = self.subproc.connect("died", self.__handleDead)
 
-    def __handleLines (self, subprocess, lines):
-        for line in lines:
-            self.__handleLine(line)
-    
-    def __handleLine (self, line):
+    def __handleLines (self, subprocess, line):
         match = self.expression.search(line)
         if match:
             time, unit = match.groups()
