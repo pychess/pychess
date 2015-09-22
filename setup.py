ed05572ceab2c7347594bc607397b05b2def5779
Bajusz Tamás <gbtami@gmail.com>
2015-09-22 23:59:08 +0200

    Fixed frozen book path on Windows

--- setup.py
+++ setup.py
@@ -193,7 +193,7 @@ PACKAGES = []
 
 if msi:
     # TODO: cx_freeze doesn't allow letters in version
-    VERSION = "0.12"
+    VERSION = "0.12.0"
     
     ## Get the site-package folder, not everybody will install
     ## Python into C:\PythonXX
