e5a29ad1ac7913ead73ab741c5235baae1c43d7a
gbtami <gbtami@gmail.com>
2015-11-16 22:19:36 +0100

    Fixed UnboundLocalError: local variable 'id' referenced before assignment

--- lib/pychess/ic/FICSObjects.py
+++ lib/pychess/ic/FICSObjects.py
@@ -429,7 +429,7 @@ class FICSPlayers (GObject.GObject):
     # converted to use FICSPlayer references rather than player's names
     def get_online_playernames (self):
         names = []
-        players = self.values()
+        players = list(self.values())
         for player in players:
             if player.online:
                 names.append(player.name)
