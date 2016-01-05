18b7a5167d55d54bf88301fe4c82ca1827e446fc
gbtami <gbtami@gmail.com>
2016-01-05 23:37:55 +0100

    First docstring using intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -57,6 +57,11 @@ extensions = [
     'sphinx.ext.intersphinx',
 ]
 
+intersphinx_mapping = {
+    'gobject': ('http://lazka.github.io/pgi-docs/GObject-2.0', None),
+    'gtk': ('http://lazka.github.io/pgi-docs/Gtk-3.0', None),
+}
+
 # Sort members by type
 autodoc_member_order = 'groupwise'
 
