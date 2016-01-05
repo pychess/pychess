26f51ca3eef489578ac4263841a66d4dce5c02b2
gbtami <gbtami@gmail.com>
2016-01-05 13:11:16 +0100

    Use sphinx_rtd_theme in sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -86,4 +86,4 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
