0784de6b5c313c1eced201b787c85c6d1d272ad5
gbtami <gbtami@gmail.com>
2016-01-19 22:10:08 +0100

    Fix formatted links wrapped with "

--- lib/pychess/widgets/__init__.py
+++ lib/pychess/widgets/__init__.py
@@ -14,9 +14,9 @@ def insert_formatted(text_view, iter, text, tag=None):
         position = 0
         for i, part in enumerate(parts):
             if "://" in part or "www" in part:
-                if part.startswith(""):
+                if part.startswith('"'):
                     part = part[1:]
-                if part.endswith(""):
+                if part.endswith('"'):
                     part = part[:-1]
                 parts[i] = '<a href="%s">%s</a>' % (part, part)
                 position = i
