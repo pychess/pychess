bb1180e3b6862c45c398d16488eda7ef285e5c25
cajone <peat@blueyonder.co.uk>
2016-02-05 11:54:33 +0000

    #docs pylint PyDockComposite 8.63/10

--- lib/pychess/widgets/pydock/PyDockComposite.py
+++ lib/pychess/widgets/pydock/PyDockComposite.py
@@ -1,17 +1,15 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-import sys
-from gi.repository import Gtk
-from gi.repository import GObject
+from gi.repository import Gtk, GObject
 
-from .__init__ import DockComposite
-from .__init__ import NORTH, EAST, SOUTH, WEST, CENTER, reprPos
+from .__init__ import DockComposite, NORTH, EAST, SOUTH, WEST, CENTER, reprPos
 
-class PyDockComposite (Gtk.Alignment, DockComposite):
-    def __init__ (self, position):
+
+class PyDockComposite(Gtk.Alignment, DockComposite):
+    def __init__(self, position):
         GObject.GObject.__init__(self, xscale=1, yscale=1)
-        
+
         if position == NORTH or position == SOUTH:
             paned = Gtk.VPaned()
         elif position == EAST or position == WEST:
@@ -21,10 +19,12 @@ class PyDockComposite (Gtk.Alignment, DockComposite):
         self.add(self.paned)
         self.paned.show()
 
-    def __repr__ (self):
-        return "composite %s (%s, %s)" % (reprPos[self.position], repr(self.paned.get_child1()), repr(self.paned.get_child2()))
-    
-    def dock (self, widget, position, title, id):
+    def __repr__(self):
+        return "composite %s (%s, %s)" % (reprPos[self.position],
+                                          repr(self.paned.get_child1()),
+                                          repr(self.paned.get_child2()))
+
+    def dock(self, widget, position, title, id):
         assert position != CENTER, "POSITION_CENTER only makes sense for leaves"
         parent = self.get_parent()
         while not isinstance(parent, DockComposite):
@@ -35,8 +35,8 @@ class PyDockComposite (Gtk.Alignment, DockComposite):
         parent.changeComponent(self, new)
         new.initChildren(self, leaf)
         return leaf
-    
-    def changeComponent (self, old, new):
+
+    def changeComponent(self, old, new):
         if old == self.paned.get_child1():
             self.paned.remove(old)
             self.paned.pack1(new, resize=True, shrink=False)
@@ -44,8 +44,8 @@ class PyDockComposite (Gtk.Alignment, DockComposite):
             self.paned.remove(old)
             self.paned.pack2(new, resize=True, shrink=False)
         new.show()
-    
-    def removeComponent (self, component):
+
+    def removeComponent(self, component):
         if component == self.paned.get_child1():
             new = self.paned.get_child2()
         else:
@@ -55,13 +55,13 @@ class PyDockComposite (Gtk.Alignment, DockComposite):
         while not isinstance(parent, DockComposite):
             parent = parent.get_parent()
         parent.changeComponent(self, new)
-        component._del() # TODO: is this necessary?
+        component._del()  # TODO: is this necessary?
         new.show()
-    
-    def getComponents (self):
+
+    def getComponents(self):
         return self.paned.get_children()
-    
-    def initChildren (self, old, new, preserve_dimensions=False):
+
+    def initChildren(self, old, new, preserve_dimensions=False):
         if self.position == NORTH or self.position == WEST:
             self.paned.pack1(new, resize=True, shrink=False)
             self.paned.pack2(old, resize=True, shrink=False)
@@ -70,7 +70,8 @@ class PyDockComposite (Gtk.Alignment, DockComposite):
             self.paned.pack2(new, resize=True, shrink=False)
         old.show()
         new.show()
-        def cb (widget, allocation):
+
+        def cb(widget, allocation):
             if allocation.height != 1:
                 if self.position == NORTH:
                     pos = 0.381966011 * allocation.height
@@ -80,10 +81,11 @@ class PyDockComposite (Gtk.Alignment, DockComposite):
                     pos = 0.381966011 * allocation.width
                 elif self.position == EAST:
                     pos = 0.618033989 * allocation.width
-                widget.set_position(int(pos+.5))
+                widget.set_position(int(pos + .5))
                 widget.disconnect(conid)
+
         if not preserve_dimensions:
             conid = self.paned.connect("size-allocate", cb)
-    
-    def getPosition (self):
+
+    def getPosition(self):
         return self.position
