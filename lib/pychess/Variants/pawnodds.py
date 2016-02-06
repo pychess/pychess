f7d72eeba4a14787a6a92e2bb026bee567f23554
cajone <peat@blueyonder.co.uk>
2016-02-06 16:39:08 +0000

    #docs pylint some docstrings added

--- lib/pychess/Variants/pawnodds.py
+++ lib/pychess/Variants/pawnodds.py
@@ -1,9 +1,13 @@
-from pychess.Utils.const import *
+""" Pawn Odds Variant"""
+from pychess.Utils.const import PAWNODDSCHESS, VARIANTS_ODDS
 from pychess.Utils.Board import Board
 
 PAWNODDSSTART = "rnbqkbnr/ppppp1pp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 class PawnOddsBoard(Board):
+    """:Description: A standard chess game where one  side starts with one less
+        pawn, this is known as giving pawn odds
+    """
     variant = PAWNODDSCHESS
     __desc__ = _("One player starts with one less pawn piece")
     name = _("Pawn odds")
