9459c4d219c80a7f23098a0b17de242c52b52f29
cajone <peat@blueyonder.co.uk>
2016-02-06 13:36:25 +0000

    #docs pylint Variants/euroshogi

--- lib/pychess/Variants/crazyhouse.py
+++ lib/pychess/Variants/crazyhouse.py
@@ -1,11 +1,13 @@
 # Crazyhouse Chess
 
-from pychess.Utils.const import *
+from pychess.Utils.const import CRAZYHOUSECHESS, VARIANTS_OTHER_NONSTANDARD
 from pychess.Utils.Board import Board
+
 
 class CrazyhouseBoard(Board):
     variant = CRAZYHOUSECHESS
-    __desc__ = _("FICS crazyhouse: http://www.freechess.org/Help/HelpFiles/crazyhouse.html")
+    __desc__ = _(
+        "FICS crazyhouse: http://www.freechess.org/Help/HelpFiles/crazyhouse.html")
     name = _("Crazyhouse")
     cecp_name = "crazyhouse"
     need_initial_board = False
