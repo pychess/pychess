f825f1045e702d2b40a61f8f97fa19300dbdc7b3
cajone <peat@blueyonder.co.uk>
2016-02-06 13:54:05 +0000

    #docs pylint Variants/fischerandom 6.86/10

--- lib/pychess/Variants/fischerandom.py
+++ lib/pychess/Variants/fischerandom.py
@@ -2,29 +2,25 @@ from __future__ import print_function
 # Chess960 (Fischer Random Chess)
 
 import random
-from copy import copy
 
-from pychess.Utils.const import *
-from pychess.Utils.Cord import Cord
+from pychess.Utils.const import FISCHERRANDOMCHESS, VARIANTS_SHUFFLE
 from pychess.Utils.Board import Board
-from pychess.Utils.Piece import Piece
-from pychess.Utils.lutils.bitboard import *
-from pychess.Utils.lutils.attack import *
-from pychess.Utils.lutils.lmove import FLAG, PROMOTE_PIECE
+#from pychess.Utils.lutils.bitboard import *
+from pychess.Utils.lutils.attack import reprFile
 
 
 class FischerandomBoard(Board):
     variant = FISCHERRANDOMCHESS
-    __desc__ = _("http://en.wikipedia.org/wiki/Chess960\n" +
-                 "FICS wild/fr: http://www.freechess.org/Help/HelpFiles/wild.html")
+    __desc__ = _(
+        "http://en.wikipedia.org/wiki/Chess960\n" +
+        "FICS wild/fr: http://www.freechess.org/Help/HelpFiles/wild.html")
     name = _("Fischer Random")
     cecp_name = "fischerandom"
     need_initial_board = True
     standard_rules = False
     variant_group = VARIANTS_SHUFFLE
 
-    
-    def __init__ (self, setup=False, lboard=None):
+    def __init__(self, setup=False, lboard=None):
         if setup == True:
             Board.__init__(self, setup=self.shuffle_start(), lboard=lboard)
         else:
@@ -34,46 +30,46 @@ class FischerandomBoard(Board):
         """ Create a random initial position.
             The king is placed somewhere between the two rooks.
             The bishops are placed on opposite-colored squares."""
-      
+
         positions = [1, 2, 3, 4, 5, 6, 7, 8]
-        tmp = [''] * 8
+        board = [''] * 8
         castl = ''
-        
+
         bishop = random.choice((1, 3, 5, 7))
-        tmp[bishop-1] = 'b'
+        board[bishop - 1] = 'b'
         positions.remove(bishop)
 
         bishop = random.choice((2, 4, 6, 8))
-        tmp[bishop-1] = 'b'
+        board[bishop - 1] = 'b'
         positions.remove(bishop)
 
         queen = random.choice(positions)
-        tmp[queen-1] = 'q'
+        board[queen - 1] = 'q'
         positions.remove(queen)
 
         knight = random.choice(positions)
-        tmp[knight-1] = 'n'
+        board[knight - 1] = 'n'
         positions.remove(knight)
 
         knight = random.choice(positions)
-        tmp[knight-1] = 'n'
+        board[knight - 1] = 'n'
         positions.remove(knight)
 
         rook = positions[0]
-        tmp[rook-1] = 'r'
-        castl += reprFile[rook-1]
+        board[rook - 1] = 'r'
+        castl += reprFile[rook - 1]
 
         king = positions[1]
-        tmp[king-1] = 'k'
+        board[king - 1] = 'k'
 
         rook = positions[2]
-        tmp[rook-1] = 'r'
-        castl += reprFile[rook-1]
+        board[rook - 1] = 'r'
+        castl += reprFile[rook - 1]
 
-        tmp = ''.join(tmp)
-        tmp = tmp + '/pppppppp/8/8/8/8/PPPPPPPP/' + tmp.upper() + ' w ' + castl.upper() + castl +' - 0 1'
-        #tmp = "rnqbbknr/pppppppp/8/8/8/8/PPPPPPPP/RNQBBKNR w AHah - 0 1"
-        return tmp
+        fen = ''.join(board)
+        fen = fen + '/pppppppp/8/8/8/8/PPPPPPPP/' + fen.upper(
+        ) + ' w ' + castl.upper() + castl + ' - 0 1'
+        return fen
 
 
 if __name__ == '__main__':
