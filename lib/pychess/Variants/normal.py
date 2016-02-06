10539b88d9fc01b8fccefd0f840be71bcc9fdb18
cajone <peat@blueyonder.co.uk>
2016-02-06 16:08:09 +0000

    #docs pylint Variant/normal

--- lib/pychess/Variants/normal.py
+++ lib/pychess/Variants/normal.py
@@ -1,6 +1,10 @@
+""" Standard Chess game"""
 from pychess.Utils.Board import Board
 
+
 class NormalBoard(Board):
+    """:Description: A standard chess game board setup
+    """
     __desc__ = _("Classic chess rules\n" +
                  "http://en.wikipedia.org/wiki/Chess")
     name = _("Normal")
