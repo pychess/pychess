d1ddc16f5ee2c6c85d418a3c8f5ce6a72128937b
cajone <peat@blueyonder.co.uk>
2016-02-08 19:17:43 +0000

    #docs pylint Utils/Rating 5.00/10

--- lib/pychess/Utils/Rating.py
+++ lib/pychess/Utils/Rating.py
@@ -1,13 +1,21 @@
 from gi.repository import GObject
-from pychess.ic import *
+from pychess.ic import DEVIATION_NONE
 
-class Rating (GObject.GObject):
-    def __init__(self, ratingtype, elo, deviation=DEVIATION_NONE, wins=0,
-                 losses=0, draws=0, bestElo=0, bestTime=0):
+
+class Rating(GObject.GObject):
+    def __init__(self,
+                 ratingtype,
+                 elo,
+                 deviation=DEVIATION_NONE,
+                 wins=0,
+                 losses=0,
+                 draws=0,
+                 bestElo=0,
+                 bestTime=0):
         GObject.GObject.__init__(self)
         self.type = ratingtype
-        for v in (elo, deviation, wins, losses, draws, bestElo, bestTime):
-            assert v == None or isinstance(v, int), v
+        for value in (elo, deviation, wins, losses, draws, bestElo, bestTime):
+            assert value is None or isinstance(value, int), value
         self.elo = elo
         self.deviation = deviation
         self.wins = wins
@@ -15,15 +23,17 @@ class Rating (GObject.GObject):
         self.draws = draws
         self.bestElo = bestElo
         self.bestTime = bestTime
-    
-    def get_elo (self):
+
+    def get_elo(self):
         return self._elo
-    def set_elo (self, elo):
+
+    def set_elo(self, elo):
         assert isinstance(elo, int), type(elo)
         self._elo = elo
+
     elo = GObject.property(get_elo, set_elo)
-    
-    def __repr__ (self):
+
+    def __repr__(self):
         r = "type=%s, elo=%s" % (self.type, self.elo)
         if self.deviation != None:
             r += ", deviation=%s" % str(self.deviation)
@@ -38,9 +48,13 @@ class Rating (GObject.GObject):
         if self.bestTime > 0:
             r += ", bestTime=%s" % str(self.bestTime)
         return "<Rating " + r + ">"
-    
-    def copy (self):
-        return Rating(self.type, self.elo, deviation=self.deviation,
-            wins=self.wins, losses=self.losses, draws=self.draws,
-            bestElo=self.bestElo, bestTime=self.bestTime)
-        
+
+    def copy(self):
+        return Rating(self.type,
+                      self.elo,
+                      deviation=self.deviation,
+                      wins=self.wins,
+                      losses=self.losses,
+                      draws=self.draws,
+                      bestElo=self.bestElo,
+                      bestTime=self.bestTime)
