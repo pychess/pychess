26ef0f2a88a2e579ac0e6dd78bf1841d1c2b7abb
cajone <peat@blueyonder.co.uk>
2016-02-08 19:12:23 +0000

    #docs pylint Utils/Piece 5.17/10

--- lib/pychess/Utils/Piece.py
+++ lib/pychess/Utils/Piece.py
@@ -3,35 +3,39 @@ from pychess.Utils.repr import reprSign, reprColor, reprPiece
 
 
 class Piece:
-    def __init__ (self, color, piece, captured=False):
+    def __init__(self, color, piece, captured=False):
         self.color = color
         self.piece = piece
         self.captured = captured
-        
+
         # in crazyhouse we need to know this for later captures
         self.promoted = False
-        
+
         self.opacity = 1.0
         self.x = None
         self.y = None
-    
+
     # Sign is a deprecated synonym for piece
-    def _set_sign (self, sign):
+    def _set_sign(self, sign):
         self.piece = sign
-    def _get_sign (self):
+
+    def _get_sign(self):
         return self.piece
+
     sign = property(_get_sign, _set_sign)
-    
-    def __repr__ (self):
+
+    def __repr__(self):
         represen = "<%s %s" % (reprColor[self.color], reprPiece[self.piece])
         if self.opacity != 1.0:
             represen += " Op:%0.1f" % self.opacity
         if self.x != None or self.y != None:
             if self.x != None:
                 represen += " X:%0.1f" % self.x
-            else: represen += " X:None"
+            else:
+                represen += " X:None"
             if self.y != None:
                 represen += " Y:%0.1f" % self.y
-            else: represen += " Y:None"
+            else:
+                represen += " Y:None"
         represen += ">"
         return represen
