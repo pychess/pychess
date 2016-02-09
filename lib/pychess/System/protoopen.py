be961320f71f9659ddca45435ff901630e7d7b34
cajone <peat@blueyonder.co.uk>
2016-02-09 14:52:03 +0000

    #docs pylint System/protoopen 9.21/10

--- lib/pychess/System/protoopen.py
+++ lib/pychess/System/protoopen.py
@@ -5,17 +5,19 @@ from pychess.compat import open, urlopen, unquote
 
 PGN_ENCODING = "latin_1"
 
-def splitUri (uri):
-    uri = unquote(uri) # escape special chars
-    uri = uri.strip('\r\n\x00') # remove \r\n and NULL
+
+def splitUri(uri):
+    uri = unquote(uri)  # escape special chars
+    uri = uri.strip('\r\n\x00')  # remove \r\n and NULL
     if sys.platform == "win32":
         return uri.split(":///")
     else:
         return uri.split("://")
 
-def protoopen (uri):
+
+def protoopen(uri):
     """ Function for opening many things """
-   
+
     try:
         return open(uri, "rU", encoding=PGN_ENCODING)
     except (IOError, OSError):
@@ -28,11 +30,12 @@ def protoopen (uri):
 
     raise IOError("Protocol isn't supported by pychess")
 
-def protosave (uri, append=False):
+
+def protosave(uri, append=False):
     """ Function for saving many things """
-    
+
     splitted = splitUri(uri)
-    
+
     if splitted[0] == "file":
         if append:
             return open(splitted[1], "a", encoding=PGN_ENCODING)
@@ -44,14 +47,16 @@ def protosave (uri, append=False):
 
     raise IOError("PyChess doesn't support writing to protocol")
 
-def isWriteable (uri):
+
+def isWriteable(uri):
     """ Returns true if protoopen can open a write pipe to the uri """
-    
+
     splitted = splitUri(uri)
-    
+
     if splitted[0] == "file":
-        return os.access (splitted[1], os.W_OK)
+        return os.access(splitted[1], os.W_OK)
     elif len(splitted) == 1:
-        return os.access (splitted[0], os.W_OK)
-    
+        return os.access(splitted[0], os.W_OK)
+
     return False
+
