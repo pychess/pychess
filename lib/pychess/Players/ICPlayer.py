eb017abaf7bc2344acf162c2a7d344ab7e543b42
cajone <peat@blueyonder.co.uk>
2016-02-10 20:22:04 +0000

    #docs pylint Players/ICPlayer 8.12/10

--- lib/pychess/Players/ICPlayer.py
+++ lib/pychess/Players/ICPlayer.py
@@ -5,13 +5,21 @@ from pychess.Players.Player import Player, PlayerIsDead, TurnInterrupt
 from pychess.Utils.Move import parseSAN, toAN
 from pychess.Utils.lutils.lmove import ParsingError
 from pychess.Utils.Offer import Offer
-from pychess.Utils.const import *
+from pychess.Utils.const import REMOTE, UNFINISHED_STATES, CHAT_ACTION, CASTLE_KK, \
+    FISCHERRANDOMCHESS, CASTLE_SAN, TAKEBACK_OFFER
 from pychess.System.Log import log
 
-class ICPlayer (Player):
+
+class ICPlayer(Player):
     __type__ = REMOTE
 
-    def __init__ (self, gamemodel, ichandle, gameno, color, name, icrating=None):
+    def __init__(self,
+                 gamemodel,
+                 ichandle,
+                 gameno,
+                 color,
+                 name,
+                 icrating=None):
         Player.__init__(self)
         self.offers = {}
         self.queue = Queue()
@@ -29,67 +37,81 @@ class ICPlayer (Player):
 
         self.connection = connection = self.gamemodel.connection
         self.connections = connections = defaultdict(list)
-        connections[connection.bm].append(connection.bm.connect_after("boardUpdate", self.__boardUpdate))
-        connections[connection.bm].append(connection.bm.connect_after("playGameCreated", self.__playGameCreated))
-        connections[connection.bm].append(connection.bm.connect_after("obsGameCreated", self.__obsGameCreated))
-        connections[connection.om].append(connection.om.connect("onOfferAdd", self.__onOfferAdd))
-        connections[connection.om].append(connection.om.connect("onOfferRemove", self.__onOfferRemove))
-        connections[connection.om].append(connection.om.connect("onOfferDeclined", self.__onOfferDeclined))
-        connections[connection.cm].append(connection.cm.connect("privateMessage", self.__onPrivateMessage))
+        connections[connection.bm].append(connection.bm.connect_after(
+            "boardUpdate", self.__boardUpdate))
+        connections[connection.bm].append(connection.bm.connect_after(
+            "playGameCreated", self.__playGameCreated))
+        connections[connection.bm].append(connection.bm.connect_after(
+            "obsGameCreated", self.__obsGameCreated))
+        connections[connection.om].append(connection.om.connect(
+            "onOfferAdd", self.__onOfferAdd))
+        connections[connection.om].append(connection.om.connect(
+            "onOfferRemove", self.__onOfferRemove))
+        connections[connection.om].append(connection.om.connect(
+            "onOfferDeclined", self.__onOfferDeclined))
+        connections[connection.cm].append(connection.cm.connect(
+            "privateMessage", self.__onPrivateMessage))
 
-    def getICHandle (self):
+    def getICHandle(self):
         return self.name
 
     @property
-    def time (self):
+    def time(self):
         return self.gamemodel.timemodel.getPlayerTime(self.color)
 
     #===========================================================================
     #    Handle signals from the connection
     #===========================================================================
 
-    def __playGameCreated (self, bm, ficsgame):
+    def __playGameCreated(self, bm, ficsgame):
         if self.gamemodel.ficsplayers[0] == ficsgame.wplayer and \
             self.gamemodel.ficsplayers[1] == ficsgame.bplayer and \
             self.gameno == ficsgame.gameno:
-                log.debug("ICPlayer.__playGameCreated: gameno reappeared: gameno=%s white=%s black=%s" % \
-                    (ficsgame.gameno, ficsgame.wplayer.name, ficsgame.bplayer.name))
-                self.current = False
+            log.debug("ICPlayer.__playGameCreated: gameno reappeared: \
+                      gameno=%s white=%s black=%s" % (ficsgame.gameno, \
+                                                      ficsgame.wplayer.name, \
+                                                      ficsgame.bplayer.name))
+            self.current = False
 
-    def __obsGameCreated (self, bm, ficsgame):
+    def __obsGameCreated(self, bm, ficsgame):
         if self.gamemodel.ficsplayers[0] == ficsgame.wplayer and \
             self.gamemodel.ficsplayers[1] == ficsgame.bplayer and \
             self.gameno == ficsgame.gameno:
-                log.debug("ICPlayer.__obsGameCreated: gameno reappeared: gameno=%s white=%s black=%s" % \
-                    (ficsgame.gameno, ficsgame.wplayer.name, ficsgame.bplayer.name))
-                self.current = False
+            log.debug("ICPlayer.__obsGameCreated: gameno reappeared: \
+                      gameno=%s white=%s black=%s" % (ficsgame.gameno, \
+                                                      ficsgame.wplayer.name, \
+                                                      ficsgame.bplayer.name))
+            self.current = False
 
-    def __onOfferAdd (self, om, offer):
-        if self.gamemodel.status in UNFINISHED_STATES and not self.gamemodel.isObservationGame():
+    def __onOfferAdd(self, om, offer):
+        if self.gamemodel.status in UNFINISHED_STATES and not self.gamemodel.isObservationGame(
+        ):
             log.debug("ICPlayer.__onOfferAdd: emitting offer: self.gameno=%s self.name=%s %s" % \
                 (self.gameno, self.name, offer))
             self.offers[offer.index] = offer
-            self.emit ("offer", offer)
+            self.emit("offer", offer)
 
-    def __onOfferDeclined (self, om, offer):
+    def __onOfferDeclined(self, om, offer):
         for offer_ in list(self.gamemodel.offers.keys()):
             if offer.type == offer_.type:
                 offer.param = offer_.param
-        log.debug("ICPlayer.__onOfferDeclined: emitting decline for %s" % offer)
+        log.debug("ICPlayer.__onOfferDeclined: emitting decline for %s" %
+                  offer)
         self.emit("decline", offer)
 
-    def __onOfferRemove (self, om, offer):
+    def __onOfferRemove(self, om, offer):
         if offer.index in self.offers:
-            log.debug("ICPlayer.__onOfferRemove: emitting withdraw: self.gameno=%s self.name=%s %s" % \
-                (self.gameno, self.name, offer))
-            self.emit ("withdraw", self.offers[offer.index])
+            log.debug("ICPlayer.__onOfferRemove: emitting withdraw: \
+                      self.gameno=%s self.name=%s %s" % (self.gameno, self.name, offer))
+            self.emit("withdraw", self.offers[offer.index])
             del self.offers[offer.index]
 
-    def __onPrivateMessage (self, cm, name, title, isadmin, text):
+    def __onPrivateMessage(self, cm, name, title, isadmin, text):
         if name == self.ichandle:
             self.emit("offer", Offer(CHAT_ACTION, param=text))
 
-    def __boardUpdate (self, bm, gameno, ply, curcol, lastmove, fen, wname, bname, wms, bms):
+    def __boardUpdate(self, bm, gameno, ply, curcol, lastmove, fen, wname,
+                      bname, wms, bms):
         log.debug("ICPlayer.__boardUpdate: id(self)=%d self=%s %s %s %s %d %d %s %s %d %d" % \
             (id(self), self, gameno, wname, bname, ply, curcol, lastmove, fen, wms, bms))
 
@@ -105,9 +127,9 @@ class ICPlayer (Player):
             if ply <= self.gamemodel.ply:
                 return
 
-            if 1-curcol == self.color and ply == self.gamemodel.ply+1 and lastmove is not None:
-                log.debug("ICPlayer.__boardUpdate: id=%d self=%s ply=%d: putting move=%s in queue" % \
-                    (id(self), self, ply, lastmove))
+            if 1 - curcol == self.color and ply == self.gamemodel.ply + 1 and lastmove is not None:
+                log.debug("ICPlayer.__boardUpdate: id=%d self=%s ply=%d: \
+                          putting move=%s in queue" % (id(self), self, ply, lastmove))
                 self.queue.put((ply, lastmove))
                 # Ensure the fics thread doesn't continue parsing, before the
                 # game/player thread has received the move.
@@ -119,19 +141,20 @@ class ICPlayer (Player):
     #    Ending the game
     #===========================================================================
 
-    def __disconnect (self):
-        if self.connections is None: return
+    def __disconnect(self):
+        if self.connections is None:
+            return
         for obj in self.connections:
             for handler_id in self.connections[obj]:
                 if obj.handler_is_connected(handler_id):
                     obj.disconnect(handler_id)
         self.connections = None
 
-    def end (self, status, reason):
+    def end(self, status, reason):
         self.__disconnect()
         self.queue.put("del")
 
-    def kill (self, reason):
+    def kill(self, reason):
         self.__disconnect()
         self.queue.put("del")
 
@@ -139,15 +162,15 @@ class ICPlayer (Player):
     #    Send the player move updates
     #===========================================================================
 
-    def makeMove (self, board1, move, board2):
+    def makeMove(self, board1, move, board2):
         log.debug("ICPlayer.makemove: id(self)=%d self=%s move=%s board1=%s board2=%s" % \
                 (id(self), self, move, board1, board2))
         if board2 and not self.gamemodel.isObservationGame():
             # TODO: Will this work if we just always use CASTLE_SAN?
-            cn = CASTLE_KK
+            castle_notation = CASTLE_KK
             if board2.variant == FISCHERRANDOMCHESS:
-                cn = CASTLE_SAN
-            self.connection.bm.sendMove (toAN (board2, move, castleNotation=cn))
+                castle_notation = CASTLE_SAN
+            self.connection.bm.sendMove(toAN(board2, move, castleNotation=castle_notation))
 
         item = self.queue.get(block=True)
         try:
@@ -159,15 +182,15 @@ class ICPlayer (Player):
             ply, sanmove = item
             if ply < board1.ply:
                 # This should only happen in an observed game
-                board1 = self.gamemodel.getBoardAtPly(max(ply-1, 0))
+                board1 = self.gamemodel.getBoardAtPly(max(ply - 1, 0))
             log.debug("ICPlayer.makemove: id(self)=%d self=%s from queue got: ply=%d sanmove=%s" % \
                 (id(self), self, ply, sanmove))
 
             try:
-                move = parseSAN (board1, sanmove)
+                move = parseSAN(board1, sanmove)
                 log.debug("ICPlayer.makemove: id(self)=%d self=%s parsed move=%s" % \
                     (id(self), self, move))
-            except ParsingError as e:
+            except ParsingError as err:
                 raise
             return move
         finally:
@@ -179,19 +202,19 @@ class ICPlayer (Player):
     #    Interacting with the player
     #===========================================================================
 
-    def pause (self):
+    def pause(self):
         pass
 
-    def resume (self):
+    def resume(self):
         pass
 
-    def setBoard (self, fen):
+    def setBoard(self, fen):
         # setBoard will currently only be called for ServerPlayer when starting
         # to observe some game. In this case FICS already knows how the board
         # should look, and we don't need to set anything
         pass
 
-    def playerUndoMoves (self, movecount, gamemodel):
+    def playerUndoMoves(self, movecount, gamemodel):
         log.debug("ICPlayer.playerUndoMoves: id(self)=%d self=%s, undoing movecount=%d" % \
             (id(self), self, movecount))
         # If current player has changed so that it is no longer us to move,
@@ -199,47 +222,49 @@ class ICPlayer (Player):
         if movecount % 2 == 1 and gamemodel.curplayer != self:
             self.queue.put("int")
 
-    def resetPosition (self):
+    def resetPosition(self):
         """ Used in observed examined games f.e. when LectureBot starts another example"""
         self.queue.put("int")
 
-    def putMessage (self, text):
-        self.connection.cm.tellPlayer (self.ichandle, text)
+    def putMessage(self, text):
+        self.connection.cm.tellPlayer(self.ichandle, text)
 
     #===========================================================================
     #    Offer handling
     #===========================================================================
 
-    def offerRematch (self):
+    def offerRematch(self):
         if self.gamemodel.timed:
-            min = int(self.gamemodel.timemodel.intervals[0][0])/60
+            minimum = int(self.gamemodel.timemodel.intervals[0][0]) / 60
             inc = self.gamemodel.timemodel.gain
         else:
-            min = 0
+            minimum = 0
             inc = 0
         self.connection.om.challenge(self.ichandle,
-            self.gamemodel.ficsgame.game_type, min, inc,
-            self.gamemodel.ficsgame.rated)
+                                     self.gamemodel.ficsgame.game_type, minimum,
+                                     inc, self.gamemodel.ficsgame.rated)
 
-    def offer (self, offer):
+    def offer(self, offer):
         log.debug("ICPlayer.offer: self=%s %s" % (repr(self), offer))
         if offer.type == TAKEBACK_OFFER:
             # only 1 outstanding takeback offer allowed on FICS, so remove any of ours
             for index in list(self.offers.keys()):
                 if self.offers[index].type == TAKEBACK_OFFER:
-                    log.debug("ICPlayer.offer: del self.offers[%s] %s" % (index, offer))
+                    log.debug("ICPlayer.offer: del self.offers[%s] %s" %
+                              (index, offer))
                     del self.offers[index]
         self.connection.om.offer(offer, self.gamemodel.ply)
 
-    def offerDeclined (self, offer):
+    def offerDeclined(self, offer):
         log.debug("ICPlayer.offerDeclined: sending decline for %s" % offer)
         self.connection.om.decline(offer)
 
-    def offerWithdrawn (self, offer):
+    def offerWithdrawn(self, offer):
         pass
 
-    def offerError (self, offer, error):
+    def offerError(self, offer, error):
         pass
 
-    def observe (self):
+    def observe(self):
         self.connection.client.run_command("observe %s" % self.ichandle)
+
