28c7038ec768acd40eec0157720c42fcc4adf41d
cajone <peat@blueyonder.co.uk>
2016-02-11 11:33:48 +0000

    #docs pylint Players/PyChess 3.99/10

--- lib/pychess/Players/PyChess.py
+++ lib/pychess/Players/PyChess.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pypy -u
+###!/usr/bin/pypy -u
 from __future__ import print_function
 
 #if __name__ == "__main__":
@@ -9,34 +9,34 @@ import random
 import sys
 from time import time
 
+from pychess.Utils.book import getOpenings
+from pychess.Utils.const import WHITE, ASEANCHESS, SITTUYINCHESS, ATOMICCHESS, reprResult, \
+    CAMBODIANCHESS, LOSERSCHESS, KINGOFTHEHILLCHESS, DRAW, BLACKWON, WHITEWON, MAKRUKCHESS, \
+    SUICIDECHESS, THREECHECKCHESS
+from pychess.Utils.lutils import lsearch
+from pychess.Utils.lutils.ldata import MAXPLY
+from pychess.Utils.lutils.lsearch import alphaBeta
+from pychess.Utils.lutils.lmove import listToSan, toSAN
+from pychess.System.Log import log
+
 this_dir = os.path.dirname(os.path.abspath(__file__))
 if os.path.join(this_dir, "../..") not in sys.path:
     sys.path = [os.path.join(this_dir, "../..")] + sys.path
 
-from pychess.compat import PY2
-from pychess.Utils import const
-from pychess.Utils.book import getOpenings
-from pychess.Utils.const import *
-from pychess.Utils.lutils import lsearch
-from pychess.Utils.lutils.ldata import MAXPLY
-from pychess.Utils.lutils.lsearch import alphaBeta
-from pychess.Utils.lutils.LBoard import LBoard
-from pychess.Utils.lutils.lmove import listToSan, toSAN
-from pychess.System.Log import log
 
 
-class PyChess (object):
-    def __init__ (self):
+class PyChess(object):
+    def __init__(self):
         self.sd = MAXPLY
         self.skipPruneChance = 0
-        
+
         self.clock = [0, 0]
         self.increment = [0, 0]
         self.movestogo = 0
         self.searchtime = 0
-        self.scr = 0 # The current predicted score. Used when accepting draw offers
+        self.scr = 0  # The current predicted score. Used when accepting draw offers
         self.playingAs = WHITE
-        self.ponder = False # Currently unused
+        self.ponder = False  # Currently unused
         self.post = False
         self.debug = True
         self.outOfBook = False
@@ -45,28 +45,28 @@ class PyChess (object):
         print(text)
         sys.stdout.flush()
         log.debug(text, extra={"task": "stdout"})
-    
+
     #===========================================================================
     # Play related
     #===========================================================================
-    
-    def __remainingMovesA (self):
+
+    def __remainingMovesA(self):
         # Based on regression of a 180k games pgn
-        x = self.board.plyCount
-        return -1.71086e-12*x**6 \
-               +1.69103e-9*x**5 \
-               -6.00801e-7*x**4 \
-               +8.17741e-5*x**3 \
-               +2.91858e-4*x**2 \
-               -0.94497*x \
+        ply_count = self.board.plyCount
+        return -1.71086e-12*ply_count**6 \
+               +1.69103e-9*ply_count**5 \
+               -6.00801e-7*ply_count**4 \
+               +8.17741e-5*ply_count**3 \
+               +2.91858e-4*ply_count**2 \
+               -0.94497*ply_count \
                +78.8979
-    
-    def __remainingMovesB (self):
+
+    def __remainingMovesB(self):
         # We bet a game will be around 80 moves
-        x = self.board.plyCount
-        return max(80-x,4)
-    
-    def __getBestOpening (self):
+        ply_count = self.board.plyCount
+        return max(80 - ply_count, 4)
+
+    def __getBestOpening(self):
         totalWeight = 0
         choice = None
         if self.board.variant not in (ASEANCHESS, CAMBODIANCHESS, MAKRUKCHESS, \
@@ -81,21 +81,21 @@ class PyChess (object):
         if choice is None:
             self.outOfBook = True
         return choice
-    
-    def __go (self, ondone=None):
+
+    def __go(self, ondone=None):
         """ Finds and prints the best move from the current position """
-        
+
         mv = False if self.outOfBook else self.__getBestOpening()
         if mv:
             mvs = [mv]
-        
+
         if not mv:
-               
+
             lsearch.skipPruneChance = self.skipPruneChance
             lsearch.searching = True
-            
+
             timed = self.basetime > 0
-            
+
             if self.searchtime > 0:
                 usetime = self.searchtime
             else:
@@ -111,14 +111,17 @@ class PyChess (object):
             lsearch.endtime = starttime + usetime if timed else sys.maxsize
             if self.debug:
                 if timed:
-                    self.print("# Time left: %3.2f s; Planing to think for %3.2f s" % (self.clock[self.playingAs], usetime))
+                    self.print(
+                        "# Time left: %3.2f s; Planing to think for %3.2f s" %
+                        (self.clock[self.playingAs], usetime))
                 else:
-                    self.print("# Searching to depth %d without timelimit" % self.sd)
+                    self.print("# Searching to depth %d without timelimit" %
+                               self.sd)
 
-            for depth in range(1, self.sd+1):
+            for depth in range(1, self.sd + 1):
                 # Heuristic time saving
                 # Don't waste time, if the estimated isn't enough to complete next depth
-                if timed and usetime <= prevtime*4 and usetime > 1:
+                if timed and usetime <= prevtime * 4 and usetime > 1:
                     break
                 lsearch.timecheck_counter = lsearch.TIMECHECK_FREQ
                 search_result = alphaBeta(self.board, depth)
@@ -127,65 +130,69 @@ class PyChess (object):
                     if time() > lsearch.endtime:
                         break
                     if self.post:
-                        pv = " ".join(listToSan(self.board, mvs))
-                        time_cs = int(100 * (time()-starttime))
-                        self.print("%s %s %s %s %s" % (depth, self.scr, time_cs, lsearch.nodes, pv))
+                        pv1 = " ".join(listToSan(self.board, mvs))
+                        time_cs = int(100 * (time() - starttime))
+                        self.print("%s %s %s %s %s" % (
+                            depth, self.scr, time_cs, lsearch.nodes, pv1))
                 else:
                     # We were interrupted
                     if depth == 1:
                         mvs, self.scr = search_result
                     break
-                prevtime = time()-starttime - prevtime
-                
-                self.clock[self.playingAs] -= time() - starttime - self.increment[self.playingAs]
-            
+                prevtime = time() - starttime - prevtime
+
+                self.clock[self.playingAs] -= time(
+                ) - starttime - self.increment[self.playingAs]
+
             if not mvs:
                 if not lsearch.searching:
                     # We were interupted
                     lsearch.nodes = 0
                     return
-                
+
                 # This should only happen in terminal mode
-                
+
                 if self.scr == 0:
                     self.print("result %s" % reprResult[DRAW])
                 elif self.scr < 0:
                     if self.board.color == WHITE:
                         self.print("result %s" % reprResult[BLACKWON])
-                    else: self.print("result %s" % reprResult[WHITEWON])
+                    else:
+                        self.print("result %s" % reprResult[WHITEWON])
                 else:
                     if self.board.color == WHITE:
                         self.print("result %s" % reprResult[WHITEWON])
-                    else: self.print("result %s" % reprResult[BLACKWON])
+                    else:
+                        self.print("result %s" % reprResult[BLACKWON])
                 return
-            
+
             lsearch.nodes = 0
             lsearch.searching = False
-        
+
         move = mvs[0]
         sanmove = toSAN(self.board, move)
         if ondone: ondone(sanmove)
         return sanmove
-    
-    def __analyze (self):
+
+    def __analyze(self):
         """ Searches, and prints info from, the position as stated in the cecp
             protocol """
-        
+
         start = time()
         lsearch.endtime = sys.maxsize
         lsearch.searching = True
-        
-        for depth in range (1, self.sd):
+
+        for depth in range(1, self.sd):
             if not lsearch.searching:
                 break
-            t = time()
             board = self.board.clone()
-            mvs, scr = alphaBeta (board, depth)
-            
-            pv = " ".join(listToSan(board, mvs))
+            mvs, scr = alphaBeta(board, depth)
+
+            pv1 = " ".join(listToSan(board, mvs))
             time_cs = int(100 * (time() - start))
-            self.print("%s %s %s %s %s" % (depth, scr, time_cs, lsearch.nodes, pv))
-            
+            self.print("%s %s %s %s %s" %
+                       (depth, scr, time_cs, lsearch.nodes, pv1))
+
             lsearch.nodes = 0
 
 ################################################################################
@@ -206,6 +213,7 @@ if __name__ == "__main__":
     else:
         print("Unknown argument(s):", repr(sys.argv))
         sys.exit(0)
-        
+
     pychess.makeReady()
     pychess.run()
+
