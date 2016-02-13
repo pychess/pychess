909af6668200e046eea56013553ef5e4d21e93f8
cajone <peat@blueyonder.co.uk>
2016-02-13 15:37:23 +0000

    #docs pylint AutoLogout

--- lib/pychess/ic/managers/AutoLogOutManager.py
+++ lib/pychess/ic/managers/AutoLogOutManager.py
@@ -1,19 +1,20 @@
 from gi.repository import GObject
 
-class AutoLogOutManager (GObject.GObject):
-    __gsignals__ = {
-        'logOut': (GObject.SignalFlags.RUN_FIRST, None, ())
-    }
-    
-    def __init__ (self, connection):
+
+class AutoLogOutManager(GObject.GObject):
+    __gsignals__ = {'logOut': (GObject.SignalFlags.RUN_FIRST, None, ())}
+
+    def __init__(self, connection):
         GObject.GObject.__init__(self)
         self.connection = connection
-        
-        self.connection.expect_line (self.onLogOut,
-           "\*\*\*\* Auto-logout because you were idle more than \d+ minutes\. \*\*\*\*")
-        self.connection.expect_line (self.onLogOut, "Logging you out\.")
-        self.connection.expect_line (self.onLogOut,
-           "\*\*\*\* .+? has arrived - you can't both be logged in\. \*\*\*\*")
-    
-    def onLogOut (self, match):
+
+        self.connection.expect_line(
+            self.onLogOut,
+            "\*\*\*\* Auto-logout because you were idle more than \d+ minutes\. \*\*\*\*")
+        self.connection.expect_line(self.onLogOut, "Logging you out\.")
+        self.connection.expect_line(
+            self.onLogOut,
+            "\*\*\*\* .+? has arrived - you can't both be logged in\. \*\*\*\*")
+
+    def onLogOut(self, match):
         self.emit("logOut")
