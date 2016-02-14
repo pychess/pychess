246ffbe7aaa9a577cb2f0bc8869fbc619b435ba8
cajone <peat@blueyonder.co.uk>
2016-02-14 20:59:32 +0000

    #docs fixed regressions

--- lib/pychess/gfx/Pieces.py
+++ lib/pychess/gfx/Pieces.py
@@ -3,7 +3,7 @@ import re
 import cairo
 from gi.repository import Rsvg
 
-from pychess.Utils.const import BLACK, WHITE
+from pychess.Utils.const import BLACK, WHITE, KING, QUEEN, BISHOP, KNIGHT, ROOK, PAWN
 from pychess.System import conf
 from pychess.System.prefix import addDataPrefix
 from pychess.System.cairoextras import create_cairo_font_face_for_file
