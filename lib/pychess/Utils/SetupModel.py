692cfcc5d7341ee903fcbcd3bc294c17f28febf2
gbtami <gbtami@gmail.com>
2016-02-17 00:04:18 +0100

    Fixed Paste/Clear FEN worked on second click only bug

--- lib/pychess/Utils/SetupModel.py
+++ lib/pychess/Utils/SetupModel.py
@@ -27,10 +27,14 @@ class SetupPlayer:
     def __init__(self, board_control):
         self.queue = Queue()
         self.board_control = board_control
-        self.board_control.connect(
-            "action",
-            lambda bc, action, param: self.queue.put((action, param)))
+        self.board_control.connect("action", self.on_action)
         self.board_control.connect("piece_moved", self.piece_moved)
+
+    def on_action(self, bc, action, param):
+        self.queue.put((action, param))
+        if action == "SETUP":
+            # force both virtual player to make_move()
+            self.queue.put((action, param))
 
     def make_move(self):
         item = self.queue.get(block=True)
@@ -85,13 +89,13 @@ class SetupModel(GObject.GObject, Thread):
             player1 = self.curplayer.make_move()[1]
 
             if isinstance(player0, SetupMove):
-                #print(player0.cord0, player0.cord1, player1)
+                # print(player0.cord0, player0.cord1, player1)
                 new_board = self.boards[-1].move(player0, player1)
                 self.moves.append(player0)
                 self.boards.append(new_board)
                 self.emit("game_changed", self.ply)
             elif player0 == "SETUP":
-                #print("SETUP", player0, player1)
+                # print("SETUP", player0, player1)
                 self.emit("game_ended", 0)
                 self.boards = [self.variant(setup=player1)]
                 self.variations = [self.boards]
@@ -99,5 +103,5 @@ class SetupModel(GObject.GObject, Thread):
                 self.emit("game_started")
                 self.emit("game_changed", 0)
             elif player0 == "CLOSE":
-                #print("CLOSE")
+                # print("CLOSE")
                 break
