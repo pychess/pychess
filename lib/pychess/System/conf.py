db25ab03152a629a804a4415ec5c261b92ae1911
gbtami <gbtami@gmail.com>
2016-06-18 15:49:42 +0200

    get rid of conf_configParser.py

--- lib/pychess/System/conf.py
+++ lib/pychess/System/conf.py
@@ -4,40 +4,81 @@
 from __future__ import absolute_import
 import sys
 import os
+import atexit
 
-from . import conf_configParser as conf_Module
-"""Module for using gconf without having to care about types"""
+from pychess.compat import RawConfigParser
+from pychess.System.Log import log
+from pychess.System.prefix import addUserConfigPrefix
+
+configParser = RawConfigParser()
+section = "General"
+path = addUserConfigPrefix("config")
+if os.path.isfile(path):
+    configParser.readfp(open(path))
+if not configParser.has_section(section):
+    configParser.add_section(section)
+atexit.register(lambda: configParser.write(open(path, "w")))
+
+idkeyfuncs = {}
+conid = 0
 
 
 def notify_add(key, func, *args):
     """The signature for func must be self, client, *args, **kwargs"""
     assert isinstance(key, str)
-    return conf_Module.notify_add(key, func, args)
+    global conid
+    idkeyfuncs[conid] = (key, func, args)
+    conid += 1
+    return conid - 1
 
 
 def notify_remove(conid):
-    conf_Module.notify_remove(conid)
+    del idkeyfuncs[conid]
 
 
 def getStrict(key):
     assert hasKey(key)
-    return conf_Module.get(key)
+    return get(key)
 
 
-def get(key, alternative):
+def get(key, alternative=None):
     if hasKey(key):
-        return conf_Module.get(key)
+        try:
+            return configParser.getint(section, key)
+        except ValueError:
+            pass
+
+        try:
+            return configParser.getboolean(section, key)
+        except ValueError:
+            pass
+
+        try:
+            return configParser.getfloat(section, key)
+        except ValueError:
+            pass
+
+        return configParser.get(section, key)
     if callable(alternative):
         alternative = alternative()
     return alternative
 
 
 def set(key, value):
-    conf_Module.set(key, value)
+    try:
+        configParser.set(section, key, str(value))
+    except Exception as err:
+        log.error(
+            "Unable to save configuration '%s'='%s' because of error: %s %s" %
+            (repr(key), repr(value), err.__class__.__name__, ", ".join(
+                str(a) for a in err.args)))
+    for key_, func, args in idkeyfuncs.values():
+        if key_ == key:
+            func(None, *args)
 
 
 def hasKey(key):
-    return conf_Module.hasKey(key)
+    return configParser.has_option(section, key)
 
 
 if sys.platform == "win32":
