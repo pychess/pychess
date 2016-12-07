90e91d7a41b2c91d7b60000ac9b2e938917e7c76
Andrew Park <andrew.kw.park@gmail.com>
2016-12-07 14:22:57 -0600

    Prevent initial output when running the pychess engine in text-based mode

--- lib/pychess/Players/PyChessCECP.py
+++ lib/pychess/Players/PyChessCECP.py
@@ -85,8 +85,6 @@ class PyChessCECP(PyChess):
         }
         python = sys.executable.split("/")[-1]
         python_version = "%s.%s.%s" % sys.version_info[0:3]
-        print("# %s [%s %s]" %
-                   (self.features["myname"], python, python_version))
 
     def handle_sigterm(self, *args):
         self.__stopSearching()
