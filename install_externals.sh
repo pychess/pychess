191af825338123f244a4d926b76513c7b0a973a7
gbtami <gbtami@gmail.com>
2017-06-24 13:11:38 +0200

    Use more conservative make targets when building external tools

--- install_externals.sh
+++ install_externals.sh
@@ -10,7 +10,7 @@ if [ ! -d scoutfish ]; then
     git clone https://github.com/gbtami/scoutfish
 fi
 cd scoutfish; git pull
-cd src; make build ARCH=x86-64-modern; cd ../..
+cd src; make build ARCH=x86-64; cd ../..
 cp scoutfish/src/scoutfish ./lib/pychess/external
 cp scoutfish/src/scoutfish.py ./lib/pychess/external
 
@@ -19,7 +19,7 @@ if [ ! -d chess_db ]; then
     git clone https://github.com/mcostalba/chess_db
 fi
 cd chess_db; git pull
-cd parser; make build ARCH=x86-64-modern; cd ../..
+cd parser; make build ARCH=x86-64; cd ../..
 cp chess_db/parser/parser ./lib/pychess/external
 cp chess_db/parser/chess_db.py ./lib/pychess/external
 
@@ -27,5 +27,5 @@ if [ ! -d pgnextractor ]; then
     git clone https://github.com/sshivaji/pgnextractor
 fi
 cd pgnextractor; git pull
-cd parser; make build ARCH=x86-64-modern; cd ../..
+cd parser; make build ARCH=x86-64; cd ../..
 cp pgnextractor/parser/pgnextractor ./lib/pychess/external
