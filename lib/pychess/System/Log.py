a97d2d3e7e1dd41a84a62690a78c2b5ae59d72cf
gbtami <gbtami@gmail.com>
2017-07-04 12:24:58 +0200

    Silence Gtk-CRITICAL **: gtk_container_remove: assertion '_gtk_widget_get_parent (widget) == GTK_WIDGET (container) || GTK_IS_ASSISTANT (container) || GTK_IS_ACTION_BAR (container) || GTK_IS_POPOVER_MENU (container)' failed

--- lib/pychess/System/Log.py
+++ lib/pychess/System/Log.py
@@ -56,3 +56,59 @@ class ExtraAdapter(logging.LoggerAdapter):
 
 
 log = ExtraAdapter(logger, {})
+
+
+def setup_glib_logging():
+    """ Code from https://github.com/GNOME/meld/blob/master/bin/meld """
+    from gi.repository import GLib
+    levels = {
+        GLib.LogLevelFlags.LEVEL_DEBUG: logging.DEBUG,
+        GLib.LogLevelFlags.LEVEL_INFO: logging.INFO,
+        GLib.LogLevelFlags.LEVEL_MESSAGE: logging.INFO,
+        GLib.LogLevelFlags.LEVEL_WARNING: logging.WARNING,
+        GLib.LogLevelFlags.LEVEL_ERROR: logging.ERROR,
+        GLib.LogLevelFlags.LEVEL_CRITICAL: logging.CRITICAL,
+    }
+    level_flag = (
+        GLib.LogLevelFlags.LEVEL_WARNING |
+        GLib.LogLevelFlags.LEVEL_ERROR |
+        GLib.LogLevelFlags.LEVEL_CRITICAL
+    )
+
+    log_domain = "Gtk"
+    log = logging.getLogger(log_domain)
+
+    def silence(message):
+        if "gtk_container_remove: assertion" in message:
+            return True
+        return False
+
+    # This logging handler is for "old" glib logging using a simple
+    # syslog-style API.
+    def log_adapter(domain, level, message, user_data):
+        if not silence(message):
+            log.log(levels.get(level, logging.WARNING), message)
+
+    try:
+        GLib.log_set_handler(log_domain, level_flag, log_adapter, None)
+    except AttributeError:
+        # Only present in glib 2.46+
+        pass
+
+    # This logging handler is for new glib logging using a structured
+    # API. Unfortunately, it was added in such a way that the old
+    # redirection API became a no-op, so we need to hack both of these
+    # handlers to get it to work.
+    def structured_log_adapter(level, fields, field_count, user_data):
+        message = GLib.log_writer_format_fields(level, fields, True)
+        if not silence(message):
+            log.log(levels.get(level, logging.WARNING), message)
+        return GLib.LogWriterOutput.HANDLED
+
+    try:
+        GLib.log_set_writer_func(structured_log_adapter, None)
+    except AttributeError:
+        # Only present in glib 2.50+
+        pass
+
+setup_glib_logging()
