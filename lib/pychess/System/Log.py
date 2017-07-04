e4c0710413b1e3d4904a6793e71b3774e8358ddc
gbtami <gbtami@gmail.com>
2017-07-04 12:31:45 +0200

    Comment about why particular log silenced

--- lib/pychess/System/Log.py
+++ lib/pychess/System/Log.py
@@ -80,6 +80,8 @@ def setup_glib_logging():
 
     def silence(message):
         if "gtk_container_remove: assertion" in message:
+            # Looks like it was some hackish code in GTK+ which is now removed:
+            # https://git.gnome.org/browse/gtk+/commit/?id=a3805333361fee37a3b1a974cfa6452a85169f08
             return True
         return False
 
