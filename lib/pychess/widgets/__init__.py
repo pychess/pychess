8d205f30ca8bf1de18a2a9e9cb3729059c62eef1
gbtami <gbtami@gmail.com>
2017-10-31 20:06:40 +0100

    Use simple tooltip text instead of custom tooltip widget

--- lib/pychess/widgets/__init__.py
+++ lib/pychess/widgets/__init__.py
@@ -45,6 +45,7 @@ def dock_panel_tab(title, desc, icon, button=None):
     pixbuf = get_pixbuf(icon, 16)
     image = Gtk.Image.new_from_pixbuf(pixbuf)
     label = Gtk.Label(label=title)
+    box.set_tooltip_text(desc)
     box.pack_start(image, False, True, 0)
     box.pack_start(label, False, True, 0)
     if button is not None:
@@ -53,28 +54,6 @@ def dock_panel_tab(title, desc, icon, button=None):
     box.set_spacing(2)
     box.show_all()
 
-    def cb(widget, x, y, keyboard_mode, tooltip, title, desc, filename):
-        table = Gtk.Table(2, 2)
-        table.set_row_spacings(2)
-        table.set_col_spacings(6)
-        table.set_border_width(4)
-        pixbuf = get_pixbuf(filename, 56)
-        image = Gtk.Image.new_from_pixbuf(pixbuf)
-        image.set_alignment(0, 0)
-        table.attach(image, 0, 1, 0, 2)
-        titleLabel = Gtk.Label()
-        titleLabel.set_markup("<b>%s</b>" % title)
-        titleLabel.set_alignment(0, 0)
-        table.attach(titleLabel, 1, 2, 0, 1)
-        descLabel = Gtk.Label(label=desc)
-        descLabel.props.wrap = True
-        table.attach(descLabel, 1, 2, 1, 2)
-        tooltip.set_custom(table)
-        table.show_all()
-        return True
-
-    box.props.has_tooltip = True
-    box.connect("query-tooltip", cb, title, desc, icon)
     return box
 
 
