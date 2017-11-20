20f62278141af83eee882c4e58236adb3933a54c
gbtami <gbtami@gmail.com>
2017-11-20 10:16:04 +0100

    Add stockfish to .rpm and .deb package dependencies

--- create_rpm_py35.sh
+++ create_rpm_py35.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python3 setup.py bdist_rpm --python=/usr/bin/python3.5 --release=1.py35 --requires python3-sqlalchemy,python3-pexpect,python3-psutil,python3-gobject,python3-cairo,gobject-introspection,glib2,gtk3,pango,gdk-pixbuf2,gtksourceview3,gstreamer1,gstreamer1-plugins-base
+python3 setup.py bdist_rpm --python=/usr/bin/python3.5 --release=1.py35 --requires python3-sqlalchemy,python3-pexpect,python3-psutil,python3-gobject,python3-cairo,gobject-introspection,glib2,gtk3,pango,gdk-pixbuf2,gtksourceview3,gstreamer1,gstreamer1-plugins-base,stockfish
 
