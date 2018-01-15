30b84fa80f6b091ca9969d18128c3844f0748c57
gbtami <gbtami@gmail.com>
2018-01-15 22:59:52 +0100

    Revorked move history panel using only 1 treeview instead of 3

--- lib/pychess/perspectives/games/historyPanel.py
+++ lib/pychess/perspectives/games/historyPanel.py
@@ -1,8 +1,9 @@
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 
 from pychess.System import conf
 from pychess.System.prefix import addDataPrefix
 from pychess.Utils.Move import toSAN, toFAN
+from pychess.widgets.Background import hexcol
 
 __title__ = _("Move History")
 __active__ = True
@@ -13,152 +14,156 @@ __desc__ = _(
 
 class Sidepanel:
     def load(self, gmwidg):
-        __widget__ = Gtk.ScrolledWindow()
-        __widget__.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
 
-        self.numbers = Gtk.TreeView()
-        self.numbers.set_property("can_focus", False)
-        self.numbers.set_property("sensitive", False)
-        self.numbers.set_property("width_request", 35)
-        self.numbers.set_fixed_height_mode(True)
-        self.left = Gtk.TreeView()
-        self.numbers.set_property("width_request", 60)
-        self.left.set_fixed_height_mode(True)
-        self.right = Gtk.TreeView()
-        self.numbers.set_property("width_request", 60)
-        self.right.set_fixed_height_mode(True)
-
-        box = Gtk.Box(spacing=0)
-        box.pack_start(self.numbers, False, True, 0)
-        box.pack_start(self.left, True, True, 0)
-        box.pack_start(self.right, True, True, 0)
-
-        port = Gtk.Viewport()
-        port.add(box)
-        port.set_shadow_type(Gtk.ShadowType.NONE)
-
-        __widget__.add(port)
-        __widget__.show_all()
-
-        self.cids = {}
-        self.boardview = gmwidg.board.view
-
+        self.gamemodel = gmwidg.board.view.model
+        self.black_starts = self.gamemodel.lowply % 2 == 1
         self.model_cids = [
-            self.boardview.model.connect_after("game_changed", self.game_changed),
-            self.boardview.model.connect_after("game_started", self.game_started),
-            self.boardview.model.connect_after("moves_undone", self.moves_undone),
-            self.boardview.model.connect_after("game_terminated", self.on_game_terminated),
+            self.gamemodel.connect_after("game_changed", self.game_changed),
+            self.gamemodel.connect_after("game_started", self.game_started),
+            self.gamemodel.connect_after("moves_undone", self.moves_undone),
+            self.gamemodel.connect_after("game_terminated", self.on_game_terminated),
         ]
-        self.cids[self.boardview] = self.boardview.connect("shownChanged", self.shownChanged)
 
-        # Initialize treeviews
+        self.tv = Gtk.TreeView()
+        self.tv.set_headers_visible(False)
+        self.tv.set_grid_lines(True)
+        self.tv.set_activate_on_single_click(True)
+        self.tv.get_selection().set_mode(Gtk.SelectionMode.NONE)
+        self.activated_cell = (None, None)
 
-        def fixList(list, xalign=0):
-            list.set_property("headers_visible", False)
-            list.set_property("rules_hint", True)
-            list.set_model(Gtk.ListStore(str))
-            renderer = Gtk.CellRendererText()
-            renderer.set_property("xalign", xalign)
-            column = Gtk.TreeViewColumn(None, renderer, text=0)
-            column.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
-            list.append_column(column)
-            list.get_selection().set_mode(Gtk.SelectionMode.SINGLE)
+        def is_row_separator(treemodel, treeiter):
+            mvcount, wmove, bmove, row, wbg, bbg = self.store[treeiter]
+            return row == 0
+        self.tv.set_row_separator_func(is_row_separator)
 
-        fixList(self.numbers, 1)
-        fixList(self.left, 0)
-        fixList(self.right, 0)
+        renderer = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("mvcount", renderer, text=0)
+        column.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
+        self.tv.append_column(column)
 
-        self.cids[self.left] = self.left.connect('cursor_changed', self.cursorChanged, self.left, 0)
-        self.cids[self.right] = self.right.connect('cursor_changed', self.cursorChanged, self.right, 1)
+        self.white_renderer = Gtk.CellRendererText()
+        self.white_renderer.set_property("xalign", 1)
+        self.white_column = Gtk.TreeViewColumn("white", self.white_renderer, text=1, background=4)
+        self.white_column.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
+        self.tv.append_column(self.white_column)
 
-        # Lock scrolling
+        self.black_renderer = Gtk.CellRendererText()
+        self.black_renderer.set_property("xalign", 1)
+        self.black_column = Gtk.TreeViewColumn("black", self.black_renderer, text=2, background=5)
+        self.black_column.set_sizing(Gtk.TreeViewColumnSizing.FIXED)
+        self.tv.append_column(self.black_column)
 
-        self.adjustment = __widget__.get_vadjustment()
+        # To prevent black moves column expand to the right we add a dummy column finally
+        renderer = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("dummy", renderer)
+        self.tv.append_column(column)
 
-        def changed(vadjust):
-            if not hasattr(vadjust, "need_scroll") or vadjust.need_scroll:
-                vadjust.set_value(vadjust.get_upper() - vadjust.get_page_size(
-                ))
-                vadjust.need_scroll = True
+        scrollwin = Gtk.ScrolledWindow()
+        scrollwin.add(self.tv)
 
-        self.adj_cid1 = self.adjustment.connect("changed", changed)
+        # Our liststore elements will be:
+        # mvcount, white move, black move, row number, white move background, black move background
+        self.store = Gtk.ListStore(str, str, str, int, str, str)
+        self.tv.set_model(self.store)
+        self.tv.get_selection().set_mode(Gtk.SelectionMode.BROWSE)
+        self.tv_cid = self.tv.connect('row_activated', self.on_row_activated)
 
-        def value_changed(vadjust):
-            vadjust.need_scroll = abs(vadjust.get_value() + vadjust.get_page_size() -
-                                      vadjust.get_upper()) < vadjust.get_step_increment()
-
-        self.adj_cid2 = self.adjustment.connect("value-changed", value_changed)
-
-        # Connect to preferences
+        self.boardview = gmwidg.board.view
+        self.cid = self.boardview.connect("shownChanged", self.shownChanged)
+        scrollwin.show_all()
 
         def figuresInNotationCallback(none):
             game = self.boardview.model
-            for board, move in zip(game.variations[0], game.moves):
+            for i, move in enumerate(game.moves):
+                board = game.variations[0][i]
+                ply = game.lowply + i + 1
                 if conf.get("figuresInNotation", False):
                     notat = toFAN(board, move)
                 else:
                     notat = toSAN(board, move, True)
-                row, col, other = self._ply_to_row_col_other(board.ply + 1)
-                iter = col.get_model().get_iter((row, ))
-                col.get_model().set(iter, 0, notat)
+
+                row, column = self.ply_to_row_col(ply)
+
+                col = 2 if column == self.black_column else 1
+                treeiter = self.store.get_iter(Gtk.TreePath(row))
+                self.store.set_value(treeiter, col, notat)
 
         self.conf_conid = conf.notify_add("figuresInNotation", figuresInNotationCallback)
 
-        # Return
+        return scrollwin
 
-        return __widget__
+    def get_background_rgba(self, selected=False):
+        found, color = self.tv.get_style_context().lookup_color("p_bg_selected")
+        if selected:
+            return hexcol(Gdk.RGBA(color.red, color.green, color.blue, 1))
+        else:
+            return hexcol(Gdk.RGBA(1, 1, 1, 1))
 
     def on_game_terminated(self, model):
-        conf.notify_remove(self.conf_conid)
-        self.adjustment.disconnect(self.adj_cid1)
-        self.adjustment.disconnect(self.adj_cid2)
+        self.tv.disconnect(self.tv_cid)
         for cid in self.model_cids:
-            self.boardview.model.disconnect(cid)
-        for obj in self.cids:
-            obj.disconnect(self.cids[obj])
-        self.cids = {}
+            self.gamemodel.disconnect(cid)
+        self.boardview.disconnect(self.cid)
 
-    def cursorChanged(self, widget, tree, col):
-        path, focus_column = tree.get_cursor()
-        if path is None:
+    def on_row_activated(self, tv, path, col, from_show_changed=False):
+        if col not in (self.white_column, self.black_column):
             return
-        indices = path.get_indices()
-        row = indices[0]
+        selection = self.tv.get_selection()
+        selection.unselect_all()
 
-        if self.boardview.model.lowply & 1:
-            ply = row * 2 + col
-        else:
-            ply = row * 2 + col + 1
+        # Make previous activated cell background color unselected
+        old_row, old_col = self.activated_cell
+        if old_row is not None:
+            bg_col = 5 if old_col == self.black_column else 4
+            treeiter = self.store.get_iter(Gtk.TreePath(old_row))
+            self.store.set_value(treeiter, bg_col, self.get_background_rgba(selected=False))
 
-        board = self.boardview.model.boards[ply]
-        self.boardview.setShownBoard(board)
+        # Make activated cell background color selected
+        self.activated_cell = path[0], col
+        bg_col = 5 if col == self.black_column else 4
+        treeiter = self.store.get_iter(Gtk.TreePath(path[0]))
+        self.store.set_value(treeiter, bg_col, self.get_background_rgba(selected=True))
 
-    def moves_undone(self, game, moves):
-        for i in reversed(range(moves)):
-            try:
-                row, view, other = self._ply_to_row_col_other(
-                    game.variations[0][-1].ply + moves - i)
-                model = view.get_model()
-                model.remove(model.get_iter((row, )))
-                if view == self.left:
-                    model = self.numbers.get_model()
-                    model.remove(model.get_iter((row, )))
-            except ValueError:
-                continue
+        index = path[0] * 2 - 1 + (1 if col == self.black_column else 0)
+        if self.black_starts:
+            index -= 1
 
-    def game_changed(self, game, ply):
+        if index < len(self.gamemodel.boards):
+            # Don't set shown board if on_row_activated() was called from shownChanged()
+            if not from_show_changed:
+                board = self.gamemodel.boards[index]
+                self.boardview.setShownBoard(board)
+
+    def shownChanged(self, boardview, shown):
+        if boardview is None or self.gamemodel is None:
+            return
+        if not boardview.shownIsMainLine():
+            return
+
+        row, column = self.ply_to_row_col(shown)
+
+        try:
+            self.on_row_activated(self, Gtk.TreePath(row), column, from_show_changed=True)
+            self.tv.scroll_to_cell(row)
+        except ValueError:
+            pass
+            # deleted variations by moves_undoing
+
+    def moves_undone(self, gamemodel, moves):
+        for i in range(moves):
+            treeiter = self.store.get_iter((len(self.store) - 1, ))
+            # If latest move is black move don't remove whole line!
+            if self.store[-1][2]:
+                self.store.set_value(treeiter, 2, "")
+            else:
+                self.store.remove(treeiter)
+
+    def game_changed(self, gamemodel, ply):
         if self.boardview is None or self.boardview.model is None:
             return
-        left_model = self.left.get_model()
-        right_model = self.right.get_model()
-        if left_model is None or right_model is None:
-            return
 
-        len_ = len(left_model) + len(right_model) + 1
-        if len(left_model) and not left_model[0][0]:
-            len_ -= 1
-        for i in range(len_ + game.lowply, ply + 1):
-            self.__addMove(game, i)
+        for i in range(len(self.store) + gamemodel.lowply, ply + 1):
+            self.add_move(gamemodel, i)
         self.shownChanged(self.boardview, ply)
 
     def game_started(self, game):
@@ -166,59 +171,33 @@ class Sidepanel:
             return
         self.game_changed(game, game.ply)
 
-    def __addMove(self, game, ply):
-        # print "Am I doing anything?"
-        row, view, other = self._ply_to_row_col_other(ply)
+    def add_move(self, gamemodel, ply):
+        if ply == gamemodel.lowply:
+            self.store.append(["%4s." % gamemodel.lowply, "1234567", "1234567", 0, self.get_background_rgba(), self.get_background_rgba()])
+            return
 
         if conf.get("figuresInNotation", False):
-            notat = toFAN(
-                game.getBoardAtPly(ply - 1), game.getMoveAtPly(ply - 1))
+            notat = toFAN(gamemodel.getBoardAtPly(ply - 1), gamemodel.getMoveAtPly(ply - 1))
         else:
-            notat = toSAN(
-                game.getBoardAtPly(ply - 1),
-                game.getMoveAtPly(ply - 1),
-                localRepr=True)
+            notat = toSAN(gamemodel.getBoardAtPly(ply - 1), gamemodel.getMoveAtPly(ply - 1), localRepr=True)
 
-        # Test if the row is 'filled'
-        if len(view.get_model()) == len(self.numbers.get_model()):
-            num = str((ply + 1) // 2) + "."
-            self.numbers.get_model().append([num])
+        row, column = self.ply_to_row_col(ply)
 
-        # Test if the move is black first move. This will be the case if the
-        # game was loaded from a fen/epd starting at black
-        if view == self.right and len(view.get_model()) == len(other.get_model(
-        )):
-            self.left.get_model().append([""])
-
-        view.get_model().append([notat])
-
-    def shownChanged(self, boardview, shown):
-        if self.boardview is None or self.boardview.model is None:
-            return
-        if not self.boardview.shownIsMainLine():
-            return
-        if shown <= self.boardview.model.lowply:
-            # print "Or is it me?"
-            self.left.get_selection().unselect_all()
-            self.right.get_selection().unselect_all()
-            return
-
-        row, col, other = self._ply_to_row_col_other(shown)
-
-        other.get_selection().unselect_all()
-        col.set_cursor(row)
-        try:
-            col.get_selection().select_iter(col.get_model().get_iter(row))
-            col.scroll_to_cell((row, ), None, False)
-        except ValueError:
-            pass
-            # deleted variations by moves_undoing
-
-    def _ply_to_row_col_other(self, ply):
-        col = ply & 1 and self.left or self.right
-        other = ply & 1 and self.right or self.left
-        if self.boardview.model.lowply & 1:
-            row = (ply - self.boardview.model.lowply) // 2
+        if len(self.store) - 1 < row:
+            mvcount = "%s." % ((ply + 1) // 2)
+            if column == self.white_column:
+                self.store.append([mvcount, notat, "", row, self.get_background_rgba(), self.get_background_rgba()])
+            else:
+                self.store.append([mvcount, "", notat, row, self.get_background_rgba(), self.get_background_rgba()])
         else:
-            row = (ply - self.boardview.model.lowply - 1) // 2
-        return row, col, other
+            treeiter = self.store.get_iter(Gtk.TreePath(row))
+            col = 1 if column == self.white_column else 2
+            self.store.set_value(treeiter, col, notat)
+
+    def ply_to_row_col(self, ply):
+        col = ply & 1 and self.white_column or self.black_column
+        if self.gamemodel.lowply & 1:
+            row = (ply - self.gamemodel.lowply) // 2
+        else:
+            row = (ply - self.gamemodel.lowply - 1) // 2
+        return row + 1, col
