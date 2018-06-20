f7153bca536e12338e56bfc7ce846bc63c6f588f
gbtami <gbtami@gmail.com>
2018-06-20 10:33:28 +0200

    Add movelist to test data

--- testing/polyglot.py
+++ testing/polyglot.py
@@ -5,15 +5,15 @@ from pychess.Utils.lutils.LBoard import LBoard
 
 # Examples taken from http://alpha.uhasselt.be/Research/Algebra/Toga/book_format.html
 testcases = [
-    ["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 0x463b96181691fc9c],
-    ["rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1", 0x823c9b50fd114196],
-    ["rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 2", 0x0756b94461c50fb0],
-    ["rnbqkbnr/ppp1pppp/8/3pP3/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 2", 0x662fafb965db29d4],
-    ["rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 3", 0x22a48b5a8e47ff78],
-    ["rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPPKPPP/RNBQ1BNR b kq - 0 3", 0x652a607ca3f242c1],
-    ["rnbq1bnr/ppp1pkpp/8/3pPp2/8/8/PPPPKPPP/RNBQ1BNR w - - 0 4", 0x00fdd303c946bdd9],
-    ["rnbqkbnr/p1pppppp/8/8/PpP4P/8/1P1PPPP1/RNBQKBNR b KQkq c3 0 3", 0x3c8123ea7b067637],
-    ["rnbqkbnr/p1pppppp/8/8/P6P/R1p5/1P1PPPP1/1NBQKBNR b Kkq - 0 4", 0x5c3f9b829b279560],
+    ["rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 0x463b96181691fc9c, ""],
+    ["rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1", 0x823c9b50fd114196, "e2e4"],
+    ["rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 2", 0x0756b94461c50fb0, "e2e4 d7d5"],
+    ["rnbqkbnr/ppp1pppp/8/3pP3/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 2", 0x662fafb965db29d4, "e2e4 d7d5 e4e5"],
+    ["rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 3", 0x22a48b5a8e47ff78, "e2e4 d7d5 e4e5 f7f5"],
+    ["rnbqkbnr/ppp1p1pp/8/3pPp2/8/8/PPPPKPPP/RNBQ1BNR b kq - 0 3", 0x652a607ca3f242c1, "e2e4 d7d5 e4e5 f7f5 e1e2"],
+    ["rnbq1bnr/ppp1pkpp/8/3pPp2/8/8/PPPPKPPP/RNBQ1BNR w - - 0 4", 0x00fdd303c946bdd9, "e2e4 d7d5 e4e5 f7f5 e1e2 e8f7"],
+    ["rnbqkbnr/p1pppppp/8/8/PpP4P/8/1P1PPPP1/RNBQKBNR b KQkq c3 0 3", 0x3c8123ea7b067637, "a2a4 b7b5 h2h4 b5b4 c2c4"],
+    ["rnbqkbnr/p1pppppp/8/8/P6P/R1p5/1P1PPPP1/1NBQKBNR b Kkq - 0 4", 0x5c3f9b829b279560, "a2a4 b7b5 h2h4 b5b4 c2c4 b4c3 a1a3"],
 ]
 
 
@@ -26,6 +26,7 @@ class PolyglotTestCase(unittest.TestCase):
             board = LBoard(Board)
             board.applyFen(testcase[0])
             self.assertEqual(board.hash, testcase[1])
+
 
 if __name__ == '__main__':
     unittest.main()
