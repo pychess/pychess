6e7d5297d7762867a9840db27472172ccc6e08e8
ecrucru <ecrucru@users.noreply.github.com>
2019-04-10 19:50:51 +0200

    Unit test for remote games

---
+++ testing/remotegame.py
@@ -0,0 +1,28 @@
+import unittest
+import random
+
+from pychess.Savers.remotegame import chess_providers, get_internet_game_as_pgn
+
+
+class RemoteGameTestCase(unittest.TestCase):
+    def test(self):
+        for cp in chess_providers:
+            print("\n%s" % cp.get_description())
+
+            # Pick one link only to not overload the remote server
+            list = cp.get_test_links()
+            if len(list) == 0:
+                continue
+            url, expected = random.choice(list)
+            print('- Target link : %s' % url)
+            print('- Expecting data : %s' % expected)
+
+            # Download link
+            data = get_internet_game_as_pgn(url)
+            ok = not data is None
+            print('- Fetched data : %s' % ok)
+            self.assertEqual(ok, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
