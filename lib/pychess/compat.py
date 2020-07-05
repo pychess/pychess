d66485283d852a5ab9fbd16e7e92277d9c14d296
gbtami <gbtami@gmail.com>
2020-07-05 09:17:17 +0200

    Fix 'RuntimeError: no running event loop' on Python >= 3.7

--- lib/pychess/compat.py
+++ lib/pychess/compat.py
@@ -1,8 +1,4 @@
 import asyncio
-import sys
 
-if sys.version_info < (3, 7, 0):
-    def create_task(coro):
-        return asyncio.get_event_loop().create_task(coro)
-else:
-    create_task = asyncio.create_task
+def create_task(coro):
+    return asyncio.get_event_loop().create_task(coro)
