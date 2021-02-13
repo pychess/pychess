50857f64aa33dc48578c5b754e155549adb96b26
gbtami <gbtami@gmail.com>
2021-02-13 10:17:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -103,6 +103,11 @@ CLASSIFIERS = [
     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Operating System :: POSIX',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'Topic :: Games/Entertainment :: Board Games']
 
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
