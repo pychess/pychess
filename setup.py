87615755a528b538bad9f9ff79b140d986a7d130
Sebastian Pipping <sebastian@pipping.org>
2023-03-08 23:53:25 +0100

    setup.py: Sync Python classifiers with reality

--- setup.py
+++ setup.py
@@ -88,11 +88,11 @@ CLASSIFIERS = [
     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Operating System :: POSIX',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
     'Topic :: Games/Entertainment :: Board Games']
 
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
