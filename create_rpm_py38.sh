62cd50f48c20366860c4f9d38de071a65feee23d
gbtami <gbtami@gmail.com>
2024-09-09 15:37:16 +0200

    Drop Python 3.8 support

--- create_rpm_py38.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export PYTHONUSERBASE=/usr
-python3 setup.py bdist_rpm --python=/usr/bin/python3.8 --release=1.py38 --requires python3-sqlalchemy,python3-pexpect,python3-psutil,python3-websockets,python3-gobject,python3-cairo,gobject-introspection,glib2,gtk3,pango,gdk-pixbuf2,gtksourceview3,gstreamer1,gstreamer1-plugins-base,stockfish
-
--- setup.py
+++ setup.py
@@ -95,7 +95,6 @@ CLASSIFIERS = [
     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     "Operating System :: POSIX",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
@@ -375,7 +374,7 @@ setup(
     license="GPL3",
     url="https://pychess.github.io/",
     download_url="https://github.com/pychess/pychess/releases",
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=[
         "pexpect",
         "psutil",
