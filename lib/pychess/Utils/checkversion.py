d019c5cf97f685b3c261f215f89b5d3967d379bf
Emfox Zhou <emfox@debian.org>
2026-09-16 10:27:02 +0800

    Compare version numbers numerically in checkversion.py

--- lib/pychess/Utils/checkversion.py
+++ lib/pychess/Utils/checkversion.py
@@ -1,5 +1,6 @@
 import os.path
 import json
+import re
 
 from gi.repository import GLib, Gtk
 
@@ -9,6 +10,14 @@ from pychess.System import download_file_async, prefix
 
 URL = "https://api.github.com/repos/pychess/pychess/releases/latest"
 LINK = "https://github.com/pychess/pychess/releases"
+
+
+def is_newer(version, reference):
+    """Return True if the given version is newer than the reference one."""
+    new = [int(part) for part in re.findall(r"\d+", version)]
+    old = [int(part) for part in re.findall(r"\d+", reference)]
+    size = max(len(new), len(old))
+    return new + [0] * (size - len(new)) > old + [0] * (size - len(old))
 
 
 def isgit():
@@ -39,5 +48,5 @@ async def checkversion():
         msg_dialog.connect("response", lambda msg_dialog, a: msg_dialog.hide())
         msg_dialog.show()
 
-    if new_version is not None and VERSION.split(".") < new_version.split("."):
+    if new_version is not None and is_newer(new_version, VERSION):
         GLib.idle_add(notify, new_version)
